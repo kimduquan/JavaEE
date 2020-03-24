1cb41286eaa7e885c793598bc2fd6f7f9be99d31
Kim Du Quan <kimduquan@gmail.com>
2020-03-25 03:56:21 +0700

    #

--- OpenUP/OpenUP-war/src/java/openup/ws/rs/OpenUP.java
+++ OpenUP/OpenUP-war/src/java/openup/ws/rs/OpenUP.java
@@ -5,6 +5,10 @@
  */
 package openup.ws.rs;
 
+import javax.annotation.security.DenyAll;
+import javax.enterprise.context.ApplicationScoped;
+import javax.security.enterprise.authentication.mechanism.http.BasicAuthenticationMechanismDefinition;
+import javax.security.enterprise.identitystore.LdapIdentityStoreDefinition;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.core.Application;
@@ -20,5 +24,16 @@ import javax.ws.rs.Produces;
 @ApplicationPath("openup")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+@ApplicationScoped
+@BasicAuthenticationMechanismDefinition( realmName = "file" )
+@LdapIdentityStoreDefinition(
+        url = "ldap://localhost:10389",
+        bindDn = "uid=admin,ou=system",
+        bindDnPassword = "admin",
+        callerBaseDn = "dc=OpenUP,dc=WSO2,dc=ORG",
+        callerSearchBase = "ou=Roles,dc=OpenUP,dc=WSO2,dc=ORG",
+        groupSearchBase = "ou=Roles,dc=OpenUP,dc=WSO2,dc=ORG"
+)
+@DenyAll
 public class OpenUP extends Application {
 }
