60dd99dc97ee3b4adea65f576a0c557faa395acb
Kim Du Quan <kimduquan@gmail.com>
2020-07-03 22:14:11 +0700

    #

---
+++ OpenUP-micro/OpenUP-micro/src/main/java/openup/micro/OpenUPApplication.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package openup.micro;
+
+import javax.annotation.security.RolesAllowed;
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.auth.LoginConfig;
+
+/**
+ *
+ * @author FOXCONN
+ */
+@ApplicationScoped
+@ApplicationPath("/openup")
+@LoginConfig(authMethod="MP-JWT", realmName="file")
+@RolesAllowed("AnyRole")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class OpenUPApplication extends Application {
+}
