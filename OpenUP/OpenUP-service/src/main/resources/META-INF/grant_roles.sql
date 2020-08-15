05fd0b999db44f562c642a9a1a5b07c340657c7c
Kim Du Quan <kimduquan@gmail.com>
2020-08-15 17:00:06 +0700

    #add more sql rights

---
+++ OpenUP/OpenUP-service/src/main/resources/META-INF/grant_roles.sql
@@ -0,0 +1,40 @@
+/*Basic Roles BEGIN*/
+/*GRANT Analyst TO PUBLIC;
+GRANT Any_Role TO PUBLIC;
+GRANT Architect TO PUBLIC;
+GRANT Developer TO PUBLIC;
+GRANT Project_Manager TO PUBLIC;
+GRANT Stakeholder TO PUBLIC;
+GRANT Tester TO PUBLIC;*/
+
+GRANT Analyst TO Any_Role;
+GRANT Architect TO Any_Role;
+GRANT Developer TO Any_Role;
+GRANT Project_Manager TO Any_Role;
+GRANT Stakeholder TO Any_Role;
+GRANT Tester TO Any_Role;
+/*Basic Roles END*/
+
+/*Deployment BEGIN*/
+/*GRANT Course_Developer TO PUBLIC;
+GRANT Deployment_Engineer TO PUBLIC;
+GRANT Deployment_Manager TO PUBLIC;
+GRANT Product_Owner TO PUBLIC;
+GRANT Technical_Writer TO PUBLIC;
+GRANT Trainer TO PUBLIC;*/
+
+GRANT Course_Developer TO Any_Role;
+GRANT Deployment_Engineer TO Any_Role;
+GRANT Deployment_Manager TO Any_Role;
+GRANT Product_Owner TO Any_Role;
+GRANT Technical_Writer TO Any_Role;
+GRANT Trainer TO Any_Role;
+/*Deployment END*/
+
+/*Environment BEGIN*/
+/*GRANT Process_Engineer TO PUBLIC;
+GRANT Tool_Specialist TO PUBLIC;/*
+
+GRANT Process_Engineer TO Any_Role;
+GRANT Tool_Specialist TO Any_Role;
+/*Environment END*/
--- OpenUP/OpenUP-service/src/main/resources/META-INF/users.sql
+++ OpenUP/OpenUP-service/src/main/resources/META-INF/users.sql
@@ -6,14 +6,6 @@ CREATE USER IF NOT EXISTS developer1 PASSWORD 'developer';
 CREATE USER IF NOT EXISTS project_manager1 PASSWORD 'project_manager';
 CREATE USER IF NOT EXISTS stakeholder1 PASSWORD 'stakeholder';
 CREATE USER IF NOT EXISTS tester1 PASSWORD 'tester';
-
-GRANT Analyst TO analyst1;
-GRANT Any_Role TO any_role1;
-GRANT Architect TO architect1;
-GRANT Developer TO developer1;
-GRANT Project_Manager TO project_manager1;
-GRANT Stakeholder TO stakeholder1;
-GRANT Tester TO tester1;
 /*Basic Roles END*/
 
 /*Deployment BEGIN*/
@@ -23,19 +15,9 @@ CREATE USER IF NOT EXISTS deployment_manager1 PASSWORD 'deployment_manager';
 CREATE USER IF NOT EXISTS product_owner1 PASSWORD 'product_owner';
 CREATE USER IF NOT EXISTS technical_writer1 PASSWORD 'technical_writer';
 CREATE USER IF NOT EXISTS trainer1 PASSWORD 'trainer';
-
-GRANT Course_Developer TO course_developer1;
-GRANT Deployment_Engineer TO deployment_engineer1;
-GRANT Deployment_Manager TO deployment_manager1;
-GRANT Product_Owner TO product_owner1;
-GRANT Technical_Writer TO technical_writer1;
-GRANT Trainer TO trainer1;
 /*Deployment END*/
 
 /*Environment BEGIN*/
 CREATE USER IF NOT EXISTS process_engineer1 PASSWORD 'process_engineer';
 CREATE USER IF NOT EXISTS tool_specialist1 PASSWORD 'tool_specialist';
-
-GRANT Process_Engineer TO process_engineer1;
-GRANT Tool_Specialist TO tool_specialist1;
 /*Environment END*/
