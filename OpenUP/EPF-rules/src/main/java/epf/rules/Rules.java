12f79b3fcd65fabd968c04056907141b70f50f60
kimduquan <kimduquan@gmail.com>
2021-06-05 19:11:12 +0700

    #

---
+++ OpenUP/EPF-rules/src/main/java/epf/rules/Rules.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package epf.rules;
+
+import javax.ws.rs.Path;
+
+/**
+ * @author PC
+ *
+ */
+@Path("rules")
+public class Rules implements epf.client.rules.Rules {
+
+}
---
+++ OpenUP/EPF-rules/src/main/java/epf/rules/admin/Admin.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package epf.rules.admin;
+
+import javax.ws.rs.Path;
+
+/**
+ * @author PC
+ *
+ */
+@Path("rules/admin")
+public class Admin implements epf.client.rules.admin.Admin {
+
+}
