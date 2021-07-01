d43b2f971bb5af8df81f507967d1fd13deb456cf
kimduquan <kimduquan@gmail.com>
2021-07-01 21:44:29 +0700

    #

--- OpenUP/EPF-cache/src/main/java/epf/cache/Cache.java
+++ OpenUP/EPF-cache/src/main/java/epf/cache/Cache.java
@@ -3,19 +3,22 @@
  */
 package epf.cache;
 
+import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import epf.cache.persistence.Persistence;
+import epf.schema.roles.Role;
 
 /**
  * @author PC
  *
  */
 @ApplicationScoped
-@Path("/")
+@Path("cache")
+@RolesAllowed(Role.DEFAULT_ROLE)
 public class Cache implements epf.client.cache.Cache {
 
 	/**
