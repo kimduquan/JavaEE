cb4561e28c2282f82e111ffbcf585a79a4a40bac
kimduquan <kimduquan@gmail.com>
2021-07-10 00:05:19 +0700

    #

--- OpenUP/EPF-schema/src/main/java/epf/schema/roles/section/Relationships.java
+++ OpenUP/EPF-schema/src/main/java/epf/schema/roles/section/Relationships.java
@@ -5,16 +5,8 @@
  */
 package epf.schema.roles.section;
 
-import epf.schema.EPF;
-import epf.schema.tasks.Task;
-import epf.schema.work_products.Artifact;
 import java.io.Serializable;
-import java.util.List;
 import javax.persistence.Embeddable;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import org.eclipse.microprofile.graphql.Name;
 import org.eclipse.microprofile.graphql.Type;
 
 /**
@@ -29,54 +21,4 @@ public class Relationships implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
-	/**
-     * 
-     */
-    @ManyToMany
-    @JoinTable(
-            name = "ROLE_ADDITIONALLY_PERFORMS",
-            schema = EPF.SCHEMA,
-            joinColumns = @JoinColumn(
-                    name = "ROLE"
-            ),
-            inverseJoinColumns = @JoinColumn(
-                    name = "TASK"
-            )
-    )
-    private List<Task> additionallyPerforms;
-    
-    /**
-     * 
-     */
-    @ManyToMany
-    @JoinTable(
-            name = "ROLE_MODIFIES",
-            schema = EPF.SCHEMA,
-            joinColumns = @JoinColumn(
-                    name = "ROLE"
-            ),
-            inverseJoinColumns = @JoinColumn(
-                    name = "ARTIFACT"
-            )
-    )
-    private List<Artifact> modifies;
-    
-    @Name("Additionally_Performs")
-    public List<Task> getAdditionallyPerforms(){
-        return additionallyPerforms;
-    }
-
-    public void setAdditionallyPerforms(final List<Task> additionallyPerforms) {
-        this.additionallyPerforms = additionallyPerforms;
-    }
-    
-    @Name("Modifies")
-    public List<Artifact> getModifies(){
-        return modifies;
-    }
-
-    public void setModifies(final List<Artifact> modifies) {
-        this.modifies = modifies;
-    }
 }
