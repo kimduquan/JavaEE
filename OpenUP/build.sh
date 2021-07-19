f8d5ebb18331cc6fa44cfd44c911ebf1b130c04f
kimduquan <kimduquan@gmail.com>
2021-07-20 01:35:58 +0700

    #

--- OpenUP/build.sh
+++ OpenUP/build.sh
@@ -1,6 +1,6 @@
 export JAVA_HOME=~/graalvm-ce-java11-21.1.0
-./copy_dependency.sh
 git pull
+./copy_dependency.sh
 cd EPF-persistence
 mvn clean install
 cd ../
--- OpenUP/startup.sh
+++ OpenUP/startup.sh
@@ -1,3 +1,5 @@
+cd ~/jaeger-1.24.0-linux-amd64
+./jaeger-all-in-one
 export PATH=~/graalvm-ee-java8-21.1.0/bin:$PATH
 export JAVA_HOME=~/graalvm-ee-java8-21.1.0
 cd ~/pluto-3.1.0/bin
