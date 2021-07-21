c0b576668240d5ad71bba8c7f8a5525159ad65e8
kimduquan <kimduquan@gmail.com>
2021-07-21 21:47:52 +0700

    #

--- OpenUP/build.sh
+++ OpenUP/build.sh
@@ -2,7 +2,10 @@ git pull
 ./copy_dependency.sh
 ./startup.sh
 export JAVA_HOME=~/graalvm-ce-java11-21.1.0
-#export JAVA_HOME=~/graalvm-ee-java8-21.1.0
+cd EPF-image
+mvn clean install
+cd ../
+export JAVA_HOME=~/graalvm-ee-java8-21.1.0
 cd EPF-persistence
 mvn clean install
 cd ../
