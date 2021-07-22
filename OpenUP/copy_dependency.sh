25c0a05cefa3cc8823f85ffc70c41dcf99bbd11e
kimduquan <kimduquan@gmail.com>
2021-07-23 02:15:39 +0700

    #

--- OpenUP/copy_dependency.sh
+++ OpenUP/copy_dependency.sh
@@ -1,3 +1,3 @@
 cd dependency
-mvn dependency:copy-dependencies -DincludeScope=compile -DoutputDirectory="~/pluto-3.1.0/lib"
+mvn dependency:copy-dependencies -DincludeScope=compile -DoutputDirectory=/home/kimduquan/pluto-3.1.0/lib
 cd ../
