6fed9c9e07f4d99f4fb290ede1e3fcfe344e816b
kimduquan <kimduquan@gmail.com>
2021-07-23 00:57:13 +0700

    #

--- OpenUP/shutdown.sh
+++ OpenUP/shutdown.sh
@@ -1,2 +1,3 @@
 export JAVA_HOME=~/graalvm-ee-java8-21.1.0
 ~/pluto-3.1.0/bin/shutdown.sh
+rm /tmp/.X10-lock
--- OpenUP/startup.sh
+++ OpenUP/startup.sh
@@ -1,3 +1,6 @@
 export JAVA_HOME=~/graalvm-ee-java8-21.1.0
 ~/pluto-3.1.0/bin/startup.sh
 #~/jaeger-1.24.0-linux-amd64/jaeger-all-in-one
+sudo chmod -R 1777 /tmp
+Xvfb :10 -ac &
+export DISPLAY=:10
