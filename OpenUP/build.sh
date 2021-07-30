68c905ff94c086a5c7c671ff6c48bf65fb1364ff
kimduquan <kimduquan@gmail.com>
2021-07-30 16:19:10 +0700

    #

--- OpenUP/build.sh
+++ OpenUP/build.sh
@@ -2,11 +2,11 @@ git pull
 git clean -f -d
 ./copy_dependency.sh
 ./startup.sh
-export JAVA_HOME=~/graalvm-ce-java11-21.1.0
+export JAVA_HOME=~/graalvm-ee-java11-21.2.0
 cd EPF-image
 mvn clean install
 cd ../
-export JAVA_HOME=~/graalvm-ee-java8-21.1.0
+export JAVA_HOME=~/graalvm-ee-java8-21.2.0
 cd EPF-persistence
 mvn clean install
 cd ../
--- OpenUP/startup.sh
+++ OpenUP/startup.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME=~/graalvm-ee-java8-21.1.0
+export JAVA_HOME=~/graalvm-ee-java8-21.2.0
 ~/pluto-3.1.0/bin/startup.sh
 ~/jaeger-1.24.0-linux-amd64/jaeger-all-in-one &
 #sudo chmod -R 1777 /tmp
