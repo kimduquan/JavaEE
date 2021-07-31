2d17be4e6077904894df7ddf38d76ba091be7baf
kimduquan <kimduquan@gmail.com>
2021-07-31 09:25:22 +0700

    #

---
+++ OpenUP/OpenUP-client/src/main/java/openup/client/OpenUPException.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package openup.client;
+
+/**
+ * @author PC
+ *
+ */
+public class OpenUPException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @param cause
+	 */
+	public OpenUPException(final Throwable cause) {
+		super(cause);
+	}
+}
