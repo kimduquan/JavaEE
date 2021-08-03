42802220b624834b33cc332405ee7e324b9b4339
kimduquan <kimduquan@gmail.com>
2021-08-03 18:00:04 +0700

    #

--- OpenUP/clean.bat
+++ OpenUP/clean.bat
@@ -2,12 +2,12 @@ set cur_dir=%CD%
 set portal_dir="C:\Program Files\pluto-3.1.0\webapps\"
 cd %portal_dir%
 del ".\*.war" /s /f /q
-del ".\OpenUP-work-products-portlet" /s /f /q
-del ".\OpenUP-tasks-portlet" /s /f /q
-del ".\OpenUP-roles-portlet" /s /f /q
-del ".\OpenUP-delivery-processes-portlet" /s /f /q
-del ".\EPF-security-portlet" /s /f /q
-del ".\EPF-persistence-portlet" /s /f /q
-del ".\EPF-messaging-portlet" /s /f /q
-del ".\EPF-file-portlet" /s /f /q
+rmdir "OpenUP-work-products-portlet" /s /q
+rmdir "OpenUP-tasks-portlet" /s /q
+rmdir "OpenUP-roles-portlet" /s /q
+rmdir "OpenUP-delivery-processes-portlet" /s /q
+rmdir "EPF-security-portlet" /s /q
+rmdir "EPF-persistence-portlet" /s /q
+rmdir "EPF-messaging-portlet" /s /q
+rmdir "EPF-file-portlet" /s /q
 cd %cur_dir%
