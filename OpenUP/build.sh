9071997281171e8d0db26956bded2e9701b71ee8
kimduquan <kimduquan@gmail.com>
2021-08-14 14:47:08 +0700

    #

--- OpenUP/build.sh
+++ OpenUP/build.sh
@@ -2,7 +2,7 @@ export PATH=~/jdk-11.0.11+9/bin:$PATH
 export JAVA_HOME=~/jdk-11.0.11+9
 git pull
 git clean -f -d
-cp .\settings.xml ~/.m2
+cp settings.xml ~/.m2
 ./clean.sh
 ./copy_dependency.sh
 ./startup.sh
