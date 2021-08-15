7c5d1a42321c910b60b2b0eeeb75d378387dfecc
kimduquan <kimduquan@gmail.com>
2021-08-16 06:58:04 +0700

    #

--- OpenUP/EPF-webapp/src/main/java/epf/webapp/WebApp.java
+++ OpenUP/EPF-webapp/src/main/java/epf/webapp/WebApp.java
@@ -1,40 +1,9 @@
 package epf.webapp;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@WebServlet("/webapp")
-public class WebApp extends HttpServlet {
+public interface WebApp {
+	
 	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
+     * 
      */
-    public WebApp() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().print("<h1><font color=green>Simple Servlet ran successfully</font></h1>"
-                        + "Powered by WebSphere Application Server Liberty Profile");
-    }
-
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
-        //    TODO Auto-generated method stub
-    }
-
+    String WEBAPP_URL = "epf.webapp.url";
 }
---
+++ OpenUP/EPF-webapp/src/main/java/epf/webapp/security/EPFIdentityStore.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package epf.webapp.security;
+
+import java.net.URI;
+import java.net.URL;
+import java.util.Set;
+import java.util.logging.Logger;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.security.enterprise.authentication.mechanism.http.BasicAuthenticationMechanismDefinition;
+import javax.security.enterprise.credential.BasicAuthenticationCredential;
+import javax.security.enterprise.identitystore.CredentialValidationResult;
+import javax.security.enterprise.identitystore.IdentityStore;
+import epf.util.client.Client;
+import epf.util.client.ClientQueue;
+import epf.util.logging.Logging;
+import epf.util.security.PasswordUtil;
+import epf.webapp.WebApp;
+import epf.util.SystemUtil;
+import epf.client.gateway.GatewayUtil;
+import epf.client.security.Security;
+import epf.client.security.Token;
+
+/**
+ *
+ * @author FOXCONN
+ */
+@ApplicationScoped
+@BasicAuthenticationMechanismDefinition(realmName = "EPF")
+public class EPFIdentityStore implements IdentityStore {
+	
+	/**
+	 * 
+	 */
+	private static final Logger LOGGER = Logging.getLogger(EPFIdentityStore.class.getName());
+    
+    /**
+     * 
+     */
+    @Inject
+    private transient ClientQueue clients;
+    
+    /**
+     * @param credential
+     * @return
+     * @throws Exception
+     */
+    public CredentialValidationResult validate(final BasicAuthenticationCredential credential) {
+        CredentialValidationResult result = CredentialValidationResult.INVALID_RESULT;
+        try {
+        	final String passwordHash = PasswordUtil.hash(credential.getCaller(), credential.getPassword().getValue());
+        	final URL webAppUrl = new URL(SystemUtil.getenv(WebApp.WEBAPP_URL));
+        	final URI securityUrl = GatewayUtil.get("security");
+        	Token token = null;
+        	try(Client client = new Client(clients, securityUrl, b -> b)){
+        		final String rawToken = Security.login(
+            			client,
+    					credential.getCaller(),
+    					passwordHash,
+    					webAppUrl
+    					);
+                if(!rawToken.isEmpty()){
+                	client.authorization(rawToken);
+                	token = Security.authenticate(client);
+                	if(token != null) {
+                		token.setRawToken(rawToken);
+                	}
+                }
+            }
+            if(token != null){
+            	result = new CredentialValidationResult(token.getName(), token.getGroups());
+            }
+        }
+        catch(Exception ex) {
+        	LOGGER.throwing(getClass().getName(), "validate", ex);
+        }
+        return result;
+    }
+    
+    @Override
+    public Set<String> getCallerGroups(final CredentialValidationResult validationResult){
+        return validationResult.getCallerGroups();
+    }
+}
