dfe239acc7be4a28f9605c6c890eb84e04377282
kimduquan <kimduquan@gmail.com>
2021-10-10 17:08:08 +0700

    #

--- OpenUP/startup.bat
+++ OpenUP/startup.bat
@@ -1,10 +1,16 @@
 setlocal
 set cur_dir=%CD%
 set cur_java=$JAVA_HOME
+set kafka_dir="C:\kafka_2.13-2.8.1\bin\windows"
+cd %kafka_dir%
+start .\zookeeper-server-start.bat ..\..\config\zookeeper.properties
+cd %cur_dir%
 set JAVA_HOME=C:\jdk8u292-b10
 set portal_dir="C:\Program Files\pluto-3.1.0\bin\"
 cd %portal_dir%
 call .\startup.bat
 cd %cur_dir%
-set JAVA_HOME=%cur_java%
+cd %kafka_dir%
+start .\kafka-server-start.bat ..\..\config\server.properties
+cd %cur_dir%
 endlocal
