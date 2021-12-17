80f6fad251f9a6901362ea6a663096e87da149f0
kimduquan <kimduquan@gmail.com>
2021-12-17 21:54:15 +0700

    #

--- OpenUP/EPF-persistence-schema/src/main/resources/META-INF/mysql/6.grant_schemas.sql
+++ OpenUP/EPF-persistence-schema/src/main/resources/META-INF/mysql/6.grant_schemas.sql
@@ -1,43 +1,43 @@
 /*Basic Roles BEGIN*/
-GRANT SELECT ON EPF_Delivery_Processes.* TO 'Analyst'@'localhost';
-GRANT SELECT ON EPF_Delivery_Processes.* TO 'Any_Role'@'localhost';
-GRANT SELECT ON EPF_Delivery_Processes.* TO 'Architect'@'localhost';
-GRANT SELECT ON EPF_Delivery_Processes.* TO 'Developer'@'localhost';
-GRANT SELECT ON EPF_Delivery_Processes.* TO 'Project_Manager'@'localhost';
-GRANT SELECT ON EPF_Delivery_Processes.* TO 'Stakeholder'@'localhost';
-GRANT SELECT ON EPF_Delivery_Processes.* TO 'Tester'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'Analyst'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'Any_Role'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'Architect'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'Developer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'Project_Manager'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'Stakeholder'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'Tester'@'localhost';
 
-GRANT SELECT ON EPF_Roles.* TO 'Analyst'@'localhost';
-GRANT SELECT ON EPF_Roles.* TO 'Any_Role'@'localhost';
-GRANT SELECT ON EPF_Roles.* TO 'Architect'@'localhost';
-GRANT SELECT ON EPF_Roles.* TO 'Developer'@'localhost';
-GRANT SELECT ON EPF_Roles.* TO 'Project_Manager'@'localhost';
-GRANT SELECT ON EPF_Roles.* TO 'Stakeholder'@'localhost';
-GRANT SELECT ON EPF_Roles.* TO 'Tester'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'Analyst'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'Any_Role'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'Architect'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'Developer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'Project_Manager'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'Stakeholder'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'Tester'@'localhost';
 
-GRANT SELECT ON EPF_Tasks.* TO 'Analyst'@'localhost';
-GRANT SELECT ON EPF_Tasks.* TO 'Any_Role'@'localhost';
-GRANT SELECT ON EPF_Tasks.* TO 'Architect'@'localhost';
-GRANT SELECT ON EPF_Tasks.* TO 'Developer'@'localhost';
-GRANT SELECT ON EPF_Tasks.* TO 'Project_Manager'@'localhost';
-GRANT SELECT ON EPF_Tasks.* TO 'Stakeholder'@'localhost';
-GRANT SELECT ON EPF_Tasks.* TO 'Tester'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'Analyst'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'Any_Role'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'Architect'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'Developer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'Project_Manager'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'Stakeholder'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'Tester'@'localhost';
 
-GRANT SELECT ON EPF_Work_Products.* TO 'Analyst'@'localhost';
-GRANT SELECT ON EPF_Work_Products.* TO 'Any_Role'@'localhost';
-GRANT SELECT ON EPF_Work_Products.* TO 'Architect'@'localhost';
-GRANT SELECT ON EPF_Work_Products.* TO 'Developer'@'localhost';
-GRANT SELECT ON EPF_Work_Products.* TO 'Project_Manager'@'localhost';
-GRANT SELECT ON EPF_Work_Products.* TO 'Stakeholder'@'localhost';
-GRANT SELECT ON EPF_Work_Products.* TO 'Tester'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'Analyst'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'Any_Role'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'Architect'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'Developer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'Project_Manager'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'Stakeholder'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'Tester'@'localhost';
 
-GRANT SELECT ON OPENUP.* TO 'Analyst'@'localhost';
-GRANT SELECT ON OPENUP.* TO 'Any_Role'@'localhost';
-GRANT SELECT ON OPENUP.* TO 'Architect'@'localhost';
-GRANT SELECT ON OPENUP.* TO 'Developer'@'localhost';
-GRANT SELECT ON OPENUP.* TO 'Project_Manager'@'localhost';
-GRANT SELECT ON OPENUP.* TO 'Stakeholder'@'localhost';
-GRANT SELECT ON OPENUP.* TO 'Tester'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'Analyst'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'Any_Role'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'Architect'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'Developer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'Project_Manager'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'Stakeholder'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'Tester'@'localhost';
 
 /*REVOKE ALL ON sys.* FROM 'Analyst';
 REVOKE ALL ON sys.* FROM 'Any_Role';
@@ -57,40 +57,40 @@ REVOKE ALL ON sys.* FROM 'tester1'@'localhost';*/
 /*Basic Roles END*/
 
 /*Deployment BEGIN*/
-GRANT SELECT ON EPF_Delivery_Processes.* TO 'Course_Developer'@'localhost';
-GRANT SELECT ON EPF_Delivery_Processes.* TO 'Deployment_Engineer'@'localhost';
-GRANT SELECT ON EPF_Delivery_Processes.* TO 'Deployment_Manager'@'localhost';
-GRANT SELECT ON EPF_Delivery_Processes.* TO 'Product_Owner'@'localhost';
-GRANT SELECT ON EPF_Delivery_Processes.* TO 'Technical_Writer'@'localhost';
-GRANT SELECT ON EPF_Delivery_Processes.* TO 'Trainer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'Course_Developer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'Deployment_Engineer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'Deployment_Manager'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'Product_Owner'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'Technical_Writer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'Trainer'@'localhost';
 
-GRANT SELECT ON EPF_Roles.* TO 'Course_Developer'@'localhost';
-GRANT SELECT ON EPF_Roles.* TO 'Deployment_Engineer'@'localhost';
-GRANT SELECT ON EPF_Roles.* TO 'Deployment_Manager'@'localhost';
-GRANT SELECT ON EPF_Roles.* TO 'Product_Owner'@'localhost';
-GRANT SELECT ON EPF_Roles.* TO 'Technical_Writer'@'localhost';
-GRANT SELECT ON EPF_Roles.* TO 'Trainer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'Course_Developer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'Deployment_Engineer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'Deployment_Manager'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'Product_Owner'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'Technical_Writer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'Trainer'@'localhost';
 
-GRANT SELECT ON EPF_Tasks.* TO 'Course_Developer'@'localhost';
-GRANT SELECT ON EPF_Tasks.* TO 'Deployment_Engineer'@'localhost';
-GRANT SELECT ON EPF_Tasks.* TO 'Deployment_Manager'@'localhost';
-GRANT SELECT ON EPF_Tasks.* TO 'Product_Owner'@'localhost';
-GRANT SELECT ON EPF_Tasks.* TO 'Technical_Writer'@'localhost';
-GRANT SELECT ON EPF_Tasks.* TO 'Trainer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'Course_Developer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'Deployment_Engineer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'Deployment_Manager'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'Product_Owner'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'Technical_Writer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'Trainer'@'localhost';
 
-GRANT SELECT ON EPF_Work_Products.* TO 'Course_Developer'@'localhost';
-GRANT SELECT ON EPF_Work_Products.* TO 'Deployment_Engineer'@'localhost';
-GRANT SELECT ON EPF_Work_Products.* TO 'Deployment_Manager'@'localhost';
-GRANT SELECT ON EPF_Work_Products.* TO 'Product_Owner'@'localhost';
-GRANT SELECT ON EPF_Work_Products.* TO 'Technical_Writer'@'localhost';
-GRANT SELECT ON EPF_Work_Products.* TO 'Trainer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'Course_Developer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'Deployment_Engineer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'Deployment_Manager'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'Product_Owner'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'Technical_Writer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'Trainer'@'localhost';
 
-GRANT SELECT ON OPENUP.* TO 'Course_Developer'@'localhost';
-GRANT SELECT ON OPENUP.* TO 'Deployment_Engineer'@'localhost';
-GRANT SELECT ON OPENUP.* TO 'Deployment_Manager'@'localhost';
-GRANT SELECT ON OPENUP.* TO 'Product_Owner'@'localhost';
-GRANT SELECT ON OPENUP.* TO 'Technical_Writer'@'localhost';
-GRANT SELECT ON OPENUP.* TO 'Trainer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'Course_Developer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'Deployment_Engineer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'Deployment_Manager'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'Product_Owner'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'Technical_Writer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'Trainer'@'localhost';
 
 /*REVOKE ALL ON sys.* FROM 'Course_Developer';
 REVOKE ALL ON sys.* FROM 'Deployment_Engineer';
@@ -108,20 +108,20 @@ REVOKE ALL ON sys.* FROM 'trainer1'@'localhost';*/
 /*Deployment END*/
 
 /*Environment BEGIN*/
-GRANT SELECT ON EPF_Delivery_Processes.* TO 'Process_Engineer'@'localhost';
-GRANT SELECT ON EPF_Delivery_Processes.* TO 'Tool_Specialist'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'Process_Engineer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'Tool_Specialist'@'localhost';
 
-GRANT SELECT ON EPF_Roles.* TO 'Process_Engineer'@'localhost';
-GRANT SELECT ON EPF_Roles.* TO 'Tool_Specialist'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'Process_Engineer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'Tool_Specialist'@'localhost';
 
-GRANT SELECT ON EPF_Tasks.* TO 'Process_Engineer'@'localhost';
-GRANT SELECT ON EPF_Tasks.* TO 'Tool_Specialist'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'Process_Engineer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'Tool_Specialist'@'localhost';
 
-GRANT SELECT ON EPF_Work_Products.* TO 'Process_Engineer'@'localhost';
-GRANT SELECT ON EPF_Work_Products.* TO 'Tool_Specialist'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'Process_Engineer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'Tool_Specialist'@'localhost';
 
-GRANT SELECT ON OPENUP.* TO 'Process_Engineer'@'localhost';
-GRANT SELECT ON OPENUP.* TO 'Tool_Specialist'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'Process_Engineer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'Tool_Specialist'@'localhost';
 
 /*REVOKE ALL ON sys.* FROM 'Process_Engineer';
 REVOKE ALL ON sys.* FROM 'Tool_Specialist';
--- OpenUP/EPF-persistence-schema/src/main/resources/META-INF/mysql/7.grant_tables.sql
+++ OpenUP/EPF-persistence-schema/src/main/resources/META-INF/mysql/7.grant_tables.sql
@@ -1,3 +1,95 @@
-GRANT ALL ON OPENUP.OPENUP_ARTIFACT TO 'Any_Role'@'localhost';
-GRANT ALL ON OPENUP.OPENUP_TASK TO 'Any_Role'@'localhost';
-GRANT SELECT ON OPENUP.OPENUP_ROLE TO 'Any_Role'@'localhost';
+/*Basic Roles BEGIN*/
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'analyst1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'any_role1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'architect1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'developer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'project_manager1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'stakeholder1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'tester1'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'analyst1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'any_role1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'architect1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'developer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'project_manager1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'stakeholder1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'tester1'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'analyst1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'any_role1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'architect1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'developer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'project_manager1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'stakeholder1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'tester1'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'analyst1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'any_role1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'architect1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'developer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'project_manager1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'stakeholder1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'tester1'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'analyst1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'any_role1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'architect1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'developer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'project_manager1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'stakeholder1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'tester1'@'localhost';
+/*Basic Roles END*/
+
+/*Deployment BEGIN*/
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'course_developer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'deployment_engineer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'deployment_manager1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'product_owner1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'technical_writer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'trainer1'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'course_developer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'deployment_engineer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'deployment_manager1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'product_owner1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'technical_writer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'trainer1'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'course_developer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'deployment_engineer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'deployment_manager1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'product_owner1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'technical_writer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'trainer1'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'course_developer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'deployment_engineer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'deployment_manager1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'product_owner1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'technical_writer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'trainer1'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'course_developer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'deployment_engineer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'deployment_manager1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'product_owner1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'technical_writer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'trainer1'@'localhost';
+/*Deployment END*/
+
+/*Environment BEGIN*/
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'process_engineer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Delivery_Processes.* TO 'tool_specialist1'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'process_engineer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Roles.* TO 'tool_specialist1'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'process_engineer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Tasks.* TO 'tool_specialist1'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'process_engineer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON EPF_Work_Products.* TO 'tool_specialist1'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'process_engineer1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON OPENUP.* TO 'tool_specialist1'@'localhost';
+/*Environment END*/
