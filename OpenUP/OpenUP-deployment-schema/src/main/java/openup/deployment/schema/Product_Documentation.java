476f9590f3906ddfe300479e25e53a7320d8fb28
kimduquan <kimduquan@gmail.com>
2021-12-21 23:42:04 +0700

    #

---
+++ OpenUP/OpenUP-deployment-schema/src/main/java/openup/deployment/schema/Product_Documentation.java
@@ -0,0 +1,34 @@
+package openup.deployment.schema;
+
+/**
+ * @author PC
+ *
+ */
+public class Product_Documentation {
+
+	/**
+	 * 
+	 */
+	private String responsiblep;
+	
+	/**
+	 * 
+	 */
+	private String modifiedBy;
+
+	public String getResponsiblep() {
+		return responsiblep;
+	}
+
+	public void setResponsiblep(final String responsiblep) {
+		this.responsiblep = responsiblep;
+	}
+
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+
+	public void setModifiedBy(final String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+}
