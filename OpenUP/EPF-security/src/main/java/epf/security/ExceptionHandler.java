cbb6dfe8ffdf65a8430514db85d4c8d0e0339688
kimduquan <kimduquan@gmail.com>
2021-12-24 11:09:30 +0700

    #

--- OpenUP/EPF-security/src/main/java/epf/security/ExceptionHandler.java
+++ OpenUP/EPF-security/src/main/java/epf/security/ExceptionHandler.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package epf.security;
 
 import java.io.Serializable;
@@ -38,6 +33,7 @@ public class ExceptionHandler implements ExceptionMapper<Exception>, Serializabl
      */
     @Override
     public Response toResponse(final Exception exception) {
+    	exception.printStackTrace();
         return handle(exception);
     }
     
