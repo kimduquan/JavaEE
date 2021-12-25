dc575253522186e37da128a2fdc32d8f21cc2d6b
kimduquan <kimduquan@gmail.com>
2021-12-25 11:45:04 +0700

    #

--- OpenUP/schema.sh
+++
@@ -1,6 +0,0 @@
-cd EPF-persistence-schema
-mvn clean install
-jshell ./delete_db.jsh
-cd ../
-cp EPF-persistence-schema/target/EPF-persistence-schema-1.0.0.war EPF-tests/target/servers/test/dropins/
-./dev.sh
