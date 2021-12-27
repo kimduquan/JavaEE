999218df42bf3606e7da162783f4b361c6b454c0
kimduquan <kimduquan@gmail.com>
2021-12-27 13:36:38 +0700

    #

--- OpenUP/startup.sh
+++ OpenUP/startup.sh
@@ -1,7 +1,8 @@
 ~/kafka_2.13-2.8.1/bin/zookeeper-server-start.sh ~/kafka_2.13-2.8.1/config/zookeeper.properties &
 export JAVA_HOME=~/jdk8u312-b07
 ~/pluto-3.1.0/bin/startup.sh
-jshell ./EPF-shell/target/appassembler/start.jsh -R-Dasync -R-Dargs="~/payara5/glassfish/bin/startserv"
+#jshell ./EPF-shell/target/appassembler/start.jsh -R-Dasync -R-Dargs="/payara5/glassfish/bin/startserv"
+~/payara5/glassfish/bin/startserv &
 export JAVA_HOME=~/jdk-11.0.13+8
 ~/jaeger-1.24.0-linux-amd64/jaeger-all-in-one &
 Xvfb :10 -ac &
