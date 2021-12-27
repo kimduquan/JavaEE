2ce8aad7e885baab17f8f53e3d3702993ecb6730
kimduquan <kimduquan@gmail.com>
2021-12-27 10:29:23 +0700

    #

--- OpenUP/shutdown.bat
+++ OpenUP/shutdown.bat
@@ -5,6 +5,8 @@ set kafka_dir="C:\kafka_2.13-2.8.1\bin\windows"
 cd %kafka_dir%
 call .\kafka-server-stop.bat
 Taskkill /IM jaeger-all-in-one.exe /F
+Taskkill /IM geckodriver.exe /F
+Taskkill /IM firefox.exe /F
 set JAVA_HOME=C:\jdk8u312-b07
 set portal_dir="C:\Program Files\pluto-3.1.0\bin\"
 cd %portal_dir%
--- OpenUP/shutdown.sh
+++ OpenUP/shutdown.sh
@@ -1,5 +1,7 @@
 ~/kafka_2.13-2.8.1/bin/kafka-server-stop.sh &
 killall jaeger-all-in-one
+killall geckodriver
+killall firefox
 killall Xvfb
 export JAVA_HOME=~/jdk8u312-b07
 ~/pluto-3.1.0/bin/shutdown.sh
