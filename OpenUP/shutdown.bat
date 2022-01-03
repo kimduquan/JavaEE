71f039462f7c6355e3222d52571237f3c9867f1c
kimduquan <kimduquan@gmail.com>
2022-01-03 13:54:14 +0700

    #

--- OpenUP/shutdown.bat
+++ OpenUP/shutdown.bat
@@ -3,20 +3,20 @@ set cur_dir=%CD%
 set cur_java=$JAVA_HOME
 set kafka_dir="C:\kafka_2.13-2.8.1\bin\windows"
 cd %kafka_dir%
-call .\kafka-server-stop.bat
+call .\kafka-server-stop.bat &
 Taskkill /IM jaeger-all-in-one.exe /F
 Taskkill /IM geckodriver.exe /F
 Taskkill /IM firefox.exe /F
 set JAVA_HOME=C:\jdk8u312-b07
 set portal_dir="C:\Program Files\pluto-3.1.0\bin\"
 cd %portal_dir%
-call .\shutdown.bat
+call .\shutdown.bat &
 cd %cur_dir%
 set payara_dir="C:\payara5\glassfish\bin\"
 cd %payara_dir%
-call .\stopserv.bat
+call .\stopserv.bat &
 cd %cur_dir%
 cd %kafka_dir%
-call .\zookeeper-server-stop.bat
+call .\zookeeper-server-stop.bat &
 cd %cur_dir%
 endlocal
--- OpenUP/startup.bat
+++ OpenUP/startup.bat
@@ -4,22 +4,22 @@ set cur_dir=%CD%
 set cur_java=$JAVA_HOME
 set kafka_dir="C:\kafka_2.13-2.8.1\bin\windows"
 cd %kafka_dir%
-start .\zookeeper-server-start.bat ..\..\config\zookeeper.properties
+start .\zookeeper-server-start.bat ..\..\config\zookeeper.properties &
 cd %cur_dir%
 set jaeger_dir="C:\jaeger-1.27.0-windows-amd64"
 cd %jaeger_dir%
-start .\jaeger-all-in-one.exe
+start .\jaeger-all-in-one.exe &
 cd %cur_dir%
 set JAVA_HOME=C:\jdk8u312-b07
 set portal_dir="C:\Program Files\pluto-3.1.0\bin\"
 cd %portal_dir%
-call .\startup.bat
+call .\startup.bat &
 cd %cur_dir%
 set payara_dir="C:\payara5\glassfish\bin\"
 cd %payara_dir%
-start .\startserv.bat --debug
+start .\startserv.bat --debug &
 cd %cur_dir%
 cd %kafka_dir%
-start .\kafka-server-start.bat ..\..\config\server.properties
+start .\kafka-server-start.bat ..\..\config\server.properties &
 cd %cur_dir%
 endlocal
