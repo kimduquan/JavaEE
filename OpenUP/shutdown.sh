51c90b1ad5dfa2727002df020c02f2757007cd98
kimduquan <kimduquan@gmail.com>
2022-01-03 18:38:53 +0700

    #

--- OpenUP/shutdown.sh
+++ OpenUP/shutdown.sh
@@ -4,6 +4,6 @@ killall geckodriver
 killall /usr/lib/firefox/firefox
 killall Xvfb
 export JAVA_HOME=~/jdk8u312-b07
-~/pluto-3.1.0/bin/shutdown.sh
+~/pluto-3.1.0/bin/shutdown.sh &
 ~/payara5/glassfish/bin/stopserv &
-~/kafka_2.13-2.8.1/bin/zookeeper-server-stop.sh
+~/kafka_2.13-2.8.1/bin/zookeeper-server-stop.sh &
