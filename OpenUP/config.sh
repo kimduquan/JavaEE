e8a571b5e20017de84cc0a07535ece2000d28f3e
kimduquan <kimduquan@gmail.com>
2022-01-21 20:02:44 +0700

    #

--- OpenUP/config.sh
+++ OpenUP/config.sh
@@ -1,5 +1,5 @@
 export GRAALVM_HOME=~/graalvm-ce-java11-21.3.0
-export PATH=$GRAALVM_HOME/bin:$PATH
+export PATH=~/graalvm-ce-java11-21.3.0/bin:$PATH
 export PATH=~/jdk-11.0.13+8/bin:$PATH
 export JAVA_HOME=~/jdk-11.0.13+8
 export epf_gateway_url=https://localhost:9443/gateway/
