396cad626f4c6f9449701170a1c1f27dd6e1be70
kimduquan <kimduquan@gmail.com>
2022-01-21 21:01:08 +0700

    #

--- OpenUP/dev.bat
+++ OpenUP/dev.bat
@@ -5,5 +5,6 @@ cd EPF-shell
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 call mvn clean install -Depf-shell-native
 cd ../
+call ./install.bat
 cd EPF-tests
 call mvn liberty:dev
--- OpenUP/dev.sh
+++ OpenUP/dev.sh
@@ -4,5 +4,6 @@
 cd EPF-shell
 mvn clean install -Depf-shell-native
 cd ../
+./install.sh
 cd EPF-tests
 mvn liberty:dev
