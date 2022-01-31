a49bacad47b8f15052a666b0a868c2b341bb09fc
kimduquan <kimduquan@gmail.com>
2022-02-01 04:24:32 +0700

    #

--- OpenUP/startup.sh
+++ OpenUP/startup.sh
@@ -1,7 +1,7 @@
 . ./env.sh
 . ./config.sh
 $KAFKA_HOME/zookeeper-server-start.sh $KAFKA_DIR/config/zookeeper.properties &
-$JAEGER_HOME$/jaeger-all-in-one &
+$JAEGER_HOME/jaeger-all-in-one &
 export JAVA_HOME=$JAVA8_HOME
 $PLUTO_HOME/bin/startup.sh &
 $WILDFLY_HOME/standalone.sh "-Djboss.http.port=80" "-Djboss.https.port=443" --debug &
