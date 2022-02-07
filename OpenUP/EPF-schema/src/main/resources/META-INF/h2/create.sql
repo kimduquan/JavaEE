ae861a70c23379c1bf9291616f8356b555eeb17b
kimduquan <kimduquan@gmail.com>
2022-02-07 13:06:46 +0700

    #

--- OpenUP/EPF-schema/src/main/resources/META-INF/h2/create.sql
+++ OpenUP/EPF-schema/src/main/resources/META-INF/h2/create.sql
@@ -1,6 +1,5 @@
 CREATE SCHEMA IF NOT EXISTS EPF_Security;
 
-
 /*Basic Roles BEGIN*/
 CREATE ROLE IF NOT EXISTS Analyst;
 CREATE ROLE IF NOT EXISTS Any_Role;
@@ -48,54 +47,4 @@ CREATE USER IF NOT EXISTS trainer1 PASSWORD 'Trainer1*';
 /*Environment BEGIN*/
 CREATE USER IF NOT EXISTS process_engineer1 PASSWORD 'Process_Engineer1*';
 CREATE USER IF NOT EXISTS tool_specialist1 PASSWORD 'Tool_Specialist1*';
-/*Environment END*/
-
-
-/*Basic Roles BEGIN*/
-GRANT Any_Role TO analyst1;
-GRANT Any_Role TO any_role1;
-GRANT Any_Role TO architect1;
-GRANT Any_Role TO developer1;
-GRANT Any_Role TO project_manager1;
-GRANT Any_Role TO stakeholder1;
-GRANT Any_Role TO tester1;
-/*Basic Roles END*/
-
-/*Deployment BEGIN*/
-GRANT Any_Role TO course_developer1;
-GRANT Any_Role TO deployment_engineer1;
-GRANT Any_Role TO deployment_manager1;
-GRANT Any_Role TO product_owner1;
-GRANT Any_Role TO technical_writer1;
-GRANT Any_Role TO trainer1;
-/*Deployment END*/
-
-/*Environment BEGIN*/
-GRANT Any_Role TO process_engineer1;
-GRANT Any_Role TO tool_specialist1;
-/*Environment END*/
-
-
-/*Basic Roles BEGIN*/
-GRANT Analyst TO analyst1;
-GRANT Any_Role TO any_role1;
-GRANT Architect TO architect1;
-GRANT Developer TO developer1;
-GRANT Project_Manager TO project_manager1;
-GRANT Stakeholder TO stakeholder1;
-GRANT Tester TO tester1;
-/*Basic Roles END*/
-
-/*Deployment BEGIN*/
-GRANT Course_Developer TO course_developer1;
-GRANT Deployment_Engineer TO deployment_engineer1;
-GRANT Deployment_Manager TO deployment_manager1;
-GRANT Product_Owner TO product_owner1;
-GRANT Technical_Writer TO technical_writer1;
-GRANT Trainer TO trainer1;
-/*Deployment END*/
-
-/*Environment BEGIN*/
-GRANT Process_Engineer TO process_engineer1;
-GRANT Tool_Specialist TO tool_specialist1;
 /*Environment END*/
