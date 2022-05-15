dfddae39b31fbfb4dd047242816d1232e3243d33
kimduquan <kimduquan@gmail.com>
2022-05-15 08:50:34 +0700

    #

--- OpenUP/config.sh
+++ OpenUP/config.sh
@@ -1,8 +1,8 @@
 export epf_gateway_url=https://localhost:9543/gateway/
 export epf_webapp_url=https://localhost/webapp
-export epf_client_net_ssl_keyStore=~/dev.p12
+export epf_client_net_ssl_keyStore=~/git/JavaEE/OpenUP/dev.p12
 export epf_client_net_ssl_keyStoreType=PKCS12
 export epf_client_net_ssl_keyStorePassword=changeit
-export epf_client_net_ssl_trustStore=~/dev.p12
+export epf_client_net_ssl_trustStore=~/git/JavaEE/OpenUP/dev.p12
 export epf_client_net_ssl_trustStoreType=PKCS12
 export epf_client_net_ssl_trustStorePassword=changeit
