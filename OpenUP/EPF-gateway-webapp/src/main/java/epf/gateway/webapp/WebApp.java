27e6002c4c9f01cb5189738be1a0b53f5c2dd88e
kimduquan <kimduquan@gmail.com>
2022-06-30 22:16:55 +0700

    #

---
+++ OpenUP/EPF-gateway-webapp/src/main/java/epf/gateway/webapp/WebApp.java
@@ -0,0 +1,14 @@
+package epf.gateway.webapp;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+/**
+ * @author PC
+ *
+ */
+@ApplicationScoped
+@FacesConfig
+public class WebApp {
+	
+}
