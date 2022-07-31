40b0d2b5a8feea0c62ca94088b347008f883b550
kimduquan <kimduquan@gmail.com>
2022-07-31 17:58:10 +0700

    #

--- OpenUP/dev.bat
+++ OpenUP/dev.bat
@@ -3,21 +3,21 @@ call ./shutdown.bat
 call ./clean.bat
 call ./startup.bat
 call mvn clean install -U -DskipTests -T 1C
-cd EPF-transaction-internal
-start dev.bat &
-cd ../
-cd EPF-gateway
-start dev.bat &
-cd ../
 cd EPF-persistence
+start dev.bat &
+cd ../
+cd EPF-transaction-internal
 start dev.bat &
 cd ../
 cd EPF-transaction
 start dev.bat &
 cd ../
+cd EPF-gateway
+start dev.bat &
+cd ../
+call ./install.bat &
 cd EPF-shell
 call dev.bat
 cd ../
-call ./install.bat &
 cd EPF-tests
 call mvn liberty:dev
--- OpenUP/dev.sh
+++ OpenUP/dev.sh
@@ -6,21 +6,21 @@ sudo service postgresql start
 ./startup.sh
 . ./config.sh
 mvn clean install -U -DskipTests -T 1C
-cd EPF-transaction-internal
-./dev.sh &>/dev/null &
-cd ../
-cd EPF-gateway
-./dev.sh &>/dev/null &
-cd ../
 cd EPF-persistence
+./dev.sh &>/dev/null &
+cd ../
+cd EPF-transaction-internal
 ./dev.sh &>/dev/null &
 cd ../
 cd EPF-transaction
 ./dev.sh &>/dev/null &
 cd ../
-cd EPF-shell
+cd EPF-gateway
 ./dev.sh &>/dev/null &
 cd ../
 ./install.sh &
+cd EPF-shell
+./dev.sh &>/dev/null &
+cd ../
 cd EPF-tests
 mvn liberty:dev
