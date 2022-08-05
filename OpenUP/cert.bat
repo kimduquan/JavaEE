3d213423fc608a8f18cd66a266e8676f4004d904
kimduquan <kimduquan@gmail.com>
2022-08-06 01:13:52 +0700

    #

--- OpenUP/cert.bat
+++ OpenUP/cert.bat
@@ -1,3 +1,4 @@
+setlocal
 call ./env.bat
 keytool -genkeypair -alias localhost -keyalg RSA -keysize 2048 -ext san=ip:127.0.0.1,dns:localhost -validity 365 -keypass changeit -keystore dev.p12 -storepass changeit -storetype PKCS12
 keytool -exportcert -rfc -alias localhost -file dev.pem -keystore dev.p12 -storepass changeit -storetype PKCS12
@@ -7,3 +8,4 @@ keytool -importcert -noprompt -alias oauth2.googleapis.com -file cert/oauth2.goo
 keytool -importcert -noprompt -alias openidconnect.googleapis.com -file cert/openidconnect.googleapis.com.cer -keystore dev.p12 -storepass changeit -storetype PKCS12
 keytool -importcert -noprompt -alias www.facebook.com -file cert/www.facebook.com.cer -keystore dev.p12 -storepass changeit -storetype PKCS12
 keytool -importcert -noprompt -alias www.googleapis.com -file cert/www.googleapis.com.cer -keystore dev.p12 -storepass changeit -storetype PKCS12
+endlocal
