3ea4a350831d0c82b3ccc68044db9715db32a0e1
kimduquan <kimduquan@gmail.com>
2022-08-18 12:24:22 +0700

    #

--- OpenUP/EPF-security-management/src/main/java/epf/security/management/internal/ManagementIdentityStore.java
+++ OpenUP/EPF-security-management/src/main/java/epf/security/management/internal/ManagementIdentityStore.java
@@ -14,9 +14,7 @@ import javax.security.enterprise.CallerPrincipal;
 import javax.transaction.Transactional;
 import org.eclipse.microprofile.context.ManagedExecutor;
 import epf.naming.Naming;
-import epf.schema.utility.TenantUtil;
 import epf.security.internal.JPAPrincipal;
-import epf.security.schema.Security;
 import epf.security.util.Credential;
 
 /**
@@ -43,8 +41,6 @@ public class ManagementIdentityStore {
 		Objects.requireNonNull(credential, "Credential");
 		Objects.requireNonNull(credential.getCaller(), "Credential.caller");
 		Objects.requireNonNull(credential.getPassword(), "Credential.password");
-		final String tenant = TenantUtil.getTenantId(Security.SCHEMA, credential.getTenant().orElse(null));
-		manager.setProperty(Naming.Management.MANAGEMENT_TENANT, tenant);
 		final Query query = manager.createNativeQuery(String.format(NativeQueries.CREATE_USER, credential.getCaller()));
 		query.setParameter(1, new String(credential.getPassword().getValue()));
 		query.executeUpdate();
@@ -55,14 +51,10 @@ public class ManagementIdentityStore {
 		final Map<String, Object> props = new ConcurrentHashMap<>();
         props.put(Naming.Persistence.JDBC.JDBC_USER, credential.getCaller());
         props.put(Naming.Persistence.JDBC.JDBC_PASSWORD, String.valueOf(credential.getPassword().getValue()));
-        final String tenant = TenantUtil.getTenantId(Security.SCHEMA, credential.getTenant().orElse(null));
-        props.put(Naming.Management.MANAGEMENT_TENANT, tenant);
         return executor.supplyAsync(() -> Persistence.createEntityManagerFactory(Naming.Security.Internal.SECURITY_MANAGEMENT_UNIT_NAME, props))
         		.thenApply(factory -> {
         			try {
-        				final Map<String, Object> newProps = new ConcurrentHashMap<>();
-        				newProps.put(Naming.Management.MANAGEMENT_TENANT, tenant);
-        				final EntityManager manager = factory.createEntityManager(newProps);
+        				final EntityManager manager = factory.createEntityManager();
         				final JPAPrincipal principal = new JPAPrincipal(credential.getTenant(), credential.getCaller(), factory, manager);
         				return principal;
 		        	}
@@ -76,10 +68,7 @@ public class ManagementIdentityStore {
 	@Transactional
 	public CompletionStage<Void> setCallerGroup(final CallerPrincipal callerPrincipal, final String group) throws Exception {
 		Objects.requireNonNull(callerPrincipal, "CallerPrincipal");
-		final JPAPrincipal principal = (JPAPrincipal) callerPrincipal;
-		final String tenant = TenantUtil.getTenantId(Security.SCHEMA, principal.getTenant().orElse(null));
-		manager.setProperty(Naming.Management.MANAGEMENT_TENANT, tenant);
-		final Query query = manager.createNativeQuery(String.format(NativeQueries.SET_ROLE, group, principal.getName()));
+		final Query query = manager.createNativeQuery(String.format(NativeQueries.SET_ROLE, group, callerPrincipal.getName()));
 		query.executeUpdate();
 		return executor.completedStage(null);
 	}
@@ -89,8 +78,6 @@ public class ManagementIdentityStore {
 		Objects.requireNonNull(credential, "Credential");
 		Objects.requireNonNull(credential.getCaller(), "Credential.caller");
 		Objects.requireNonNull(credential.getPassword(), "Credential.password");
-		final String tenant = TenantUtil.getTenantId(Security.SCHEMA, credential.getTenant().orElse(null));
-		manager.setProperty(Naming.Management.MANAGEMENT_TENANT, tenant);
 		manager.createNativeQuery(String.format(NativeQueries.SET_USER_PASSWORD, credential.getCaller())).setParameter(1, new String(credential.getPassword().getValue())).executeUpdate();
 		return executor.completedStage(null);
 	}
@@ -98,8 +85,6 @@ public class ManagementIdentityStore {
 	public CompletionStage<Boolean> isCaller(final Credential credential) throws Exception {
 		Objects.requireNonNull(credential, "Credential");
 		Objects.requireNonNull(credential.getCaller(), "Credential.caller");
-		final String tenant = TenantUtil.getTenantId(Security.SCHEMA, credential.getTenant().orElse(null));
-		manager.setProperty(Naming.Management.MANAGEMENT_TENANT, tenant);
 		final Long count = (Long) manager.createNativeQuery(NativeQueries.CHECK_USER).setParameter(1, credential.getCaller()).getSingleResult();
 		return executor.completedStage(count > 0);
 	}
