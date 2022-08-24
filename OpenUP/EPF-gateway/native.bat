1f916e01fdc577237d89344efbfcdaea16c73bdb
kimduquan <kimduquan@gmail.com>
2022-08-24 08:26:13 +0700

    #

--- OpenUP/EPF-gateway/native.bat
+++ OpenUP/EPF-gateway/native.bat
@@ -1,9 +1,9 @@
 del .\EPF-gateway.log.*
 setlocal
 call ../env.bat
+call ../native_env.bat
 call ../config.bat
 call config.bat
-java -version
 call mvn clean install -Depf-gateway-native -U
-call .\target\EPF-gateway-1.0.0-runner.exe
+.\target\EPF-gateway-1.0.0-runner.exe
 endlocal
