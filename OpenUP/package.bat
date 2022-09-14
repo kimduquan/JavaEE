1034bf004758d16d7ca1940eacb1bd03202b0ea5
kimduquan <kimduquan@gmail.com>
2022-09-14 23:23:05 +0700

    #

--- OpenUP/package.bat
+++ OpenUP/package.bat
@@ -2,8 +2,8 @@ call git pull
 call .\env.bat
 call .\config.bat
 call .\clean.bat
-mvn clean package
-docker pull icr.io/appcafe/open-liberty:full-java11-openj9-ubi
-docker build -t openup:1.0.0 ./
-kubectl apply -f deploy.yaml
-kubectl get OpenLibertyApplications
+call mvn clean package
+call docker pull icr.io/appcafe/open-liberty:full-java11-openj9-ubi
+call docker build -t openup:1.0.0 EPF-assembly/.
+call kubectl apply -f deploy.yaml
+call kubectl get OpenLibertyApplications
