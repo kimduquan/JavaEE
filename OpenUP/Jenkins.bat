81e30f5fd3f030c00290608efd38d04dd61a01e0
kimduquan <kimduquan@gmail.com>
2022-09-15 21:09:06 +0700

    #

--- OpenUP/Jenkins.bat
+++ OpenUP/Jenkins.bat
@@ -1,3 +1,5 @@
-kubectl apply -f https://raw.githubusercontent.com/jenkinsci/kubernetes-operator/master/config/crd/bases/jenkins.io_jenkins.yaml
-kubectl apply -f https://raw.githubusercontent.com/jenkinsci/kubernetes-operator/master/deploy/all-in-one-v1alpha2.yaml
-kubectl get pods -w
+call kubectl apply -f https://raw.githubusercontent.com/jenkinsci/kubernetes-operator/master/config/crd/bases/jenkins.io_jenkins.yaml
+call kubectl apply -f https://raw.githubusercontent.com/jenkinsci/kubernetes-operator/master/deploy/all-in-one-v1alpha2.yaml
+call kubectl create namespace jenkins
+call helm repo add jenkins https://raw.githubusercontent.com/jenkinsci/kubernetes-operator/master/chart
+call helm install jenkins jenkins/jenkins-operator -n jenkins
