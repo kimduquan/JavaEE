bff49579f45479f89a74c5bd7c5955e00249277d
kimduquan <kimduquan@gmail.com>
2022-09-19 00:40:34 +0700

    #

--- OpenUP/EPF-tests/dev.bat
+++ OpenUP/EPF-tests/dev.bat
@@ -2,7 +2,7 @@ copy "C:\Users\PC\EPF-query.trace.db" EPF-query.trace.db
 copy "C:\Users\PC\EPF-security.mv.db" EPF-security.mv.db
 copy "C:\Users\PC\EPF-security.trace.db" EPF-security.trace.db
 copy "C:\Users\PC\EPF-query.mv.db" EPF-query.mv.db
-copy ../dev.p12 dev.p12
+copy "./target/servers/test/apps" "./target/.libertyDevc/apps/"
 setlocal
 call ../env.bat
 call ../config.bat
