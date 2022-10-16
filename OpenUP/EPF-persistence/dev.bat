a0e3a764b1cedf3ebd5672ec58daa7b25d5fba4b
kimduquan <kimduquan@gmail.com>
2022-10-16 15:20:48 +0700

    #

--- OpenUP/EPF-persistence/dev.bat
+++ OpenUP/EPF-persistence/dev.bat
@@ -2,5 +2,8 @@ del .\EPF-persistence.log.*
 setlocal
 call ../env.bat
 call config.bat
-call mvn quarkus:dev -Ddebug=5007
+:: call mvn quarkus:dev -Ddebug=5007
+call mvn clean install -U -Dquarkus.container-image.build=true
+call kubectl delete -f target/kubernetes/kubernetes.yml
+call kubectl apply -f target/kubernetes/kubernetes.yml
 endlocal
--- OpenUP/EPF-transaction-internal/dev.bat
+++ OpenUP/EPF-transaction-internal/dev.bat
@@ -3,5 +3,8 @@ rmdir "ObjectStore" /s /q
 setlocal
 call ../env.bat
 call config.bat
-java -jar ./target/quarkus-app/quarkus-run.jar
+:: java -jar ./target/quarkus-app/quarkus-run.jar
+call mvn clean install -U -Dquarkus.container-image.build=true
+call kubectl delete -f target/kubernetes/kubernetes.yml
+call kubectl apply -f target/kubernetes/kubernetes.yml
 endlocal
