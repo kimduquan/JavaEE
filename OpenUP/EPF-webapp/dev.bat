a1d87d7120f1b32a3e2463237b2b54a07898d55a
kimduquan <kimduquan@gmail.com>
2022-10-23 17:15:26 +0700

    #

--- OpenUP/EPF-webapp/dev.bat
+++ OpenUP/EPF-webapp/dev.bat
@@ -2,4 +2,7 @@ setlocal
 call ../env.bat
 call ../config.bat
 call mvn clean install -U
+docker build -t wildfly:1.0.0 ./
+kubectl delete -f wildfly.yml
+kubectl apply -f wildfly.yml
 endlocal
--- OpenUP/Wildfly/dev.bat
+++
@@ -1,3 +0,0 @@
-docker build -t wildfly:1.0.0 ./
-kubectl delete -f wildfly.yml
-kubectl apply -f wildfly.yml
