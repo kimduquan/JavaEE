20105d8c43f0968de7567a74783beed112ca4e4d
kimduquan <kimduquan@gmail.com>
2022-10-23 10:05:38 +0700

    #

--- OpenUP/EPF-gateway/dev.bat
+++ OpenUP/EPF-gateway/dev.bat
@@ -6,5 +6,4 @@ call ../env.bat
 call mvn clean install -U -Dquarkus.container-image.build=true
 call kubectl delete -f target/kubernetes/kubernetes.yml
 call kubectl apply -f target/kubernetes/kubernetes.yml
-kubectl port-forward svc/epf-gateway 9543:9543
 endlocal
