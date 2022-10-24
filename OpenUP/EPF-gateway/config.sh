9e055322bdb89466c7d9337c215edcca18efd716
kimduquan <kimduquan@gmail.com>
2022-10-24 19:45:23 +0700

    #

--- OpenUP/EPF-gateway/config.sh
+++ OpenUP/EPF-gateway/config.sh
@@ -1,6 +1,6 @@
 export EPF_REGISTRY_URL=http://localhost:9080/registry/registry/
-export MP_JWT_VERIFY_AUDIENCES=http://localhost:8080/,https://localhost:9543/
-export QUARKUS_HTTP_SSL_PORT=9543
+export MP_JWT_VERIFY_AUDIENCES=http://localhost:8080/,https://localhost:8443/
+export QUARKUS_HTTP_SSL_PORT=8443
 export QUARKUS_HTTP_INSECURE_REQUESTS=disabled
 export QUARKUS_LOG_FILE_ENABLE=true
 export QUARKUS_LOG_FILE_PATH=EPF-gateway.log
