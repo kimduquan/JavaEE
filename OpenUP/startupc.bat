01a2885cb92e11ff62f830efdbba692854447e54
kimduquan <kimduquan@gmail.com>
2022-10-26 01:36:21 +0700

    #

--- OpenUP/startupc.bat
+++ OpenUP/startupc.bat
@@ -30,6 +30,7 @@ cd ../
 
 cd EPF-tests
 kubectl apply -f ./openup.yaml
+kubectl wait deployment --for condition=available openup --timeout=300s
 kubectl wait pod --for condition=ready -l name=openup --timeout=300s
 cd ../
 
