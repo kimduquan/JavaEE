2dde520064b7eb071a4ba55758fc98c7a83efeb1
kimduquan <kimduquan@gmail.com>
2022-10-26 01:28:22 +0700

    #

--- OpenUP/shutdownc.bat
+++ OpenUP/shutdownc.bat
@@ -10,33 +10,33 @@ Taskkill /IM java.exe /F
 
 Taskkill /IM kubectl.exe /F
 cd EPF-webapp
-call kubectl delete -f ./epf-webapp.yaml
-call kubectl wait --for=delete -f ./epf-webapp.yaml
+kubectl delete -f ./epf-webapp.yaml
+kubectl wait --for=delete -f ./epf-webapp.yaml
 cd ../
 cd EPF-gateway
-call kubectl delete -f ./epf-gateway.yaml
-call kubectl wait --for=delete -f ./epf-gateway.yaml
+kubectl delete -f ./epf-gateway.yaml
+kubectl wait --for=delete -f ./epf-gateway.yaml
 cd ../
 cd EPF-tests
-call kubectl delete -f ./openup.yaml
-call kubectl wait --for=delete -f ./openup.yaml
+kubectl delete -f ./openup.yaml
+kubectl wait --for=delete -f ./openup.yaml
 cd ../
 cd EPF-persistence
-call kubectl delete -f target/kubernetes/kubernetes.yml
-call kubectl wait --for=delete -f target/kubernetes/kubernetes.yml
+kubectl delete -f target/kubernetes/kubernetes.yml
+kubectl wait --for=delete -f target/kubernetes/kubernetes.yml
 cd ../
 cd EPF-transaction
-call kubectl delete -f target/kubernetes/kubernetes.yml
-call kubectl wait --for=delete -f target/kubernetes/kubernetes.yml
+kubectl delete -f target/kubernetes/kubernetes.yml
+kubectl wait --for=delete -f target/kubernetes/kubernetes.yml
 cd ../
 cd EPF-transaction-internal
-call kubectl delete -f target/kubernetes/kubernetes.yml
-call kubectl wait --for=delete -f target/kubernetes/kubernetes.yml
+kubectl delete -f target/kubernetes/kubernetes.yml
+kubectl wait --for=delete -f target/kubernetes/kubernetes.yml
 cd ../
-call kubectl delete -f ./kafka.yaml
-call kubectl wait --for=delete -f ./kafka.yaml
-call kubectl delete -f ./zookeeper.yaml
-call kubectl wait --for=delete -f ./zookeeper.yaml
-call kubectl delete -f ./postgresql.yaml
-call kubectl wait --for=delete -f ./postgresql.yaml
+kubectl delete -f ./kafka.yaml
+kubectl wait --for=delete -f ./kafka.yaml
+kubectl delete -f ./zookeeper.yaml
+kubectl wait --for=delete -f ./zookeeper.yaml
+kubectl delete -f ./postgresql.yaml
+kubectl wait --for=delete -f ./postgresql.yaml
 endlocal
--- OpenUP/startupc.bat
+++ OpenUP/startupc.bat
@@ -1,48 +1,48 @@
-call kubectl apply -f ./postgresql.yaml
-call kubectl wait deployment --for condition=available postgresql
-call kubectl wait pod --for condition=ready -l app.kubernetes.io/name=postgresql
+kubectl apply -f ./postgresql.yaml
+kubectl wait deployment --for condition=available postgresql
+kubectl wait pod --for condition=ready -l app.kubernetes.io/name=postgresql
 
-call kubectl apply -f ./zookeeper.yaml
-call kubectl wait deployment --for condition=available zookeeper
-call kubectl wait pod --for condition=ready -l app.kubernetes.io/name=zookeeper
+kubectl apply -f ./zookeeper.yaml
+kubectl wait deployment --for condition=available zookeeper
+kubectl wait pod --for condition=ready -l app.kubernetes.io/name=zookeeper
 
-call kubectl apply -f ./kafka.yaml
-call kubectl wait deployment --for condition=available kafka
-call kubectl wait pod --for condition=ready -l app.kubernetes.io/name=kafka
+kubectl apply -f ./kafka.yaml
+kubectl wait deployment --for condition=available kafka
+kubectl wait pod --for condition=ready -l app.kubernetes.io/name=kafka
 
 cd EPF-transaction-internal
-call kubectl apply -f target/kubernetes/kubernetes.yml
-call kubectl wait deployment --for condition=available epf-transaction-internal
-call kubectl wait pod --for condition=ready -l app.kubernetes.io/name=epf-transaction-internal
+kubectl apply -f target/kubernetes/kubernetes.yml
+kubectl wait deployment --for condition=available epf-transaction-internal
+kubectl wait pod --for condition=ready -l app.kubernetes.io/name=epf-transaction-internal
 cd ../
 
 cd EPF-transaction
-call kubectl apply -f target/kubernetes/kubernetes.yml
-call kubectl wait deployment --for condition=available epf-transaction
-call kubectl wait pod --for condition=ready -l app.kubernetes.io/name=epf-transaction
+kubectl apply -f target/kubernetes/kubernetes.yml
+kubectl wait deployment --for condition=available epf-transaction
+kubectl wait pod --for condition=ready -l app.kubernetes.io/name=epf-transaction
 cd ../
 
 cd EPF-persistence
-call kubectl apply -f target/kubernetes/kubernetes.yml
-call kubectl wait deployment --for condition=available epf-persistence
-call kubectl wait pod --for condition=ready -l app.kubernetes.io/name=epf-persistence
+kubectl apply -f target/kubernetes/kubernetes.yml
+kubectl wait deployment --for condition=available epf-persistence
+kubectl wait pod --for condition=ready -l app.kubernetes.io/name=epf-persistence
 cd ../
 
 cd EPF-tests
-call kubectl apply -f ./openup.yaml
-call kubectl wait pod --for condition=ready -l name=openup --timeout=300s
+kubectl apply -f ./openup.yaml
+kubectl wait pod --for condition=ready -l name=openup --timeout=300s
 cd ../
 
 cd EPF-gateway
-call kubectl apply -f ./epf-gateway.yaml
-call kubectl wait deployment --for condition=available epf-gateway --timeout=300s
-call kubectl wait pod --for condition=ready -l app.kubernetes.io/name=epf-gateway --timeout=300s
+kubectl apply -f ./epf-gateway.yaml
+kubectl wait deployment --for condition=available epf-gateway --timeout=300s
+kubectl wait pod --for condition=ready -l app.kubernetes.io/name=epf-gateway --timeout=300s
 cd ../
 
 cd EPF-webapp
-call kubectl apply -f ./epf-webapp.yaml
-call kubectl wait deployment --for condition=available epf-webapp
-call kubectl wait pod --for condition=ready -l app.kubernetes.io/name=epf-webapp
+kubectl apply -f ./epf-webapp.yaml
+kubectl wait deployment --for condition=available epf-webapp
+kubectl wait pod --for condition=ready -l app.kubernetes.io/name=epf-webapp
 cd ../
 
 start kubectl port-forward svc/epf-gateway-internal 8080:8080
