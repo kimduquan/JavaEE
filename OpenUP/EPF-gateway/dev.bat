0c87d8f63ce43fd09b32a523ccc28f856f27f38c
kimduquan <kimduquan@gmail.com>
2022-10-26 21:08:42 +0700

    #

--- OpenUP/EPF-gateway/dev.bat
+++ OpenUP/EPF-gateway/dev.bat
@@ -1,9 +1,7 @@
 del .\EPF-gateway.log.*
 setlocal
 call ../env.bat
-:: call ../config.bat
-:: call mvn quarkus:dev -Ddebug=5006
 call mvn clean install -U -Dquarkus.container-image.build=true
-call kubectl delete -f .\epf-gateway.yaml
-call kubectl apply -f .\epf-gateway.yaml
+call stop.bat
+call start.bat
 endlocal
--- OpenUP/EPF-tests/stop.bat
+++ OpenUP/EPF-tests/stop.bat
@@ -1,2 +1,6 @@
+setlocal
+call ..\env.bat
+call mvn liberty:stop
 kubectl delete -f ./openup.yaml
 kubectl wait --for=delete -f ./openup.yaml
+endlocal
