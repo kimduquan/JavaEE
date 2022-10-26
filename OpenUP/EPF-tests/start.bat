0d2e655ab6f78fce928228aab4d1e2ce89ca66b9
kimduquan <kimduquan@gmail.com>
2022-10-26 19:43:38 +0700

    #

--- OpenUP/EPF-tests/start.bat
+++ OpenUP/EPF-tests/start.bat
@@ -1,3 +1,3 @@
 kubectl apply -f ./openup.yaml
 kubectl wait deployment --for condition=available openup --timeout=300s
-kubectl wait pod --for condition=ready -l name=openup --timeout=300s
+kubectl wait pod --for condition=ready -l app.kubernetes.io/name=openup --timeout=300s
