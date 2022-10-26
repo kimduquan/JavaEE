271b7cd36e0166cac9e2734163dcc19a82bc6050
kimduquan <kimduquan@gmail.com>
2022-10-26 08:36:22 +0700

    #

--- OpenUP/db.bat
+++ OpenUP/db.bat
@@ -1,3 +1,3 @@
 start kubectl port-forward svc/postgresql 5432:5432
 set PGPASSWORD=090323508
-"C:\Program Files\PostgreSQL\14\bin\psql.exe" -h localhost -d postgres -U postgres -p 5432 -a -w -f "./EPF-schema/src/main/resources/META-INF/persistence/data.sql"
+"C:\Program Files\PostgreSQL\14\bin\psql.exe" -h localhost -d epf -U postgres -p 5432 -a -w -f "./EPF-schema/src/main/resources/META-INF/persistence/data.sql"
