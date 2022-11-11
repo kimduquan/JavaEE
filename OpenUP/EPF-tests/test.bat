90fe8332d0880e20d250efb995e6b510ba0d4a79
kimduquan <kimduquan@gmail.com>
2022-11-11 07:28:52 +0700

    #

--- OpenUP/EPF-tests/test.bat
+++ OpenUP/EPF-tests/test.bat
@@ -1,6 +1,7 @@
 setlocal
 call ../env.bat
 call ../config.bat
+set mp.messaging.connector.liberty-kafka.bootstrap.servers=localhost:9092
 copy %SOURCE_DIR%\dev.p12 .\
 copy %SOURCE_DIR%\private.pem.pem .\
 copy %SOURCE_DIR%\public.pem .\
