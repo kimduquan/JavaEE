d3a77291ac15551156fc26765a9ce1cb5e8d882e
kimduquan <kimduquan@gmail.com>
2022-11-22 22:09:43 +0700

    #

--- OpenUP/Jaeger/Jaeger.bat
+++
@@ -1,3 +0,0 @@
-call kubectl create -f https://github.com/jaegertracing/jaeger-operator/releases/download/v1.38.0/jaeger-operator.yaml -n observability
-call kubectl get deployment jaeger-operator -n observability
-call kubectl apply -f Jaeger.yaml
