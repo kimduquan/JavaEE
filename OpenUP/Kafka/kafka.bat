0e54143c97e6ba21f0876f8aab1844ffa0dd4f57
kimduquan <kimduquan@gmail.com>
2022-11-28 18:16:48 +0700

    #

--- OpenUP/Kafka/kafka.bat
+++ OpenUP/Kafka/kafka.bat
@@ -1,3 +1,3 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 ::helm install kafka bitnami/kafka --set persistence.size=1Gi --set logPersistence.size=1Gi --set zookeeper.persistence.size=1Gi
-helm install kafka bitnami/kafka -f kafka_values.yaml
+helm install kafka bitnami/kafka -f values.yaml
