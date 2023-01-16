ddf4498d3fcd7a05458fb4338756e70841b509ff
kimduquan <kimduquan@gmail.com>
2023-01-16 19:26:11 +0700

    #

--- OpenUP/EPF-tests/test.bat
+++ OpenUP/EPF-tests/test.bat
@@ -1,6 +1,7 @@
 setlocal
 call ../env.bat
 call ../config.bat
+copy %SOURCE_DIR%\dev.p12 .\
 set mp.messaging.connector.liberty-kafka.bootstrap.servers=localhost:9092
 call mvn liberty:dev -U -P Container -DskipTestServer=true
 endlocal
--- OpenUP/EPF-tests/test.sh
+++ OpenUP/EPF-tests/test.sh
@@ -1,3 +1,4 @@
 . ../env.sh
 . ../config.sh
+cp $SOURCE_DIR/dev.p12 ./
 mvn liberty:dev -U -P Container -DskipTestServer=true
