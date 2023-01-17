a8803fba3d63d1dca0cf43df1406e09fa6b3b1b5
kimduquan <kimduquan@gmail.com>
2023-01-17 23:18:20 +0700

    #

--- OpenUP/db.sh
+++ OpenUP/db.sh
@@ -1,3 +1,3 @@
-kubectl port-forward svc/postgresql 5432:5432
+kubectl port-forward svc/postgresql 5432:5432 &
 export PGPASSWORD=090323508
 psql -h localhost -d epf -U postgres -p 5432 -a -w -f "./EPF-schema/src/main/resources/META-INF/persistence/data.sql"
