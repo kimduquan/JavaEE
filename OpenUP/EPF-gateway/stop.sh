1166c7188cb5dc8f1eb976962f30b92e27f2c956
kimduquan <kimduquan@gmail.com>
2023-01-18 01:40:10 +0700

    #

--- OpenUP/EPF-gateway/stop.sh
+++ OpenUP/EPF-gateway/stop.sh
@@ -1,4 +1,7 @@
 killall kubectl
-kubectl delete -f ./epf-gateway.yaml
+#kubectl delete -f ./epf-gateway.yaml
+#kubectl wait pod --for condition=ready=false -l app.kubernetes.io/name=epf-gateway
+#kubectl wait --for=delete -f ./epf-gateway.yaml
+kubectl delete deployment -l app.kubernetes.io/name=epf-gateway
 kubectl wait pod --for condition=ready=false -l app.kubernetes.io/name=epf-gateway
-kubectl wait --for=delete -f ./epf-gateway.yaml
+kubectl wait deployment --for=delete -l app.kubernetes.io/name=epf-gateway
--- OpenUP/EPF-messaging/stop.sh
+++ OpenUP/EPF-messaging/stop.sh
@@ -1,3 +1,6 @@
-kubectl delete -f target/kubernetes/kubernetes.yml
+#kubectl delete -f target/kubernetes/kubernetes.yml
+#kubectl wait pod --for condition=ready=false -l app.kubernetes.io/name=epf-messaging
+#kubectl wait --for=delete -f target/kubernetes/kubernetes.yml
+kubectl delete deployment -l app.kubernetes.io/name=epf-messaging
 kubectl wait pod --for condition=ready=false -l app.kubernetes.io/name=epf-messaging
-kubectl wait --for=delete -f target/kubernetes/kubernetes.yml
+kubectl wait deployment --for=delete -l app.kubernetes.io/name=epf-messaging
