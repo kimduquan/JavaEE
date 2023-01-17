39f046ba91168636a7ff1baf7b8aa192030c7164
kimduquan <kimduquan@gmail.com>
2023-01-17 23:30:41 +0700

    #

---
+++ OpenUP/EPF-schema/src/main/resources/META-INF/security/h2/drop_users.sql
@@ -0,0 +1,23 @@
+/*Basic Roles BEGIN*/
+DROP USER IF EXISTS 'analyst1@openup.org';
+DROP USER IF EXISTS 'any_role1@openup.org';
+DROP USER IF EXISTS 'architect1@openup.org';
+DROP USER IF EXISTS 'developer1@openup.org';
+DROP USER IF EXISTS 'project_manager1@openup.org';
+DROP USER IF EXISTS 'stakeholder1@openup.org';
+DROP USER IF EXISTS 'tester1@openup.org';
+/*Basic Roles END*/
+
+/*Deployment BEGIN*/
+DROP USER IF EXISTS 'course_developer1@openup.org';
+DROP USER IF EXISTS 'deployment_engineer1@openup.org';
+DROP USER IF EXISTS 'deployment_manager1@openup.org';
+DROP USER IF EXISTS 'product_owner1@openup.org';
+DROP USER IF EXISTS 'technical_writer1@openup.org';
+DROP USER IF EXISTS 'trainer1@openup.org';
+/*Deployment END*/
+
+/*Environment BEGIN*/
+DROP USER IF EXISTS 'process_engineer1@openup.org';
+DROP USER IF EXISTS 'tool_specialist1@openup.org';
+/*Environment END*/
