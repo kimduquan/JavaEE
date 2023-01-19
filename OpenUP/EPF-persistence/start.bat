927ceeeafc90c3730f8d72b54ac06043fdef371e
kimduquan <kimduquan@gmail.com>
2023-01-19 07:48:30 +0700

    #

--- OpenUP/EPF-persistence/start.bat
+++ OpenUP/EPF-persistence/start.bat
@@ -1,3 +1,3 @@
 kubectl apply -f target/kubernetes/kubernetes.yml
-kubectl wait deployment --for condition=available --timeout=40s epf-persistence
-kubectl wait pod --for condition=ready --timeout=40s -l app.kubernetes.io/name=epf-persistence
+kubectl wait deployment --for condition=available --timeout=60s epf-persistence
+kubectl wait pod --for condition=ready --timeout=60s -l app.kubernetes.io/name=epf-persistence
