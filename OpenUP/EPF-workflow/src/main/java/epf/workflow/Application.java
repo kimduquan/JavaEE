385f82de10d6edd61bfe96666fcd777af66d8ea9
kimduquan <kimduquan@gmail.com>
2023-02-19 19:11:03 +0700

    #

--- OpenUP/EPF-workflow/src/main/java/epf/workflow/Application.java
+++ OpenUP/EPF-workflow/src/main/java/epf/workflow/Application.java
@@ -1,20 +1,19 @@
-package epf.schedule.shell;
+package epf.workflow;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 
 /**
+ * @author PC
  *
- * @author FOXCONN
  */
 @ApplicationScoped
 @ApplicationPath("/")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-public class Service extends Application {
-	
+public class Application extends javax.ws.rs.core.Application {
+
 }
--- OpenUP/EPF-workflow/src/main/java/epf/workflow/Workflow.java
+++ OpenUP/EPF-workflow/src/main/java/epf/workflow/Workflow.java
@@ -1,11 +1,15 @@
 package epf.workflow;
 
+import java.util.Optional;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.json.JsonValue;
 import javax.validation.Valid;
-import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import epf.naming.Naming;
@@ -17,7 +21,7 @@ import jakarta.nosql.mapping.document.DocumentTemplate;
  *
  */
 @ApplicationScoped
-@ApplicationPath(Naming.WORKFLOW)
+@Path(Naming.WORKFLOW)
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class Workflow {
@@ -26,13 +30,41 @@ public class Workflow {
 	 * 
 	 */
 	@Inject
+	WorkflowRuntime workflowRuntime;
+	
+	/**
+	 * 
+	 */
+	@Inject
 	DocumentTemplate document;
 
 	/**
+	 * @param workflowDefinition
 	 * @return
 	 */
 	@POST
 	public WorkflowDefinition newWorkflowDefinition(@Valid final WorkflowDefinition workflowDefinition) {
 		return document.insert(workflowDefinition);
+	}
+	
+	/**
+	 * @param workflowId
+	 * @param input
+	 * @return
+	 * @throws Exception
+	 */
+	@POST
+	@Path("{workflowId}")
+	public JsonValue start(
+			@PathParam("workflowId") 
+			final String workflowId, 
+			final JsonValue input) throws Exception {
+		final WorkflowData workflowData = new WorkflowData();
+		final Optional<WorkflowDefinition> workflowDefinition = document.find(WorkflowDefinition.class, workflowId);
+		if(workflowDefinition.isPresent()) {
+			workflowRuntime.start(workflowDefinition.get(), workflowData);
+			return workflowData.getOutput();
+		}
+		throw new NotFoundException();
 	}
 }
