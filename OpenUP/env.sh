8d296ede01c712da66ccc8382a0bc9c0ee8611d3
kimduquan <kimduquan@gmail.com>
2023-03-21 22:23:04 +0700

    #

--- OpenUP/env.sh
+++ OpenUP/env.sh
@@ -1,12 +1,12 @@
 export USER_DIR=~/
 export SOURCE_DIR=~/git/JavaEE/OpenUP
-export JAVA8_HOME=~/jdk8u352-b08
-export JAVA11_HOME=~/jdk-11.0.17+8
+export JAVA8_HOME=~/jdk8u362-b09
+export JAVA11_HOME=~/jdk-11.0.18+10
 export JAVA_HOME=$JAVA11_HOME
 export PLUTO_HOME=~/pluto-3.1.1
 export KAFKA_DIR=~/kafka_2.13-3.3.1
 export KAFKA_HOME=$KAFKA_DIR/bin
 export WILDFLY_HOME=~/wildfly-24.0.1.Final/bin
 export JAEGER_HOME=~/jaeger-1.41.0-linux-amd64
-export MAVEN_HOME=~/apache-maven-3.8.7/bin
+export MAVEN_HOME=~/apache-maven-3.9.1/bin
 export PATH=$MAVEN_HOME:$PATH
