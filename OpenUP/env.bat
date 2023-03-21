207e152d7cba74f62d0f54fbfd0936a59d1e90a2
kimduquan <kimduquan@gmail.com>
2023-03-21 22:07:38 +0700

    #

--- OpenUP/env.bat
+++ OpenUP/env.bat
@@ -1,11 +1,11 @@
 set USER_DIR=C:\Users\PC
 set SOURCE_DIR=C:\GIT\JavaEE\OpenUP
-set JAVA8_HOME=C:\jdk8u352-b08
-set JAVA11_HOME=C:\jdk-11.0.17+8
+set JAVA8_HOME=C:\jdk8u362-b09
+set JAVA11_HOME=C:\jdk-11.0.18+10
 set JAVA_HOME=%JAVA11_HOME%
 set PLUTO_HOME="C:\pluto-3.1.1"
 set KAFKA_HOME="C:\kafka_2.13-3.3.1\bin\windows"
 set WILDFLY_HOME="C:\wildfly-24.0.1.Final\bin"
 set JAEGER_HOME="C:\jaeger-1.38.1-windows-amd64"
-set MAVEN_HOME=C:\apache-maven-3.8.7\bin
+set MAVEN_HOME=C:\apache-maven-3.9.1\bin
 set PATH=%MAVEN_HOME%;%PATH%
