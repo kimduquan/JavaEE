1d540084bf2d7696fa2b5d1f53ff200e68af54b4
kimduquan <kimduquan@gmail.com>
2023-03-26 16:20:31 +0700

    #

--- OpenUP/EPF-workflow-webapp/src/main/java/epf/webapp/workflow/view/StartView.java
+++ OpenUP/EPF-workflow-webapp/src/main/java/epf/webapp/workflow/view/StartView.java
@@ -2,6 +2,7 @@ package epf.webapp.workflow.view;
 
 import java.io.Serializable;
 import javax.faces.view.ViewScoped;
+import epf.json.schema.JsonSchema;
 
 /**
  * @author PC
@@ -14,4 +15,17 @@ public class StartView implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * 
+	 */
+	private JsonSchema schema;
+
+	public JsonSchema getSchema() {
+		return schema;
+	}
+
+	public void setSchema(final JsonSchema schema) {
+		this.schema = schema;
+	}
 }
