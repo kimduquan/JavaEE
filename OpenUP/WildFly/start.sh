4bb2b3f9da51152dfa2e9a46ab20e705af6067c9
kimduquan <kimduquan@gmail.com>
2023-04-26 00:08:00 +0700

    #

--- OpenUP/WildFly/start.sh
+++ OpenUP/WildFly/start.sh
@@ -2,4 +2,4 @@ kubectl apply -f ./wildfly.yaml
 kubectl wait deployment --for condition=available wildfly
 kubectl wait pod --for condition=ready -l app.kubernetes.io/name=wildfly
 kubectl port-forward svc/wildfly-internal 9990:9990 &
-kubectl port-forward svc/wildfly 443 &
+sudo kubectl port-forward svc/wildfly 443 &
