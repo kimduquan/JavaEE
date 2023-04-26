745740a1e1983d36e0d5d7085bde59483a5ef1d1
kimduquan <kimduquan@gmail.com>
2023-04-26 20:57:13 +0700

    #

--- OpenUP/Cassandra/cassandra.sh
+++ OpenUP/Cassandra/cassandra.sh
@@ -1,3 +1,4 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
-helm install cassandra bitnami/cassandra --set dbUser.password=123456
+helm uninstall cassandra
+helm install cassandra bitnami/cassandra
