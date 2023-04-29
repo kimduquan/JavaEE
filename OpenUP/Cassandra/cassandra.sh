499078a420634b6f6a83983f51959dc8ca5a8c53
kimduquan <kimduquan@gmail.com>
2023-04-29 18:51:14 +0700

    #

--- OpenUP/Cassandra/cassandra.sh
+++ OpenUP/Cassandra/cassandra.sh
@@ -1,3 +1,3 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
-helm install cassandra bitnami/cassandra --set jvm.maxHeapSize=1G
+helm install cassandra bitnami/cassandra --set jvm.maxHeapSize=1G --set jvm.newHeapSize=200M
