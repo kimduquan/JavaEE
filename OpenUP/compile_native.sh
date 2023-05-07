9d2201306f1adef344c00a7ff1eba8b89afd610c
kimduquan <kimduquan@gmail.com>
2023-05-07 19:01:59 +0700

    #

---
+++ OpenUP/compile_native.sh
@@ -0,0 +1,24 @@
+cd EPF-config
+./dev.sh
+cd ../
+cd EPF-gateway
+./dev.sh
+cd ../
+cd EPF-logging
+./dev.sh
+cd ../
+cd EPF-messaging
+./dev.sh
+cd ../
+cd EPF-net
+./dev.sh
+cd ../
+cd EPF-registry
+./dev.sh
+cd ../
+cd EPF-shell
+./dev.sh
+cd ../
+cd EPF-transaction
+./dev.sh
+cd ../
