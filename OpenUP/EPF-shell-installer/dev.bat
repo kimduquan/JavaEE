d9a091192e2c561b605767f14f91278b939448ad
kimduquan <kimduquan@gmail.com>
2023-05-13 22:15:46 +0700

    #

--- OpenUP/EPF-shell-installer/dev.bat
+++ OpenUP/EPF-shell-installer/dev.bat
@@ -1,5 +1,7 @@
 setlocal
 call ../env.bat
 call ../native_env.bat
-call mvn clean package -U -Dnative
+call mvn clean package -U
+::call %GRAALVM_HOME%/bin/java.exe -agentlib:native-image-agent=config-merge-dir=src/main/resources/META-INF/native-image -jar target/EPF-shell-installer-1.0.0.jar
+call mvn quarkus:build -U -Dnative -e
 endlocal
