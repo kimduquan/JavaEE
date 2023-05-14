6d4c2644c31213af8988438eb142b9c209f2c139
kimduquan <kimduquan@gmail.com>
2023-05-14 22:12:24 +0700

    #

--- OpenUP/EPF-shell-installer/dev.bat
+++ OpenUP/EPF-shell-installer/dev.bat
@@ -1,7 +1,19 @@
 setlocal
 call ../env.bat
 call ../native_env.bat
-call mvn clean package -U
-::call %GRAALVM_HOME%/bin/java.exe -agentlib:native-image-agent=config-merge-dir=src/main/resources/META-INF/native-image -jar target/EPF-shell-installer-1.0.0.jar
-call mvn quarkus:build -U -Dnative -e
+set JAVA_HOME=%GRAALVM_HOME%
+call mvn clean package -U -e -X
+call rmdir /s /q ".\target\EPF-shell-installer-1.0.0\"
+call mkdir target\EPF-shell-installer-1.0.0
+call tar -xvzf ".\target\EPF-shell-installer-1.0.0.jar" -C ".\target\EPF-shell-installer-1.0.0"
+call xcopy ".\target\EPF-shell-installer-1.0.0\META-INF\" ".\src\main\resources\META-INF\" /e /exclude:exclude.txt
+call rmdir /s /q ".\src\main\resources\resources\"
+call mkdir .\src\main\resources\resources
+call xcopy ".\target\EPF-shell-installer-1.0.0\resources\" ".\src\main\resources\resources\" /e
+call rmdir /s /q ".\src\main\resources\uninstaller-META-INF\"
+call mkdir .\src\main\resources\uninstaller-META-INF
+call xcopy ".\target\EPF-shell-installer-1.0.0\uninstaller-META-INF\" ".\src\main\resources\uninstaller-META-INF\" /e
+call %GRAALVM_HOME%/bin/java.exe -agentlib:native-image-agent=config-merge-dir=src/main/resources/META-INF/native-image -jar target/EPF-shell-installer-1.0.0.jar
+::call mvn clean package -U -e -X -Dnative
+call mvn quarkus:build -e -X -Dnative 
 endlocal
