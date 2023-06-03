6b53a669e19e799745b95055047bb594f289633b
kimduquan <kimduquan@gmail.com>
2023-06-03 19:51:41 +0700

    #

--- OpenUP/EPF-workflow/src/main/java/epf/workflow/WorkflowEventStore.java
+++ OpenUP/EPF-workflow/src/main/java/epf/workflow/WorkflowEventStore.java
@@ -1,17 +1,21 @@
 package epf.workflow;
 
 import java.time.Instant;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Stream;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import epf.naming.Naming;
 import epf.workflow.event.Event;
 import epf.workflow.event.persistence.CallbackStateEvent;
 import epf.workflow.event.persistence.EventStateActionEvent;
 import epf.workflow.event.persistence.EventStateEvent;
 import epf.workflow.event.persistence.WorkflowEvent;
 import epf.workflow.event.schema.EventDefinition;
-import jakarta.nosql.column.ColumnTemplate;
+import jakarta.nosql.column.ColumnDeleteQuery;
+import jakarta.nosql.column.ColumnQuery;
+import jakarta.nosql.mapping.column.ColumnTemplate;
 
 /**
  * @author PC
@@ -40,7 +44,8 @@ public class WorkflowEventStore {
 	 * @return
 	 */
 	public Stream<EventStateEvent> findEventStateEvent(final Event event) {
-		return template.select(EventStateEvent.class).where("source").eq(event.getSource()).and("type").eq(event.getType()).and("name").eq(EventStateEvent.class.getSimpleName()).stream();
+		final ColumnQuery columnQuery = ColumnQuery.select().from(Naming.Workflow.EVENT).where("source").eq(event.getSource()).and("type").eq(event.getType()).and("name").eq(EventStateEvent.class.getSimpleName()).build();
+		return template.select(columnQuery);
 	}
 	
 	/**
@@ -48,7 +53,8 @@ public class WorkflowEventStore {
 	 * @return
 	 */
 	public Stream<EventStateActionEvent> findEventStateActionEvent(final Event event) {
-		return template.select(EventStateActionEvent.class).where("source").eq(event.getSource()).and("type").eq(event.getType()).and("name").eq(EventStateActionEvent.class.getSimpleName()).stream();
+		final ColumnQuery columnQuery = ColumnQuery.select().from(Naming.Workflow.EVENT).where("source").eq(event.getSource()).and("type").eq(event.getType()).and("name").eq(EventStateActionEvent.class.getSimpleName()).build();
+		return template.select(columnQuery);
 	}
 	
 	/**
@@ -56,7 +62,8 @@ public class WorkflowEventStore {
 	 * @return
 	 */
 	public Stream<CallbackStateEvent> findCallbackStateEvent(final Event event){
-		return template.select(CallbackStateEvent.class).where("source").eq(event.getSource()).and("type").eq(event.getType()).and("name").eq(CallbackStateEvent.class.getSimpleName()).stream();
+		final ColumnQuery columnQuery = ColumnQuery.select().from(Naming.Workflow.EVENT).where("source").eq(event.getSource()).and("type").eq(event.getType()).and("name").eq(CallbackStateEvent.class.getSimpleName()).build();
+		return template.select(columnQuery);
 	}
 	
 	/**
@@ -67,7 +74,8 @@ public class WorkflowEventStore {
 	}
 	
 	public void remove(final WorkflowEvent event) {
-		template.delete(event.getClass()).where("id").eq(UUID.fromString(event.getId())).and("source").eq(event.getSource()).and("type").eq(event.getType());
+		final ColumnDeleteQuery deleteQuery = ColumnDeleteQuery.delete().from(Naming.Workflow.EVENT).where("id").eq(UUID.fromString(event.getId())).and("source").eq(event.getSource()).and("type").eq(event.getType()).build();
+		template.delete(deleteQuery);
 	}
 	
 	/**
@@ -75,6 +83,8 @@ public class WorkflowEventStore {
 	 * @return
 	 */
 	public long count(final EventDefinition eventDefinition) {
-		return template.select(eventDefinition.getClass()).where("source").eq(eventDefinition.getSource()).and("type").eq(eventDefinition.getType()).stream().count();
+		final ColumnQuery columnQuery = ColumnQuery.select("COUNT(id)").from(Naming.Workflow.EVENT).where("source").eq(eventDefinition.getSource()).and("type").eq(eventDefinition.getType()).build();
+		Optional<Integer> count = template.singleResult(columnQuery);
+		return count.get();
 	}
 }
--- OpenUP/EPF-workflow/src/main/java/epf/workflow/WorkflowPersistence.java
+++ OpenUP/EPF-workflow/src/main/java/epf/workflow/WorkflowPersistence.java
@@ -5,7 +5,8 @@ import java.util.UUID;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import epf.workflow.schema.WorkflowDefinition;
-import jakarta.nosql.document.DocumentTemplate;
+import jakarta.nosql.document.DocumentQuery;
+import jakarta.nosql.mapping.document.DocumentTemplate;
 
 /**
  * @author PC
@@ -26,8 +27,9 @@ public class WorkflowPersistence {
 	 * @return
 	 */
 	public WorkflowDefinition find(final String id, final String version) {
-		final Optional<WorkflowDefinition> workflowDef = document.select(WorkflowDefinition.class).where("id").eq(id).and("version").eq(version).singleResult();
-		return workflowDef.get();
+		final DocumentQuery query = DocumentQuery.select().from("WorkflowDefinition").where("id").eq(id).and("version").eq(version).build();
+		final Optional<WorkflowDefinition> workflowDefinition = document.singleResult(query);
+		return workflowDefinition.get();
 	}
 	
 	/**
