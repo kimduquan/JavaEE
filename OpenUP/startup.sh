b2588fc0837265121f76b4f9f898ea4873e1071f
kimduquan <kimduquan@gmail.com>
2023-07-01 10:41:39 +0700

    #

--- OpenUP/startup.sh
+++ OpenUP/startup.sh
@@ -8,13 +8,13 @@ cd $CASSANDRA_HOME
 bin/cassandra &
 cd $JAEGER_HOME
 ./jaeger-all-in-one --collector.zipkin.host-port=:9411 &
+Xvfb :10 -ac &
+export DISPLAY=:10
+cd $KAFKA_DIR
+bin/kafka-server-start.sh config/server.properties &
 export JAVA_HOME=$JAVA8_HOME
 $PLUTO_HOME/bin/startup.sh &
 cd $WILDFLY_HOME
 sudo -E ./standalone.sh "-Djboss.http.port=80" "-Djboss.https.port=443" --debug &
-Xvfb :10 -ac &
-export DISPLAY=:10
 export JAVA_HOME=$JAVA11_HOME
-cd $KAFKA_DIR
-bin/kafka-server-start.sh config/server.properties &
 cd $CUR_DIR
