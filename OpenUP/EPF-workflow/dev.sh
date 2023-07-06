74db45d36687728cf624361feac2db381b0a967a
kimduquan <kimduquan@gmail.com>
2023-07-07 06:54:37 +0700

    #

--- OpenUP/EPF-workflow/dev.sh
+++ OpenUP/EPF-workflow/dev.sh
@@ -1,5 +1,6 @@
 . ../env.sh
 mvn clean install -U
+cp target/EPF-workflow-1.0.0.war ../EPF-tests/target/servers/test/apps
 cd ../
 cd EPF-messaging
 mvn clean install -U
@@ -17,3 +18,5 @@ cd EPF-gateway
 mvn clean install -U
 java -Dquarkus.http.port=8081 -jar target/quarkus-app/quarkus-run.jar &
 cd ../
+cd EPF-tests
+mvn liberty:dev
