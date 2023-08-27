6471cd3b00499377fdcb83253d937506cca74ddd
kimduquan <kimduquan@gmail.com>
2023-08-27 20:30:49 +0700

    #

--- OpenUP/EPF-shell/dev.bat
+++ OpenUP/EPF-shell/dev.bat
@@ -5,7 +5,7 @@ call ../env.bat
 call ../native_env.bat
 call ../config.bat
 call ../config_ssl.bat
-set QUARKUS_NATIVE_ADDITIONAL_BUILD_ARGS=--initialize-at-run-time=org.h2.fulltext.FullTextLucene\\,org.h2.fulltext.FullTextLucene\$FullTextTrigger
+set QUARKUS_NATIVE_ADDITIONAL_BUILD_ARGS=--initialize-at-run-time=org.h2.fulltext.FullTextLucene\,org.h2.fulltext.FullTextLucene\$FullTextTrigger
 call mvn clean install -U -Dnative
 :: call mvn clean install -U
 :: java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8001 -jar .\target\quarkus-app\quarkus-run.jar messaging connect -tid VQFEAvNQw6w1qXO_8qxTUg -p persistence
