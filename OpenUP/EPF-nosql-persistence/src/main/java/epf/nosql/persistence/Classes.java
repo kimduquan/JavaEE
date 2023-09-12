434cabf325ffa1794745b547591e6084d126ab04
kimduquan <kimduquan@gmail.com>
2023-09-12 22:53:23 +0700

    #

---
+++ OpenUP/EPF-nosql-persistence/src/main/java/epf/nosql/persistence/Classes.java
@@ -0,0 +1,12 @@
+package epf.nosql.persistence;
+
+import javax.json.JsonValue;
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+/**
+ * 
+ */
+@RegisterForReflection(targets={JsonValue.class})
+public interface Classes {
+
+}
