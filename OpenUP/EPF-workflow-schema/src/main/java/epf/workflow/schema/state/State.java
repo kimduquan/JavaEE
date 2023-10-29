76ed404644dcaa7ffee95611609a51520e03f423
kimduquan <kimduquan@gmail.com>
2023-10-29 11:37:18 +0700

    #

--- OpenUP/EPF-workflow-schema/src/main/java/epf/workflow/schema/state/State.java
+++ OpenUP/EPF-workflow-schema/src/main/java/epf/workflow/schema/state/State.java
@@ -1,6 +1,8 @@
 package epf.workflow.schema.state;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.json.bind.annotation.JsonbCreator;
+import jakarta.json.bind.annotation.JsonbProperty;
 import jakarta.nosql.Column;
 import org.eclipse.jnosql.mapping.DiscriminatorColumn;
 import org.eclipse.jnosql.mapping.MappedSuperclass;
@@ -26,6 +28,49 @@ public class State {
 	@NotNull
 	@Column
 	private Type type;
+	
+	/**
+	 * @param type
+	 * @param name
+	 * @return
+	 */
+	@JsonbCreator
+	public static State newState(@JsonbProperty("type") final String type, @JsonbProperty("name") final String name) {
+		final Type stateType = "switch".equals(type) ? Type.Switch : Type.valueOf(type.toLowerCase());
+		State state = null;
+		switch(stateType) {
+			case Switch:
+				state = new SwitchState();
+				break;
+			case callback:
+				state = new CallbackState();
+				break;
+			case event:
+				state = new EventState();
+				break;
+			case foreach:
+				state = new ForEachState();
+				break;
+			case inject:
+				state = new InjectState();
+				break;
+			case operation:
+				state = new OperationState();
+				break;
+			case parallel:
+				state = new ParallelState();
+				break;
+			case sleep:
+				state = new SleepState();
+				break;
+			default:
+				state = new State();
+				break;
+		}
+		state.type = stateType;
+		state.name = name;
+		return state;
+	}
 
 	public String getName() {
 		return name;
