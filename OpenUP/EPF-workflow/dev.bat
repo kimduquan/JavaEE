43273d969518e7a55e2e6bc5aea75fd150c7e35e
kimduquan <kimduquan@gmail.com>
2023-11-11 20:39:14 +0700

    #

--- OpenUP/EPF-workflow/dev.bat
+++ OpenUP/EPF-workflow/dev.bat
@@ -1,6 +1,12 @@
 setlocal
 call ../env.bat
+cd ../EPF-gateway
+mvn clean install -U
+copy %SOURCE_DIR%\dev.p12 .\
+copy %SOURCE_DIR%\public.pem .\
+java -Dquarkus.http.port=8081 -jar target/quarkus-app/quarkus-run.jar &
+cd ../EPF-workflow
 set quarkus.devservices.enabled=false
-call mvn clean install -U
+mvn clean install -U
 mvn quarkus:dev -Ddebug=5007
 endlocal
