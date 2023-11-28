80ff529dde6834db3100ab30111c3719197482f2
kimduquan <kimduquan@gmail.com>
2023-11-29 00:38:01 +0700

    #

--- OpenUP/EPF-workflow/src/main/java/epf/workflow/WorkflowLink.java
+++ OpenUP/EPF-workflow/src/main/java/epf/workflow/WorkflowLink.java
@@ -18,9 +18,9 @@ public interface WorkflowLink {
 	 */
 	static Link startLink(final String workflow, final String version) {
 		if(version != null) {
-			return LinkUtil.build(UriBuilder.fromUri(Naming.WORKFLOW + "/{workflow}").queryParam("version", version),  Naming.WORKFLOW, HttpMethod.PUT, workflow);
+			return LinkUtil.build(UriBuilder.fromUri("{workflow}").queryParam("version", version),  Naming.WORKFLOW, HttpMethod.PUT, workflow);
 		}
-		return LinkUtil.build(UriBuilder.fromUri(Naming.WORKFLOW + "/{workflow}"),  Naming.WORKFLOW, HttpMethod.PUT, workflow);
+		return LinkUtil.build(UriBuilder.fromUri("{workflow}"),  Naming.WORKFLOW, HttpMethod.PUT, workflow);
 	}
 	
 	/**
@@ -30,9 +30,9 @@ public interface WorkflowLink {
 	 */
 	static Link getWorkflowDefinitionLink(final String workflow, final String version) {
 		if(version != null) {
-			return LinkUtil.build(UriBuilder.fromUri(Naming.WORKFLOW + "/{workflow}").queryParam("version", version), Naming.WORKFLOW, HttpMethod.GET, workflow);
+			return LinkUtil.build(UriBuilder.fromUri("{workflow}").queryParam("version", version), Naming.WORKFLOW, HttpMethod.GET, workflow);
 		}
-		return LinkUtil.build(UriBuilder.fromUri(Naming.WORKFLOW + "/{workflow}"), Naming.WORKFLOW, HttpMethod.GET, workflow);
+		return LinkUtil.build(UriBuilder.fromUri("{workflow}"), Naming.WORKFLOW, HttpMethod.GET, workflow);
 	}
 	
 	/**
@@ -43,9 +43,9 @@ public interface WorkflowLink {
 	 */
 	static Link transitionLink(final String workflow, final String version, final String state) {
 		if(version != null) {
-			return LinkUtil.build(UriBuilder.fromUri(Naming.WORKFLOW + "/{workflow}/{state}").queryParam("version", version), Naming.WORKFLOW, HttpMethod.PUT, workflow, state);
+			return LinkUtil.build(UriBuilder.fromUri("{workflow}/{state}").queryParam("version", version), Naming.WORKFLOW, HttpMethod.PUT, workflow, state);
 		}
-		return LinkUtil.build(UriBuilder.fromUri(Naming.WORKFLOW + "/{workflow}/{state}"), Naming.WORKFLOW, HttpMethod.PUT, workflow, state);
+		return LinkUtil.build(UriBuilder.fromUri("{workflow}/{state}"), Naming.WORKFLOW, HttpMethod.PUT, workflow, state);
 	}
 	
 	/**
@@ -56,9 +56,9 @@ public interface WorkflowLink {
 	 */
 	static Link endLink(final String workflow, final String version, final String state) {
 		if(version != null) {
-			return LinkUtil.build(UriBuilder.fromUri(Naming.WORKFLOW + "/{workflow}/{state}/end").queryParam("version", version), Naming.WORKFLOW, HttpMethod.PUT, workflow, state);
+			return LinkUtil.build(UriBuilder.fromUri("{workflow}/{state}/end").queryParam("version", version), Naming.WORKFLOW, HttpMethod.PUT, workflow, state);
 		}
-		return LinkUtil.build(UriBuilder.fromUri(Naming.WORKFLOW + "/{workflow}/{state}/end"), Naming.WORKFLOW, HttpMethod.PUT, workflow, state);
+		return LinkUtil.build(UriBuilder.fromUri("{workflow}/{state}/end"), Naming.WORKFLOW, HttpMethod.PUT, workflow, state);
 	}
 	
 	/**
@@ -69,9 +69,9 @@ public interface WorkflowLink {
 	 */
 	static Link compensateLink(final String workflow, final String version, final String state) {
 		if(version != null) {
-			return LinkUtil.build(UriBuilder.fromUri(Naming.WORKFLOW + "/{workflow}/{state}/compensate").queryParam("version", version), Naming.WORKFLOW, HttpMethod.PUT, workflow, state);
+			return LinkUtil.build(UriBuilder.fromUri("{workflow}/{state}/compensate").queryParam("version", version), Naming.WORKFLOW, HttpMethod.PUT, workflow, state);
 		}
-		return LinkUtil.build(UriBuilder.fromUri(Naming.WORKFLOW + "/{workflow}/{state}/compensate"), Naming.WORKFLOW, HttpMethod.PUT, workflow, state);
+		return LinkUtil.build(UriBuilder.fromUri("{workflow}/{state}/compensate"), Naming.WORKFLOW, HttpMethod.PUT, workflow, state);
 	}
 	
 	/**
@@ -86,6 +86,6 @@ public interface WorkflowLink {
 			final String method,
 			final String path,
 			final String recurringTimeInterval) {
-		return LinkUtil.build(UriBuilder.fromUri(Naming.SCHEDULE + "/REST").queryParam("service", service).queryParam("method", method).queryParam("recurringTimeInterval", recurringTimeInterval), Naming.SCHEDULE, HttpMethod.POST);
+		return LinkUtil.build(UriBuilder.fromUri("REST").queryParam("service", service).queryParam("method", method).queryParam("recurringTimeInterval", recurringTimeInterval), Naming.SCHEDULE, HttpMethod.POST);
 	}
 }
