9edcb44065326e85131a30e59ac0747361f04ad1
kimduquan <kimduquan@gmail.com>
2023-12-04 20:52:23 +0700

    #

--- OpenUP/EPF-workflow/dev.sh
+++ OpenUP/EPF-workflow/dev.sh
@@ -1,7 +1,7 @@
 . ../env.sh
 . ../config.sh
 mvn clean install -U
-$JAVA21_HOME/bin/java -jar target/quarkus-app/quarkus-run.jar &
+#$JAVA21_HOME/bin/java -jar target/quarkus-app/quarkus-run.jar &
 #mvn quarkus:dev -Ddebug=5007 &
 cd ../
 cd EPF-config
