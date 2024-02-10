40623b781f2bc4db0903015f60bd944ec51c511c
kimduquan <kimduquan@gmail.com>
2024-02-10 13:44:08 +0700

    #

--- OpenUP/EPF-workflow-management/src/main/java/epf/workflow/management/WorkflowManagement.java
+++ OpenUP/EPF-workflow-management/src/main/java/epf/workflow/management/WorkflowManagement.java
@@ -2,27 +2,21 @@ package epf.workflow.management;
 
 import java.io.InputStream;
 import java.util.Optional;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
 import jakarta.validation.Validator;
 import jakarta.ws.rs.BadRequestException;
-import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Link;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.ResponseBuilder;
-import org.eclipse.microprofile.context.ManagedExecutor;
 import org.eclipse.microprofile.health.Readiness;
 import epf.naming.Naming;
+import epf.workflow.client.Management;
 import epf.workflow.management.internal.WorkflowCache;
 import epf.workflow.management.internal.WorkflowLink;
 import epf.workflow.management.internal.WorkflowPersistence;
@@ -35,7 +29,7 @@ import io.smallrye.common.annotation.RunOnVirtualThread;
  */
 @ApplicationScoped
 @Path(Naming.Workflow.WORKFLOW_MANAGEMENT)
-public class WorkflowManagement  {
+public class WorkflowManagement implements Management {
 	
 	/**
 	 * 
@@ -49,12 +43,6 @@ public class WorkflowManagement  {
 	@Inject
 	@Readiness
 	transient WorkflowCache cache;
-	
-	/**
-	 * 
-	 */
-	@Inject
-	transient ManagedExecutor executor;
 	
 	/**
 	 * 
@@ -84,10 +72,8 @@ public class WorkflowManagement  {
 		return workflowDefinition.orElseThrow(NotFoundException::new);
 	}
 
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
 	@RunOnVirtualThread
+	@Override
 	public Response newWorkflowDefinition(final InputStream body) throws Exception {
 		WorkflowDefinition workflowDefinition = null;
 		try(Jsonb jsonb = JsonbBuilder.create()){
@@ -109,15 +95,9 @@ public class WorkflowManagement  {
 		return getWorkflowLink(newWorkflowDefinition).build();
 	}
 
-	@GET
-	@Path("{workflow}")
-	@Produces(MediaType.APPLICATION_JSON)
 	@RunOnVirtualThread
-	public Response getWorkflowDefinition(
-			@PathParam("workflow")
-			final String workflow, 
-			@QueryParam("version")
-			final String version) throws Exception {
+	@Override
+	public Response getWorkflowDefinition(final String workflow, final String version) throws Exception {
 		final WorkflowDefinition workflowDefinition = findWorkflowDefinition(workflow, version);
 		try(Jsonb jsonb = JsonbBuilder.create()){
 			return Response.ok(jsonb.toJson(workflowDefinition), MediaType.APPLICATION_JSON).build();
