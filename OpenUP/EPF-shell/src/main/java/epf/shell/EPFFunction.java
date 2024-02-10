3615e689f6cb7e7115e6ea6073f63a5c8066f91b
kimduquan <kimduquan@gmail.com>
2024-02-10 21:19:30 +0700

    #

--- OpenUP/EPF-shell/src/main/java/epf/shell/EPFFunction.java
+++ OpenUP/EPF-shell/src/main/java/epf/shell/EPFFunction.java
@@ -12,6 +12,7 @@ import epf.shell.rules.Rules;
 import epf.shell.schema.Schema;
 import epf.shell.security.Security;
 import epf.shell.util.Utility;
+import epf.shell.workflow.Workflow;
 import io.quarkus.picocli.runtime.annotations.TopCommand;
 import picocli.CommandLine.Command;
 import javax.enterprise.context.ApplicationScoped;
@@ -21,7 +22,7 @@ import javax.enterprise.context.ApplicationScoped;
  *
  */
 @TopCommand
-@Command(name = "epf", subcommands = {Security.class, Persistence.class, Query.class, Search.class, Schema.class, FileStore.class, Rules.class, Utility.class, Image.class, Messaging.class, Health.class, Mail.class})
+@Command(name = "epf", subcommands = {Security.class, Persistence.class, Query.class, Search.class, Schema.class, FileStore.class, Rules.class, Utility.class, Image.class, Messaging.class, Health.class, Mail.class, Workflow.class})
 @ApplicationScoped
 public class EPFFunction {
 	
