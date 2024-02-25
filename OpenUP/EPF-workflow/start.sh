c1fb7365d0a9666d5abb4b969d0e6834e9ead116
kimduquan <kimduquan@gmail.com>
2024-02-25 19:10:32 +0700

    #

--- OpenUP/EPF-workflow/start.sh
+++ OpenUP/EPF-workflow/start.sh
@@ -1,3 +1,6 @@
+curl http://localhost:9183/q/health
+curl http://localhost:9191/q/health
+curl http://localhost:9192/q/health
+curl http://localhost:9189/q/health
 curl http://localhost:9080/health
 curl http://localhost:8081/q/health
-curl http://localhost:9189/q/health
