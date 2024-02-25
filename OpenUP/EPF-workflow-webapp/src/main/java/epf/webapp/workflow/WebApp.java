7702797942d152234f30105397575587b65f2e37
kimduquan <kimduquan@gmail.com>
2024-02-25 20:35:40 +0700

    #

--- OpenUP/EPF-workflow-webapp/src/main/java/epf/webapp/workflow/WebApp.java
+++ OpenUP/EPF-workflow-webapp/src/main/java/epf/webapp/workflow/WebApp.java
@@ -1,9 +1,7 @@
 package epf.webapp.workflow;
 
-import javax.annotation.security.DeclareRoles;
 import javax.enterprise.context.ApplicationScoped;
 import javax.faces.annotation.FacesConfig;
-import epf.naming.Naming;
 
 /**
  * @author PC
@@ -11,7 +9,6 @@ import epf.naming.Naming;
  */
 @ApplicationScoped
 @FacesConfig
-@DeclareRoles(value = { Naming.Security.DEFAULT_ROLE })
 public class WebApp {
 	
 }
