e5bcc90ae751ac571d244612ae29bcb238f6f850
kimduquan <kimduquan@gmail.com>
2024-03-09 01:42:31 +0700

    #

--- OpenUP/ScyllaDB/scylladb.sh
+++ OpenUP/ScyllaDB/scylladb.sh
@@ -1,5 +1,5 @@
-sudo mkdir -p /etc/apt/keyrings
-sudo gpg --homedir /tmp --no-default-keyring --keyring /etc/apt/keyrings/scylladb.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys d0a112e067426ab2
-sudo curl -L --output /etc/apt/sources.list.d/scylla.list http://downloads.scylladb.com/deb/ubuntu/scylla-5.4.list
-sudo apt-get update
-sudo apt-get install -y scylla 
+helm repo add scylla https://scylla-operator-charts.storage.googleapis.com/stable
+helm repo update
+helm install scylla-operator scylla/scylla-operator --create-namespace --namespace scylla-operator
+helm install scylla-manager scylla/scylla-manager --create-namespace --namespace scylla-manager
+helm install scylla scylla/scylla --create-namespace --namespace scylla
