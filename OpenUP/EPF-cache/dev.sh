21e9577a854a41259a3cc76d9b7aeb561b50b115
kimduquan <kimduquan@gmail.com>
2024-03-09 16:45:14 +0700

    #

--- OpenUP/EPF-cache/dev.sh
+++ OpenUP/EPF-cache/dev.sh
@@ -1,4 +1,4 @@
 . ../env.sh
-mvn clean install -U -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true -P native
+mvn clean install -U -Dquarkus.container-image.build=true
 ./stop.sh
 ./start.sh
