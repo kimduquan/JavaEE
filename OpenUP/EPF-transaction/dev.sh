dc2455a0f569188511c9e9d2cbd8c15b516baaab
kimduquan <kimduquan@gmail.com>
2024-03-10 12:29:16 +0700

    #

--- OpenUP/EPF-transaction/dev.sh
+++ OpenUP/EPF-transaction/dev.sh
@@ -1,6 +1,5 @@
 rm -R -d ObjectStore
 . ../env.sh
-. ../native_env.sh
-mvn clean install -U -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true -P native
+mvn clean install -U -Dquarkus.container-image.build=true
 ./stop.sh
 ./start.sh
