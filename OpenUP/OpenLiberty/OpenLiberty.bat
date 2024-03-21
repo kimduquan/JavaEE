78405a068c03b6e3fd2f9b6c9a69869b6ed6ee77
kimduquan <kimduquan@gmail.com>
2024-03-21 21:39:39 +0700

    #

--- OpenUP/OpenLiberty/OpenLiberty.bat
+++
@@ -1,4 +0,0 @@
-setlocal
-kubectl apply --server-side -f https://raw.githubusercontent.com/OpenLiberty/open-liberty-operator/main/deploy/releases/1.2.2/kubectl/openliberty-app-crd.yaml
-kubectl apply -f ./openliberty-app-operator.yaml
-endlocal
---
+++ OpenUP/OpenLiberty/openliberty.sh
@@ -0,0 +1,2 @@
+kubectl apply --server-side -f https://raw.githubusercontent.com/OpenLiberty/open-liberty-operator/main/deploy/releases/1.3.1/kubectl/openliberty-app-crd.yaml
+kubectl apply -f ./openliberty-app-operator.yaml
