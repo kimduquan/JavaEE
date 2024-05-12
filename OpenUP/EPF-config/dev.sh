bb9dd8ee49a6ea544b7a15f9e03b048ab2012fce
kimduquan <kimduquan@gmail.com>
2024-05-12 07:21:29 +0700

    #

--- OpenUP/EPF-config/dev.sh
+++ OpenUP/EPF-config/dev.sh
@@ -1,6 +1,6 @@
 . ../env.sh
 . ../native_env.sh
-cp $SOURCE_DIR/public.pem ./src/main/jib/home/jboss/
+cp $SOURCE_DIR/public.pem ./src/main/jib/
 mvn clean install -U -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true -P native
 ./stop.sh
 ./start.sh
