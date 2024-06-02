37c2a4d9612e2562186be7b3e0a9a0ac1d218296
kimduquan <kimduquan@gmail.com>
2024-06-02 11:59:54 +0700

    #

--- OpenUP/Odoo/odoo.sh
+++ OpenUP/Odoo/odoo.sh
@@ -1,3 +1,3 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
-helm install odoo bitnami/odoo --set postgresql.enabled=false --set postgresql.auth.username=odoo --set postgresql.auth.password=123456 --set postgresql.auth.database=odoo --set externalDatabase.host=postgresql.default.svc.cluster.local --set externalDatabase.user=odoo --set externalDatabase.password=123456 --set externalDatabase.database=odoo --set externalDatabase.postgresqlPostgresPassword=090323508
+helm install odoo bitnami/odoo --set postgresql.enabled=false --set postgresql.auth.username=odoo --set postgresql.auth.password=123456 --set postgresql.auth.database=odoo --set externalDatabase.host=postgresql.default.svc.cluster.local --set externalDatabase.user=odoo --set externalDatabase.password="123456" --set externalDatabase.database=odoo --set externalDatabase.postgresqlPostgresPassword="090323508"
