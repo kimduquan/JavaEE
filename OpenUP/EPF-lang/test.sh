a323ee2e9832c7466bb56fa41cbcfe68b2194f19
kimduquan <kimduquan@gmail.com>
2024-06-15 22:35:45 +0700

    #

--- OpenUP/EPF-lang/test.sh
+++ OpenUP/EPF-lang/test.sh
@@ -1,7 +1,8 @@
 . ../env.sh
-. ../EPF-concurrent/start.sh
-. ../EPF-registry/start.sh
-. ../EPF-gateway/start.sh
+../EPF-tests/start.sh
+../EPF-concurrent/start.sh
+../EPF-registry/start.sh
+../EPF-gateway/start.sh
 mvn clean install -U -Dquarkus.container-image.build=true
 ./stop.sh
 ./start.sh
