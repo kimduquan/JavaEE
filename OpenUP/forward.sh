63c88b1503178bc7df93f8d2e21c2478927bf260
kimduquan <kimduquan@gmail.com>
2024-06-16 13:53:56 +0700

    #

---
+++ OpenUP/forward.sh
@@ -0,0 +1,7 @@
+kubectl port-forward svc/hazelcast 5701
+kubectl port-forward svc/kafka 9092
+kubectl port-forward svc/mongodb 27017
+kubectl port-forward svc/ollama 11434
+kubectl port-forward svc/postgresql 5432
+kubectl port-forward svc/wildfly 443
+kubectl port-forward svc/wildfly-internal 9990
