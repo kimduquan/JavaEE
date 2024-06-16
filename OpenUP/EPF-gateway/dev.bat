19644e4b3ec1f2c427cd9ac56f6d38e19dc8b928
kimduquan <kimduquan@gmail.com>
2024-06-16 13:42:17 +0700

    #

--- OpenUP/EPF-gateway/dev.bat
+++ OpenUP/EPF-gateway/dev.bat
@@ -1,8 +1,5 @@
 setlocal
 call ../env.bat
-copy %SOURCE_DIR%\dev.p12 .\src\main\jib\
-copy %SOURCE_DIR%\public.pem .\src\main\jib\
-call mvn clean install -U -Dquarkus.container-image.build=true
-call stop.bat
-call start.bat
+call mvn clean install -U
+call mvn quarkus:dev
 endlocal
