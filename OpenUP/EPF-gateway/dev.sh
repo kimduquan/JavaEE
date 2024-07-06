f43298e9b69372dd71c81f1d46e4985e8fdccc42
kimduquan <kimduquan@gmail.com>
2024-07-06 09:55:42 +0700

    #

--- OpenUP/EPF-gateway/dev.sh
+++ OpenUP/EPF-gateway/dev.sh
@@ -1,5 +1,5 @@
 . ../env.sh
-mkdir –p src/main/jib
+mkdir src/main/jib
 cp $SOURCE_DIR/dev.p12 ./src/main/jib/
 cp $SOURCE_DIR/public.pem ./src/main/jib/
 mvn clean install -U -Dquarkus.container-image.build=true
