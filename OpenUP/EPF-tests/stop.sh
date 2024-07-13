d2bc54df58ee955bb37ad3da88caabd4d57ad529
kimduquan <kimduquan@gmail.com>
2024-07-13 18:37:28 +0700

    #

--- OpenUP/EPF-tests/stop.sh
+++ OpenUP/EPF-tests/stop.sh
@@ -1,5 +1,3 @@
-. ../env.sh
-mvn liberty:stop
 kubectl delete -f ./openup.yaml
 kubectl wait pod --for condition=ready=false -l app.kubernetes.io/name=openup
 kubectl wait --for=delete -f ./openup.yaml
