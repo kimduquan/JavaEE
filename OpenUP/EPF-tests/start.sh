1984fd5e1c9112d0b2e35bfaf743c374cfc3881f
kimduquan <kimduquan@gmail.com>
2024-07-13 19:25:57 +0700

    #

--- OpenUP/EPF-tests/start.sh
+++ OpenUP/EPF-tests/start.sh
@@ -1,3 +1,3 @@
 kubectl apply -f ./openup.yaml
 kubectl wait --for condition=RECONCILED=True openlibertyapplication.apps.openliberty.io/openup
-kubectl wait pod --for condition=ready -l app.kubernetes.io/name=openup --timeout=300s
+kubectl wait pod --for condition=ready -l app.kubernetes.io/name=openup --timeout=600s
