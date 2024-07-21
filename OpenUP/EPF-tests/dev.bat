5b841298b86e91bfcf7cb2640d8d338597d7d5fe
kimduquan <kimduquan@gmail.com>
2024-07-21 10:25:47 +0700

    #

--- OpenUP/EPF-tests/dev.bat
+++ OpenUP/EPF-tests/dev.bat
@@ -1,10 +1,6 @@
 setlocal
 call ../env.bat
 call ../config.bat
-copy %USER_DIR%\EPF-query.trace.db .\
-copy %USER_DIR%\EPF-query.mv.db .\
-copy %USER_DIR%\EPF-security.mv.db .\
-copy %USER_DIR%\EPF-security.trace.db .\
 copy %SOURCE_DIR%\dev.p12 .\
 copy %SOURCE_DIR%\private.pem .\
 copy %SOURCE_DIR%\public.pem .\
