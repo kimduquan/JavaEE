98ed1be70265fa3484cd035d396e932e2903d813
kimduquan <kimduquan@gmail.com>
2024-07-23 21:11:14 +0700

    #

--- OpenUP/db.sh
+++ OpenUP/db.sh
@@ -1,3 +1,4 @@
-kubectl port-forward svc/postgresql 5432:5432 &
 export PGPASSWORD=090323508
 psql -h localhost -d epf -U postgres -p 5432 -a -w -f "./EPF-schema/src/main/resources/META-INF/persistence/data.sql"
+CREATE USER openpg WITH CREATEDB CREATEROLE PASSWORD 'openpgpwd';
+CREATE DATABASE odoo WITH OWNER = openpg;
--- OpenUP/forward.sh
+++ OpenUP/forward.sh
@@ -3,3 +3,5 @@ kubectl port-forward svc/kafka 9092 &
 kubectl port-forward svc/mongodb 27017 &
 kubectl port-forward svc/ollama 11434 &
 kubectl port-forward svc/postgresql 5432 &
+kubectl port-forward svc/neo4j 7687 &
+kubectl port-forward svc/neo4j 7474 &
