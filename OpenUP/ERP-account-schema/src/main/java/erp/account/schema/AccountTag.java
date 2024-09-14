d6d29f5d5e438092ef4339443cb8c5e17ff0f98e
kimduquan <kimduquan@gmail.com>
2024-09-14 09:48:40 +0700

    #

--- OpenUP/ERP-account-schema/src/main/java/erp/account/schema/AccountTag.java
+++ OpenUP/ERP-account-schema/src/main/java/erp/account/schema/AccountTag.java
@@ -2,7 +2,11 @@ package erp.account.schema;
 
 import org.eclipse.microprofile.graphql.DefaultValue;
 import org.eclipse.microprofile.graphql.Description;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Property;
+import org.neo4j.ogm.annotation.typeconversion.Convert;
 import erp.base.schema.res.Company;
+import erp.schema.util.NameAttributeConverter;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -17,6 +21,7 @@ import jakarta.validation.constraints.NotNull;
 @Entity
 @Table(name = "account_account_tag")
 @Description("Account Tag")
+@NodeEntity("Account Tag")
 public class AccountTag {
 
 	/**
@@ -25,6 +30,8 @@ public class AccountTag {
 	@Column(nullable = false)
 	@NotNull
 	@Description("Tag Name")
+	@Convert(NameAttributeConverter.class)
+	@Property
 	private String name;
 	
 	/**
@@ -33,7 +40,8 @@ public class AccountTag {
 	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
 	@NotNull
-	@DefaultValue("accounts")	
+	@DefaultValue("accounts")
+	@Property
 	private String applicability = "accounts";
 	
 	/**
@@ -41,6 +49,7 @@ public class AccountTag {
 	 */
 	@Column
 	@Description("Color Index")
+	@Property
 	private Integer color;
 	
 	/**
@@ -49,6 +58,7 @@ public class AccountTag {
 	@Column
 	@DefaultValue("true")
 	@Description("Set active to false to hide the Account Tag without removing it.")
+	@Property
 	private Boolean active = true;
 	
 	/**
@@ -56,6 +66,7 @@ public class AccountTag {
 	 */
 	@Column
 	@Description("Negate Tax Balance")
+	@Property
 	private Boolean tax_negate; 
 	
 	/**
@@ -64,5 +75,6 @@ public class AccountTag {
 	@Column
 	@ManyToOne(targetEntity = Company.class)
 	@Description("Country")
+	@Property
 	private String country_id;
 }
