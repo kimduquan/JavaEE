de16d0c9cbb8976f2c2c677604fba66c5c23e7e9
kimduquan <kimduquan@gmail.com>
2024-10-27 13:40:20 +0700

    #

--- OpenUP/forward.sh
+++ OpenUP/forward.sh
@@ -3,7 +3,7 @@ kubectl port-forward svc/kafka 9092 &
 kubectl port-forward svc/mongodb 27017 &
 kubectl port-forward svc/ollama 11434 &
 kubectl port-forward svc/postgresql 5432 &
-kubectl port-forward svc/neo4j 7687 &
-kubectl port-forward svc/neo4j 7474 &
+#kubectl port-forward svc/neo4j 7687 &
+#kubectl port-forward svc/neo4j 7474 &
 kubectl port-forward svc/jaeger-query 16686 &
-kubectl port-forward svc/solr 8983 &
+#kubectl port-forward svc/solr 8983 &
