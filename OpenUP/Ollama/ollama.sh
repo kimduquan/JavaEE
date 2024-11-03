f5b49786fc838ebe886140c04ee94fd43839afb1
kimduquan <kimduquan@gmail.com>
2024-11-03 17:43:42 +0700

    #

--- OpenUP/Ollama/ollama.sh
+++ OpenUP/Ollama/ollama.sh
@@ -1,4 +1,4 @@
-docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama -e OLLAMA_KEEP_ALIVE='24h' -e OLLAMA_NUM_PARALLEL='1' -e OLLAMA_MAX_QUEUE='86400' -e OLLAMA_MAX_LOADED_MODELS='3'
+docker run -d -e OLLAMA_KEEP_ALIVE='24h' -e OLLAMA_NUM_PARALLEL='1' -e OLLAMA_MAX_QUEUE='86400' -e OLLAMA_MAX_LOADED_MODELS='3' --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
 docker exec -it ollama ollama pull mxbai-embed-large
 docker exec -it ollama ollama pull granite3-dense:8b-instruct-q4_0
 kubectl get nodes -o wide
