43bcbc045dee5bc28e6d5474ff8b75645d316d78
kimduquan <kimduquan@gmail.com>
2025-01-05 16:56:55 +0700

    #

--- OpenUP/EPF-lang/src/main/java/epf/lang/messaging/messenger/Messenger.java
+++ OpenUP/EPF-lang/src/main/java/epf/lang/messaging/messenger/Messenger.java
@@ -1,6 +1,8 @@
 package epf.lang.messaging.messenger;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
@@ -18,10 +20,20 @@ import epf.lang.schema.ollama.ChatRequest;
 import epf.lang.schema.ollama.ChatResponse;
 import epf.lang.schema.ollama.Role;
 import epf.naming.Naming;
+import org.eclipse.microprofile.graphql.DefaultValue;
+import org.eclipse.microprofile.graphql.Description;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+import jakarta.persistence.metamodel.Bindable;
+import jakarta.persistence.metamodel.CollectionAttribute;
+import jakarta.persistence.metamodel.EntityType;
+import jakarta.persistence.metamodel.ListAttribute;
+import jakarta.persistence.metamodel.MapAttribute;
+import jakarta.persistence.metamodel.SetAttribute;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.ForbiddenException;
 import jakarta.ws.rs.GET;
@@ -66,17 +78,76 @@ public class Messenger {
 	@PostConstruct
 	void postConstruct() {
 		final StringBuilder prompt = new StringBuilder();
+		final Map<String, EntityType<?>> entityTypes = new HashMap<>();
 		manager.getMetamodel().getEntities().forEach(entityType -> {
+			entityTypes.put(entityType.getJavaType().getName(), entityType);
+		});
+		manager.getMetamodel().getEntities().forEach(entityType -> {
+			final Description description = entityType.getJavaType().getAnnotation(Description.class);
+			if(description != null && !description.value().isEmpty()) {
+				prompt.append(" // ");
+				prompt.append(description.value());
+				prompt.append('\n');
+			}
 			prompt.append("class ");
 			prompt.append(entityType.getName());
 			prompt.append(" {\n");
 			entityType.getAttributes().forEach(attr -> {
-				prompt.append(attr.getJavaType().getCanonicalName());
-				prompt.append(' ');
-				prompt.append(attr.getName());
-				prompt.append(";\n");
+				if(attr.getJavaMember() instanceof Field) {
+					final Field field = (Field) attr.getJavaMember();
+					if(field.getAnnotation(Id.class) == null && field.getAnnotation(Transient.class) == null) {
+						prompt.append('\t');
+						if(attr instanceof Bindable) {
+							final Bindable<?> bindable = (Bindable<?>) attr;
+							if(attr instanceof CollectionAttribute) {
+								prompt.append(String.format("Collection<%s>", bindable.getBindableJavaType().getSimpleName()));
+							}
+							else if(attr instanceof ListAttribute) {
+								prompt.append(String.format("List<%s>", bindable.getBindableJavaType().getSimpleName()));
+							}
+							else if(attr instanceof MapAttribute) {
+								prompt.append(String.format("Map<%s>", bindable.getBindableJavaType().getSimpleName()));
+							}
+							else if(attr instanceof SetAttribute) {
+								prompt.append(String.format("Set<%s>", bindable.getBindableJavaType().getSimpleName()));
+							}
+							else {
+								prompt.append(bindable.getBindableJavaType().getSimpleName());
+							}
+						}
+						else {
+							prompt.append(attr.getJavaType().getSimpleName());
+						}
+						prompt.append(' ');
+						prompt.append(attr.getName());
+						final DefaultValue defaultValue = field.getAnnotation(DefaultValue.class);
+						if(defaultValue != null) {
+							prompt.append(" = ");
+							if(attr.getJavaType() == String.class) {
+								prompt.append('"');
+								prompt.append(defaultValue.value());
+								prompt.append('"');
+							}
+							else if(attr.getJavaType().isEnum()) {
+								prompt.append(attr.getJavaType().getSimpleName());
+								prompt.append('.');
+								prompt.append(defaultValue.value().replace(' ', '_').replace('-', '_'));
+							}
+							else {
+								prompt.append(defaultValue.value());
+							}
+						}
+						prompt.append(";");
+						final Description fieldDesc = field.getAnnotation(Description.class);
+						if(fieldDesc != null && !fieldDesc.value().isEmpty()) {
+							prompt.append(" // ");
+							prompt.append(fieldDesc.value());
+						}
+						prompt.append('\n');
+					}
+				}
 			});
-			prompt.append("};\n");
+			prompt.append("}\n");
 		});
 		LOGGER.info("prompt:" + prompt.toString());
 	}
