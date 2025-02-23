6afc589155cc3da05b33ad82a6ee0e9803dfdba4
kimduquan <kimduquan@gmail.com>
2025-02-23 22:01:45 +0700

    #

---
+++ OpenUP/KubernetesDashboard/kubernetes-dashboard.sh
@@ -0,0 +1,8 @@
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --namespace default
+kubectl apply -f dashboard-adminuser.yaml
+kubectl apply -f dashboard-cluster-admin.yaml
+kubectl -n default create token admin-user
+kubectl apply -f dashboard-adminuser-token.yaml
+kubectl -n default port-forward svc/kubernetes-dashboard-kong-proxy 8443:443
+kubectl get secret admin-user -n default -o jsonpath="{.data.token}" | base64 -d
