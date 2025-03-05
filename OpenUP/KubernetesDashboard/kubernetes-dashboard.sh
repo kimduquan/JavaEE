473d259fe34abc949071aa54b92638384fd20ed5
kimduquan <kimduquan@gmail.com>
2025-03-05 23:17:05 +0700

    #

--- OpenUP/KubernetesDashboard/kubernetes-dashboard.sh
+++ OpenUP/KubernetesDashboard/kubernetes-dashboard.sh
@@ -4,6 +4,6 @@ kubectl apply -f dashboard-adminuser.yaml
 kubectl apply -f dashboard-cluster-admin.yaml
 kubectl apply -f dashboard-adminuser-token.yaml
 kubectl wait pod --for condition=ready -l app.kubernetes.io/name=kong --timeout=300s
-kubectl -n default port-forward svc/kubernetes-dashboard-kong-proxy 8443:443 &
+#kubectl -n default port-forward svc/kubernetes-dashboard-kong-proxy 8443:443 &
 kubectl -n default create token admin-user
 kubectl get secret admin-user -n default -o jsonpath="{.data.token}" | base64 -d
