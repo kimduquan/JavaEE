7eb25116e71dca3beed498975701117adab95e83
kimduquan <kimduquan@gmail.com>
2025-03-09 13:19:56 +0700

    #

--- OpenUP/EPF-gateway/dev.sh
+++ OpenUP/EPF-gateway/dev.sh
@@ -1,6 +1,5 @@
 . ../env.sh
 mkdir src/main/jib
-cp $SOURCE_DIR/dev.p12 ./src/main/jib/
 cp $SOURCE_DIR/public.pem ./src/main/jib/
 mvn clean install -U -Dquarkus.container-image.build=true
 ./stop.sh
--- OpenUP/EPF-gateway/start.sh
+++ OpenUP/EPF-gateway/start.sh
@@ -1,4 +1,3 @@
-kubectl apply -f ./epf-gateway.yaml
+kubectl apply -f ./target/kubernetes/kubernetes.yml
 kubectl wait deployment --for condition=available epf-gateway --timeout=300s
 kubectl wait pod --for condition=ready -l app.kubernetes.io/name=epf-gateway --timeout=300s
-#kubectl port-forward svc/epf-gateway-internal 8080:8080 &
