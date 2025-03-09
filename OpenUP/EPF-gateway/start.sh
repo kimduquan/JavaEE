6d4ed62a836228df8ab85adc8f166978655e9262
kimduquan <kimduquan@gmail.com>
2025-03-09 11:36:50 +0700

    #

--- OpenUP/EPF-gateway/start.sh
+++ OpenUP/EPF-gateway/start.sh
@@ -1,4 +1,4 @@
 kubectl apply -f ./epf-gateway.yaml
 kubectl wait deployment --for condition=available epf-gateway --timeout=300s
 kubectl wait pod --for condition=ready -l app.kubernetes.io/name=epf-gateway --timeout=300s
-kubectl port-forward svc/epf-gateway-internal 8080:8080 &
+#kubectl port-forward svc/epf-gateway-internal 8080:8080 &
