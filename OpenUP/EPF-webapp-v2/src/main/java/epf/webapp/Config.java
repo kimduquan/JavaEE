348005a6e5d62e546fff8f1c252a397b6d061f8b
kimduquan <kimduquan@gmail.com>
2025-03-23 09:51:56 +0700

    #

---
+++ OpenUP/EPF-webapp-v2/src/main/java/epf/webapp/Config.java
@@ -0,0 +1,25 @@
+package epf.webapp;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+
+@ApplicationScoped
+@Named("config")
+public class Config {
+
+	private final String providerURI = "http://localhost:9196/.well-known/openid-configuration";
+	private final String clientId = "oidc-client";
+	private final String clientSecret = "{noop}secret";
+
+	public String getProviderURI() {
+		return providerURI;
+	}
+
+	public String getClientId() {
+		return clientId;
+	}
+
+	public String getClientSecret() {
+		return clientSecret;
+	}
+}
--- OpenUP/EPF-webapp-v2/src/main/java/epf/webapp/WebApp.java
+++ OpenUP/EPF-webapp-v2/src/main/java/epf/webapp/WebApp.java
@@ -2,9 +2,28 @@ package epf.webapp;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.faces.annotation.FacesConfig;
+import jakarta.security.enterprise.authentication.mechanism.http.OpenIdAuthenticationMechanismDefinition;
+import jakarta.security.enterprise.authentication.mechanism.http.openid.ClaimsDefinition;
+import jakarta.security.enterprise.authentication.mechanism.http.openid.LogoutDefinition;
+import jakarta.security.enterprise.authentication.mechanism.http.openid.OpenIdConstant;
 
 @ApplicationScoped
 @FacesConfig
+@OpenIdAuthenticationMechanismDefinition(
+		providerURI = "${config.providerURI}",
+		clientId = "${config.clientId}",
+		clientSecret = "${config.clientSecret}",
+		claimsDefinition = @ClaimsDefinition(
+					callerNameClaim = OpenIdConstant.SUBJECT_IDENTIFIER
+				),
+		logout = @LogoutDefinition(
+					notifyProvider = true,
+					accessTokenExpiry = true,
+					identityTokenExpiry = true
+				),
+		redirectToOriginalResource = true,
+		tokenAutoRefresh = true
+		)
 public class WebApp {
 
 }
