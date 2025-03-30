f76e655c775a773bb713b340a955fbda0952500c
kimduquan <kimduquan@gmail.com>
2025-03-30 15:16:14 +0700

    #

--- OpenUP/WildFly/stop.bat
+++
@@ -1,3 +0,0 @@
-kubectl delete -f ./wildfly.yaml
-kubectl wait pod --for condition=ready=false -l app.kubernetes.io/name=wildfly
-kubectl wait --for=delete -f ./wildfly.yaml
--- OpenUP/WildFly/wildfly.sh
+++
@@ -1,5 +0,0 @@
-. ../env.sh
-cp $SOURCE_DIR/dev.p12 ./
-docker build -t wildfly:1.0.0 ./
-./stop.sh
-./start.sh
