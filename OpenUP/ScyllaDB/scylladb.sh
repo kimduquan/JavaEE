15daa8836d4dff636444cf65c7ae9fa163449ba2
kimduquan <kimduquan@gmail.com>
2025-04-05 04:02:26 +0700

    #

--- OpenUP/ScyllaDB/scylladb.sh
+++ OpenUP/ScyllaDB/scylladb.sh
@@ -1,18 +1,1 @@
-helm repo add scylla https://scylla-operator-charts.storage.googleapis.com/stable
-helm repo update
-
-./cert-manager.sh
-
-helm install scylla-operator scylla/scylla-operator --create-namespace --namespace scylla-operator
-kubectl wait -n scylla-operator --for=condition=ready pod -l "app.kubernetes.io/name=scylla-operator"
-kubectl wait -n scylla-operator --for=condition=ready pod -l "app.kubernetes.io/name=webhook-server"
-kubectl -n scylla-operator get all
-
-helm install scylla-manager scylla/scylla-manager --create-namespace --namespace scylla-manager
-kubectl wait -n scylla-manager --for=condition=ready pod -l "app.kubernetes.io/name=scylla-manager"
-kubectl wait -n scylla-manager --for=condition=ready pod -l "app.kubernetes.io/name=scylla-manager-controller"
-kubectl -n scylla-manager get all
-
-helm install scylla scylla/scylla --create-namespace --namespace scylla
-kubectl wait -n scylla --for=condition=ready pod -l "app.kubernetes.io/name=scylla"
-kubectl -n scylla get all
+helm install scylladb oci://registry-1.docker.io/bitnamicharts/scylladb
