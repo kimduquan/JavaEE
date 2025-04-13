44bb26ce1ec0181feb24333c70c823b79d8ce163
kimduquan <kimduquan@gmail.com>
2025-04-13 14:50:43 +0700

    #

--- OpenUP/WildFly/wildfly.sh
+++ OpenUP/WildFly/wildfly.sh
@@ -1,5 +1,5 @@
 kubectl delete secret oidc
-kubectl create secret generic oidc --from-literal=OIDC_CLIENT_SECRET='Ux3NYf3FPWGtFuCIgS7wmEquW8NHUTmX'
+kubectl create secret generic oidc --from-literal=OIDC_CLIENT_SECRET='4GFBcFZ0lrJVHUBWz2nGh1w5VWhOXePV'
 kubectl delete cm oidc
-kubectl create cm oidc --from-literal=OIDC_PROVIDER_URI='http://host.docker.internal/realms/EPF/.well-known/openid-configuration' --from-literal=OIDC_CLIENT_ID='oidc-client'
+kubectl create cm oidc --from-literal=OIDC_PROVIDER_URI='http://host.docker.internal/realms/EPF-dev/.well-known/openid-configuration' --from-literal=OIDC_CLIENT_ID='account'
 helm install wildfly oci://registry-1.docker.io/bitnamicharts/wildfly -f values-wildfly.yaml
