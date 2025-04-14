c5d02b3478be6d3f152b602e68d60bca3340a19b
kimduquan <kimduquan@gmail.com>
2025-04-15 00:26:06 +0700

    #

---
+++ OpenUP/EPF-webapp-v2/src/main/java/epf/webapp/client/Config.java
@@ -0,0 +1,16 @@
+package epf.webapp.client;
+
+import java.util.Map;
+import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import epf.webapp.util.AuthFilter;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.QueryParam;
+
+@RegisterRestClient
+@RegisterProvider(AuthFilter.class)
+public interface Config {
+
+	@GET
+	Map<String, String> getProperties(@QueryParam("name") final String name) throws Exception;
+}
