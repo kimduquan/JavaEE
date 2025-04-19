1715dae3e9c859b93188917f861aa645a46643fd
kimduquan <kimduquan@gmail.com>
2025-04-19 19:45:15 +0700

    #

--- OpenUP/EPF-persistence/dev.sh
+++ OpenUP/EPF-persistence/dev.sh
@@ -1,6 +1,5 @@
 . ../env.sh
-. ../native_env.sh
 . ./config.sh
-mvn clean install -U -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true -P native
+mvn clean install -U -Dquarkus.container-image.build=true
 ./stop.sh
 ./start.sh
