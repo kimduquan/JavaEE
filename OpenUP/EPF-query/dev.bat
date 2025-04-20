9f2fdaedf0e3d59261e8b920e25969aec968b93b
kimduquan <kimduquan@gmail.com>
2025-04-20 10:24:06 +0700

    #

--- OpenUP/EPF-query/dev.bat
+++ OpenUP/EPF-query/dev.bat
@@ -1,7 +1,6 @@
 setlocal
 call ../env.bat
 call ../config.bat
-call config.bat
 mkdir -p ./src/main/jib/home/jboss/
 copy %USER_DIR%\EPF-query.trace.db ./src/main/jib/home/jboss/
 copy %USER_DIR%\EPF-query.mv.db ./src/main/jib/home/jboss/
--- OpenUP/EPF-query/dev.sh
+++ OpenUP/EPF-query/dev.sh
@@ -1,5 +1,4 @@
 . ../env.sh
-. ./config.sh
 mkdir -p ./src/main/jib/home/jboss/
 cp $USER_DIR/EPF-query.trace.db ./src/main/jib/home/jboss/
 cp $USER_DIR/EPF-query.mv.db ./src/main/jib/home/jboss/
