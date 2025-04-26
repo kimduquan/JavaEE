1526e0851f9fbf74811169b0f4c38b3771ebe275
kimduquan <kimduquan@gmail.com>
2025-04-26 07:32:09 +0700

    #

--- OpenUP/Ollama/ollama.sh
+++ OpenUP/Ollama/ollama.sh
@@ -2,7 +2,7 @@ kubectl delete -f ollama.yaml
 docker rm ollama
 docker run -d -e OLLAMA_KEEP_ALIVE='24h' -e OLLAMA_NUM_PARALLEL='1' -e OLLAMA_MAX_QUEUE='86400' -e OLLAMA_MAX_LOADED_MODELS='3' --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
 docker exec -it ollama ollama pull granite-embedding:278m
-docker exec -it ollama ollama pull granite3.1-dense
+docker exec -it ollama ollama pull granite3.3
 kubectl get nodes -o wide
 kubectl apply -f ollama.yaml
 kubectl wait deployment --for condition=available ollama
