89bf6092206db53f9c7e99121fd219ca835077e8
kimduquan <kimduquan@gmail.com>
2025-06-21 17:37:44 +0700

    #

--- OpenUP/EPF-persistence/start.sh
+++ OpenUP/EPF-persistence/start.sh
@@ -1,3 +1,4 @@
-kubectl apply -f target/kubernetes/kubernetes.yml
+#kubectl apply -f target/kubernetes/kubernetes.yml
+helm install epf-persistence target/helm/kubernetes/epf-persistence
 kubectl wait deployment --for condition=available --timeout=60s epf-persistence
 kubectl wait pod --for condition=ready --timeout=60s -l app.kubernetes.io/name=epf-persistence
