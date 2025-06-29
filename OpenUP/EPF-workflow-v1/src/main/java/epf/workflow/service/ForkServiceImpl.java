1515bfa068b2ce68a88037f9e6e352c7dcb72e1b
kimduquan <kimduquan@gmail.com>
2025-06-29 12:22:39 +0700

    #

--- OpenUP/EPF-workflow-v1/src/main/java/epf/workflow/service/ForkServiceImpl.java
+++ OpenUP/EPF-workflow-v1/src/main/java/epf/workflow/service/ForkServiceImpl.java
@@ -1,9 +1,14 @@
 package epf.workflow.service;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicBoolean;
 import org.eclipse.microprofile.context.ManagedExecutor;
 import epf.workflow.schema.Error;
+import epf.workflow.schema.RuntimeError;
 import epf.workflow.schema.RuntimeExpressionArguments;
 import epf.workflow.schema.Workflow;
 import epf.workflow.spi.ForkService;
@@ -23,11 +28,27 @@ public class ForkServiceImpl implements ForkService {
 
 	@Override
 	public Object fork(final Workflow workflow, final Object workflowInput, final RuntimeExpressionArguments arguments, final ForkTask task, final AtomicBoolean end) throws Error {
+		final List<Callable<Object>> branchTasks = new ArrayList<>();
 		task.getFork().getBranches().forEach((branchTaskName, branchTask) -> {
 			final URI branchURI = URI.create(arguments.getTask().getReference()).resolve(branchTaskName);
-			executor.submit(() -> taskService.start(workflow, workflowInput, arguments, branchTaskName, branchURI, branchTask, arguments.getInput(), end));
+			branchTasks.add(() -> taskService.start(workflow, workflowInput, arguments, branchTaskName, branchURI, branchTask, arguments.getInput(), end));
 		});
-		return null;
+		try {
+			Object taskOutput;
+			if(task.getFork().isCompete()) {
+				final List<Object> outputs = new ArrayList<>();
+				for(Future<Object> future : executor.invokeAll(branchTasks)) {
+					outputs.add(future.get());
+				}
+				taskOutput = outputs;
+			}
+			else {
+				taskOutput = executor.invokeAny(branchTasks);
+			}
+			return taskOutput;
+		}
+		catch(Exception ex) {
+			throw new RuntimeError(ex);
+		}
 	}
-
 }
