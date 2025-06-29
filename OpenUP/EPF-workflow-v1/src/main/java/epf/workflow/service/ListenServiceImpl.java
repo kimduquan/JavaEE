cd0878c2ee26ba08a8616fc0acf45384b57bb73a
kimduquan <kimduquan@gmail.com>
2025-06-29 19:59:48 +0700

    #

--- OpenUP/EPF-workflow-v1/src/main/java/epf/workflow/service/ListenServiceImpl.java
+++ OpenUP/EPF-workflow-v1/src/main/java/epf/workflow/service/ListenServiceImpl.java
@@ -1,5 +1,8 @@
 package epf.workflow.service;
 
+import org.eclipse.jnosql.communication.semistructured.CriteriaCondition;
+import org.eclipse.jnosql.communication.semistructured.SelectQuery;
+import org.eclipse.jnosql.mapping.column.ColumnTemplate;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import epf.naming.Naming;
 import epf.workflow.schema.Error;
@@ -9,9 +12,13 @@ import epf.workflow.schema.Workflow;
 import epf.workflow.spi.ListenService;
 import epf.workflow.task.schema.ListenTask;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class ListenServiceImpl implements ListenService {
+	
+	@Inject
+	transient ColumnTemplate columnTemplate;
 
 	@Override
 	public Object listen(final Workflow workflow, final Object workflowInput, final RuntimeExpressionArguments arguments, final ListenTask task, final Object taskInput) throws Error {
@@ -20,6 +27,7 @@ public class ListenServiceImpl implements ListenService {
 
 	@Incoming(Naming.Workflow.EVENTS)
 	public void listen(final EventProperties event) throws Error {
-		
+		final SelectQuery query = SelectQuery.builder().from(Naming.Workflow.EVENTS).where(CriteriaCondition.eq("", "")).build();
+		columnTemplate.select(query);
 	}
 }
