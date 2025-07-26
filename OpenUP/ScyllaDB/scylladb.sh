a463aaf0ebb5e9c672a3d624776fc9c4c38df7fa
kimduquan <kimduquan@gmail.com>
2025-07-26 14:32:16 +0700

    #

--- OpenUP/ScyllaDB/scylladb.sh
+++ OpenUP/ScyllaDB/scylladb.sh
@@ -1,3 +1,3 @@
 kubectl delete configmap scylla
 kubectl create cm scylla --from-file=scylla.yaml
-helm install scylladb oci://registry-1.docker.io/bitnamicharts/scylladb -f values.yaml --set existingConfiguration=scylla
+helm install scylladb oci://registry-1.docker.io/bitnamicharts/scylladb -f values-scylladb.yaml --set existingConfiguration=scylla
