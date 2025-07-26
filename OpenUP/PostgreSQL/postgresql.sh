9b5c9644c5669c34473e81c663004cda78c53fe9
kimduquan <kimduquan@gmail.com>
2025-07-26 14:50:35 +0700

    #

--- OpenUP/PostgreSQL/postgresql.sh
+++ OpenUP/PostgreSQL/postgresql.sh
@@ -1,3 +1,1 @@
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo update
-helm install postgresql bitnami/postgresql --set auth.postgresPassword=090323508 --set auth.database=epf
+helm upgrade postgresql oci://registry-1.docker.io/bitnamicharts/postgresql -f values-postgresql.yaml
