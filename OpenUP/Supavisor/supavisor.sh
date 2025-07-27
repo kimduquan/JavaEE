bcc3151a3ff6c3f64a588a63126ad25ca2774894
kimduquan <kimduquan@gmail.com>
2025-07-27 10:20:16 +0700

    #

--- OpenUP/Supavisor/supavisor.sh
+++ OpenUP/Supavisor/supavisor.sh
@@ -1,3 +1,6 @@
 kubectl delete secret supavisor
+postgres_password=(kubectl get secret postgresql -o jsonpath="{.data.postgres-password}" | base64 --decode)
 kubectl create secret generic supavisor --from-literal=SECRET_KEY_BASE="$(openssl rand -base64 64)"
 kubectl apply -f kubernetes.yml
+kubectl wait deployment --for condition=available --timeout=60s supavisor
+kubectl wait pod --for condition=ready --timeout=60s -l app.kubernetes.io/name=supavisor
