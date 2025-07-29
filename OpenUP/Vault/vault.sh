e215a7cc8e96a3d7e4ba19f53830d34bb5a28879
kimduquan <kimduquan@gmail.com>
2025-07-29 22:50:43 +0700

    #

--- OpenUP/Vault/vault.sh
+++ OpenUP/Vault/vault.sh
@@ -1,3 +1,5 @@
+#helm repo add secrets-store-csi-driver https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts
+#helm install csi-secrets-store secrets-store-csi-driver/secrets-store-csi-driver
 helm install vault oci://registry-1.docker.io/bitnamicharts/vault -f values-vault.yaml
 #kubectl exec vault-server-0 -- vault operator init
 #kubectl exec vault-server-0 -i -t -- vault operator unseal
