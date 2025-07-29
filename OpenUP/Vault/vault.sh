448e6aeb1883e616b0226f4fbd1b08650066dfb8
kimduquan <kimduquan@gmail.com>
2025-07-29 21:19:14 +0700

    #

--- OpenUP/Vault/vault.sh
+++ OpenUP/Vault/vault.sh
@@ -1,1 +1,3 @@
 helm install vault oci://registry-1.docker.io/bitnamicharts/vault -f values-vault.yaml
+#kubectl exec vault-server-0 -- vault operator init
+#kubectl exec vault-server-0 -i -t -- vault operator unseal
