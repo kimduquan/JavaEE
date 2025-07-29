4272a05ea911cc8e902bc71b49734b2eda4cf8af
kimduquan <kimduquan@gmail.com>
2025-07-29 23:55:02 +0700

    #

---
+++ OpenUP/secrets-store-csi-driver/secrets-store-csi-driver.sh
@@ -0,0 +1,2 @@
+helm repo add secrets-store-csi-driver https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts
+helm install csi-secrets-store secrets-store-csi-driver/secrets-store-csi-driver -f values.yaml
