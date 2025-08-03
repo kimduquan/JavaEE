551846c934f508c187177bf4ca72657d1e10e46a
kimduquan <kimduquan@gmail.com>
2025-08-03 22:08:58 +0700

    #

--- OpenUP/Supavisor/supavisor.sh
+++ OpenUP/Supavisor/supavisor.sh
@@ -1,5 +1,5 @@
 kubectl delete secret supavisor
-kubectl create secret generic supavisor --from-literal=SECRET_KEY_BASE="$(openssl rand -hex 32)"
+kubectl create secret generic supavisor --from-literal=SECRET_KEY_BASE="$(openssl rand -hex 32)" --from-literal=API_JWT_SECRET="$(openssl rand -hex 32)" --from-literal=METRICS_JWT_SECRET="$(openssl rand -hex 32)" --from-literal=VAULT_ENC_KEY="$(openssl rand -hex 16)"
 kubectl apply -f kubernetes.yml
 kubectl wait deployment --for condition=available --timeout=60s supavisor
 kubectl wait pod --for condition=ready --timeout=60s -l app.kubernetes.io/name=supavisor
