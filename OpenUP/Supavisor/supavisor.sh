71f7d96ae95dfc12254edd30505dcb9961de36e4
kimduquan <kimduquan@gmail.com>
2025-08-03 23:12:48 +0700

    #

--- OpenUP/Supavisor/supavisor.sh
+++ OpenUP/Supavisor/supavisor.sh
@@ -3,3 +3,4 @@ kubectl create secret generic supavisor --from-literal=SECRET_KEY_BASE="$(openss
 kubectl apply -f kubernetes.yml
 kubectl wait deployment --for condition=available --timeout=60s supavisor
 kubectl wait pod --for condition=ready --timeout=60s -l app.kubernetes.io/name=supavisor
+#psql postgres://epf.epf:Password1234@supavisor.default.svc.cluster.local:5452/epf
