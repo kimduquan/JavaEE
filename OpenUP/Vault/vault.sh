40e6f8f484646d08cf34fd0e95d066b9ccadead8
kimduquan <kimduquan@gmail.com>
2025-08-04 22:25:14 +0700

    #

--- OpenUP/Vault/vault.sh
+++ OpenUP/Vault/vault.sh
@@ -1,5 +1,10 @@
 #helm repo add secrets-store-csi-driver https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts
 #helm install csi-secrets-store secrets-store-csi-driver/secrets-store-csi-driver
-helm install vault oci://registry-1.docker.io/bitnamicharts/vault -f values-vault.yaml
+#helm install vault oci://registry-1.docker.io/bitnamicharts/vault -f values-vault.yaml
 #kubectl exec vault-server-0 -- vault operator init
 #kubectl exec vault-server-0 -i -t -- vault operator unseal
+helm repo add hashicorp https://helm.releases.hashicorp.com
+helm repo update
+helm install vault hashicorp/vault --set "server.enabled=true" --set "injector.enabled=false" --set "csi.enabled=true"
+#kubectl exec vault-0 -- vault operator init
+#kubectl exec vault-0 -i -t -- vault operator unseal
