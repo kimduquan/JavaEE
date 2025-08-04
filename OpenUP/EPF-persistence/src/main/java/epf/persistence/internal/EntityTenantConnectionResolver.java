a6604909d654f6cd5fede0430604cc15535530cd
kimduquan <kimduquan@gmail.com>
2025-08-05 00:11:53 +0700

    #

--- OpenUP/EPF-persistence/src/main/java/epf/persistence/internal/EntityTenantConnectionResolver.java
+++ OpenUP/EPF-persistence/src/main/java/epf/persistence/internal/EntityTenantConnectionResolver.java
@@ -5,16 +5,20 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
 import io.agroal.api.AgroalDataSource;
+import io.agroal.api.configuration.AgroalConnectionPoolConfiguration.TransactionRequirement;
 import io.agroal.api.configuration.AgroalDataSourceConfiguration;
 import io.agroal.api.configuration.supplier.AgroalDataSourceConfigurationSupplier;
 import io.agroal.api.security.NamePrincipal;
 import io.agroal.api.security.SimplePassword;
+import io.agroal.narayana.NarayanaTransactionIntegration;
 import io.agroal.pool.DataSource;
 import io.quarkus.hibernate.orm.PersistenceUnitExtension;
 import io.quarkus.hibernate.orm.runtime.customized.QuarkusConnectionProvider;
 import io.quarkus.hibernate.orm.runtime.tenant.TenantConnectionResolver;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.transaction.TransactionManager;
+import jakarta.transaction.TransactionSynchronizationRegistry;
 
 @ApplicationScoped
 @PersistenceUnitExtension
@@ -33,13 +37,30 @@ public class EntityTenantConnectionResolver implements TenantConnectionResolver 
 	@Inject
     @ConfigProperty(name = "datasource.jdbc.url.format")
 	String format;
+	
+	@Inject
+    @ConfigProperty(name = "datasource.connection.pool.size")
+	int connectionPoolSize;
+	
+	@Inject
+	TransactionManager transactionManager;
+	
+	@Inject
+    TransactionSynchronizationRegistry transactionSynchronizationRegistry;
 
 	@Override
 	public ConnectionProvider resolve(final String tenantId) {
 		return connectionProviders.computeIfAbsent(tenantId, tenant -> {
 			final String jdbcUrl = String.format(format, tenant);
 			final AgroalDataSourceConfigurationSupplier supplier = new AgroalDataSourceConfigurationSupplier();
-			supplier.connectionPoolConfiguration().connectionFactoryConfiguration().credential(new NamePrincipal(userName + "." + tenant)).credential(new SimplePassword(password)).jdbcUrl(jdbcUrl);
+			supplier.connectionPoolConfiguration()
+			.maxSize(connectionPoolSize)
+			.transactionIntegration(new NarayanaTransactionIntegration(transactionManager, transactionSynchronizationRegistry))
+			.transactionRequirement(TransactionRequirement.STRICT)
+			.connectionFactoryConfiguration()
+			.credential(new NamePrincipal(userName + "." + tenant))
+			.credential(new SimplePassword(password))
+			.jdbcUrl(jdbcUrl);
 			final AgroalDataSourceConfiguration config = supplier.get();
 			final AgroalDataSource dataSource = new DataSource(config);
 			return new QuarkusConnectionProvider(dataSource);
