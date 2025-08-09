47b4dc3cfe578c4a0d2585eec912a0813d278e36
kimduquan <kimduquan@gmail.com>
2025-08-09 18:42:21 +0700

    #

--- OpenUP/WildFly/wildfly.sh
+++ OpenUP/WildFly/wildfly.sh
@@ -1,6 +1,6 @@
 kubectl delete secret oidc
-kubectl create secret generic oidc --from-literal=OIDC_CLIENT_SECRET='4GFBcFZ0lrJVHUBWz2nGh1w5VWhOXePV'
+kubectl create secret generic oidc --from-literal=OIDC_CLIENT_SECRET='kcG3e9FAbsiZmRzZydEQee3cZLmyci3W'
 kubectl delete cm oidc
-kubectl create cm oidc --from-literal=OIDC_PROVIDER_URI='https://touching-pika-enabled.ngrok-free.app/realms/EPF-dev/.well-known/openid-configuration' --from-literal=OIDC_CLIENT_ID='account'
+kubectl create cm oidc --from-literal=OIDC_PROVIDER_URI='https://keycloak.default.svc.cluster.local/auth/realms/EPF-dev/.well-known/openid-configuration' --from-literal=OIDC_CLIENT_ID='account'
 helm install wildfly oci://registry-1.docker.io/bitnamicharts/wildfly -f values-wildfly.yaml
 #/opt/bitnami/wildfly/bin/jboss-cli.sh --connect --controller=127.0.0.1:9990
