fb1c1c0e27554f02eaed2682068e53c435ffd9d2
kimduquan <kimduquan@gmail.com>
2025-08-09 19:36:15 +0700

    #

--- OpenUP/Keycloak/keycloak.sh
+++ OpenUP/Keycloak/keycloak.sh
@@ -1,3 +1,2 @@
-kubectl delete cm keycloak-custom-headers
-kubectl create cm keycloak-custom-headers --from-literal=ngrok-skip-browser-warning=true
 helm install keycloak oci://registry-1.docker.io/bitnamicharts/keycloak -f values-keycloak.yaml
+kubectl apply -f proxy.yaml
