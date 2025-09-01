14148a83758b77c66be719812e24d8ad2c6a1a40
kimduquan <kimduquan@gmail.com>
2025-09-01 20:16:42 +0700

    #

--- OpenUP/EPF-event/src/main/java/epf/event/EventCache.java
+++ OpenUP/EPF-event/src/main/java/epf/event/EventCache.java
@@ -1,8 +1,6 @@
 package epf.event;
 
-import java.util.Optional;
-import org.eclipse.jnosql.communication.keyvalue.KeyValueEntity;
-import org.eclipse.jnosql.mapping.keyvalue.KeyValueTemplate;
+import org.eclipse.jnosql.mapping.graph.GraphTemplate;
 import epf.event.schema.Link;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -11,15 +9,14 @@ import jakarta.inject.Inject;
 public class EventCache {
 	
 	@Inject
-	transient KeyValueTemplate cache;
+	transient GraphTemplate graph;
 	
 	public void put(final String key, final Link link) {
-		cache.put(KeyValueEntity.of(key, link));
+		graph.insert(link);
 	}
 	
 	public Link remove(final String key) {
-		final Optional<Link> value = cache.get(key, Link.class);
-		cache.delete(key);
-		return value.orElse(null);
+		graph.delete(Link.class, key);
+		return null;
 	}
 }
