9b7648755b4724ee6048fbfa2097df278e57cb98
kimduquan <kimduquan@gmail.com>
2025-09-03 21:56:17 +0700

    #

---
+++ OpenUP/EPF-webapp-v2/src/main/java/epf/webapp/view/Principal.java
@@ -0,0 +1,146 @@
+package epf.webapp.view;
+
+public class Principal {
+	
+	private String subject;
+	private String name;
+	private String familyName;
+	private String givenName;
+	private String middleName;
+	private String nickname;
+	private String preferredUsername;
+	private String profile;
+	private String picture;
+	private String gender;
+	private String birthdate;
+	private String zoneinfo;
+	private String locale;
+	private String updatedAt;
+	private String email;
+	private String emailVerified;
+	private String address;
+	private String phoneNumber;
+	private String phoneNumberVerified;
+	private String website;
+	
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFamilyName() {
+		return familyName;
+	}
+	public void setFamilyName(String familyName) {
+		this.familyName = familyName;
+	}
+	public String getGivenName() {
+		return givenName;
+	}
+	public void setGivenName(String givenName) {
+		this.givenName = givenName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getPreferredUsername() {
+		return preferredUsername;
+	}
+	public void setPreferredUsername(String preferredUsername) {
+		this.preferredUsername = preferredUsername;
+	}
+	public String getProfile() {
+		return profile;
+	}
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+	public String getZoneinfo() {
+		return zoneinfo;
+	}
+	public void setZoneinfo(String zoneinfo) {
+		this.zoneinfo = zoneinfo;
+	}
+	public String getLocale() {
+		return locale;
+	}
+	public void setLocale(String locale) {
+		this.locale = locale;
+	}
+	public String getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(String updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEmailVerified() {
+		return emailVerified;
+	}
+	public void setEmailVerified(String emailVerified) {
+		this.emailVerified = emailVerified;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getPhoneNumberVerified() {
+		return phoneNumberVerified;
+	}
+	public void setPhoneNumberVerified(String phoneNumberVerified) {
+		this.phoneNumberVerified = phoneNumberVerified;
+	}
+	public String getWebsite() {
+		return website;
+	}
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+}
