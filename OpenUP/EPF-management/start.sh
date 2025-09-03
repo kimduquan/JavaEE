6b1c493c50ea0ba7f75fb6f249e71e6e0b67486d
kimduquan <kimduquan@gmail.com>
2025-09-03 23:10:32 +0700

    #

--- OpenUP/EPF-management/start.sh
+++ OpenUP/EPF-management/start.sh
@@ -1,3 +1,3 @@
 kubectl apply -f target/kubernetes/kubernetes.yml
-kubectl wait deployment --for condition=available epf-net
-kubectl wait pod --for condition=ready -l app.kubernetes.io/name=epf-net
+kubectl wait deployment --for condition=available epf-management
+kubectl wait pod --for condition=ready -l app.kubernetes.io/name=epf-management
