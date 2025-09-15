1fb5f85720dd7b4684136c61a9b6ce4667599352
kimduquan <kimduquan@gmail.com>
2025-09-16 00:32:11 +0700

    #

---
+++ OpenUP/EPF-workflow-webapp/src/main/java/epf/webapp/workflow/Session.java
@@ -0,0 +1,13 @@
+package epf.webapp.workflow;
+
+import jakarta.enterprise.context.SessionScoped;
+
+@SessionScoped
+public class Session extends epf.management.schema.Session {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
