a8fbc22104593564ddf0d452dc5b6dd254cfcd2f
kimduquan <kimduquan@gmail.com>
2025-09-20 16:05:55 +0700

    #

--- OpenUP/native_env.bat
+++ OpenUP/native_env.bat
@@ -1,4 +1,3 @@
-set GRAALVM_HOME=C:\graalvm-ce-java17-22.3.3
-set JAVA_HOME=%GRAALVM_HOME%
-set PATH=%GRAALVM_HOME%/bin;%PATH%
+set GRAALVM_HOME=C:\graalvm-community-openjdk-25+37.1
+::set PATH=%GRAALVM_HOME%/bin;%PATH%
 call "D:\Installed\VisualStudio\VC\Auxiliary\Build\vcvars64.bat"
