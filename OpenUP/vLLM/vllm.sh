06c58bb0167c448d1d6b12dd7891fe5bbc79785c
kimduquan <kimduquan@gmail.com>
2025-09-21 13:43:44 +0700

    #

--- OpenUP/vLLM/vllm.sh
+++ OpenUP/vLLM/vllm.sh
@@ -1,3 +1,3 @@
 #helm repo add vllm https://vllm-project.github.io/production-stack
 #helm install vllm vllm/vllm-stack -f values.yaml
-docker run --runtime nvidia --gpus all -v ~/.cache/huggingface:/root/.cache/huggingface --env "HUGGING_FACE_HUB_TOKEN=" -p 8000:8000 --ipc=host vllm/vllm-openai:latest --model ibm-granite/granite-3.3-8b-instruct
+docker run --runtime nvidia --gpus all -v ~/.cache/huggingface:/root/.cache/huggingface --env "HUGGING_FACE_HUB_TOKEN=" -p 8000:8000 --ipc=host vllm/vllm-openai:latest --model neuralmagic/Mistral-7B-Instruct-v0.3-GPTQ-4bit --gpu-memory-utilization 0.87 --max_model_len 23296
