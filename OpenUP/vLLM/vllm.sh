18dd75ddb0e8b073966423c3a3c56f4cead4c66c
kimduquan <kimduquan@gmail.com>
2025-09-21 14:13:51 +0700

    #

--- OpenUP/vLLM/vllm.sh
+++ OpenUP/vLLM/vllm.sh
@@ -1,3 +1,4 @@
-#helm repo add vllm https://vllm-project.github.io/production-stack
-#helm install vllm vllm/vllm-stack -f values.yaml
-docker run --runtime nvidia --gpus all -v ~/.cache/huggingface:/root/.cache/huggingface --env "HUGGING_FACE_HUB_TOKEN=" -p 8000:8000 --ipc=host vllm/vllm-openai:latest --model neuralmagic/Mistral-7B-Instruct-v0.3-GPTQ-4bit --gpu-memory-utilization 0.87 --max_model_len 23296
+helm repo add vllm https://vllm-project.github.io/production-stack
+helm install vllm vllm/vllm-stack -f values.yaml
+#docker run --runtime nvidia --gpus all -v ~/.cache/huggingface:/root/.cache/huggingface --env "HUGGING_FACE_HUB_TOKEN=" -p 8000:8000 --ipc=host vllm/vllm-openai:latest --model neuralmagic/Mistral-7B-Instruct-v0.3-GPTQ-4bit --gpu-memory-utilization 0.87 --max_model_len 23296
+#docker run --runtime nvidia --gpus all -v ~/.cache/huggingface:/root/.cache/huggingface --env "HUGGING_FACE_HUB_TOKEN=" -p 8000:8000 --ipc=host vllm/vllm-openai:latest --model ModelCloud/Mistral-Nemo-Instruct-2407-gptq-4bit --gpu-memory-utilization 0.87
