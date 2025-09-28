fb210f222e1e3d533d494efd6c0933df84b42138
kimduquan <kimduquan@gmail.com>
2025-09-29 00:09:30 +0700

    #

--- OpenUP/Lustre/lustre.sh
+++ OpenUP/Lustre/lustre.sh
@@ -1,3 +1,8 @@
+#sudo zgenhostid -f -o /etc/hostid
+#sudo dnf config-manager --add-repo https://downloads.whamcloud.com/public/lustre/latest-release/el8.10/server
+#sudo dnf config-manager --add-repo https://downloads.whamcloud.com/public/e2fsprogs/latest/el8
+#sudo dnf install --enablerepo=downloads.whamcloud.com_public_lustre_latest-release_el8.10_server --enablerepo=downloads.whamcloud.com_public_e2fsprogs_latest_el8 kernel
+#sudo dnf install --enablerepo=downloads.whamcloud.com_public_lustre_latest-release_el8.10_server --enablerepo=downloads.whamcloud.com_public_e2fsprogs_latest_el8 --nogpgcheck kmod-lustre kmod-lustre-osd-zfs e2fsprogs
 git clone https://github.com/HewlettPackard/lustre-csi-driver.git .
 git checkout master
 cd charts
