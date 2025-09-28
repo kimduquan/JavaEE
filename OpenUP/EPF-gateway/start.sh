a13169921cbfc67bcec9a72388d762c6f44c753a
kimduquan <kimduquan@gmail.com>
2025-09-28 20:08:31 +0700

    #

--- OpenUP/EPF-gateway/start.sh
+++ OpenUP/EPF-gateway/start.sh
@@ -1,3 +1,4 @@
 kubectl apply -f ./target/kubernetes/kubernetes.yml
 kubectl wait deployment --for condition=available epf-gateway --timeout=300s
 kubectl wait pod --for condition=ready -l app.kubernetes.io/name=epf-gateway --timeout=300s
+kubectl autoscale deployment epf-gateway --max 3
--- OpenUP/EPF-gateway/stop.sh
+++ OpenUP/EPF-gateway/stop.sh
@@ -1,3 +1,4 @@
+kubectl delete hpa epf-gateway
 kubectl delete -f ./target/kubernetes/kubernetes.yml
 kubectl wait pod --for condition=ready=false -l app.kubernetes.io/name=epf-gateway
 kubectl wait deployment --for=delete -l app.kubernetes.io/name=epf-gateway
