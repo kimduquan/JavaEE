d7f6d2f847c6796dbcdbaf904064fd701c199a62
kimduquan <kimduquan@gmail.com>
2025-10-01 03:20:27 +0700

    #

--- OpenUP/Jaeger/jaeger.sh
+++ OpenUP/Jaeger/jaeger.sh
@@ -1,7 +1,1 @@
-kubectl delete -f ./jaeger.yaml
-kubectl wait pod --for condition=ready=false -l app.kubernetes.io/name=jaeger
-kubectl wait --for=delete -f ./jaeger.yaml
-
-kubectl apply -f ./jaeger.yaml
-kubectl wait deployment --for condition=available jaeger --timeout=300s
-kubectl wait pod --for condition=ready -l app.kubernetes.io/name=jaeger --timeout=300s
+helm install jaeger oci://registry-1.docker.io/bitnamicharts/jaeger -f values-jaeger.yaml
