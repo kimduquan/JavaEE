120c4b956022dad55bdb44e8e9af711c916c9139
kimduquan <kimduquan@gmail.com>
2025-10-19 17:19:51 +0700

    #

--- OpenUP/EPF-persistence/src/main/java/epf/persistence/config/Schema.java
+++ OpenUP/EPF-persistence/src/main/java/epf/persistence/config/Schema.java
@@ -1,14 +1,15 @@
-package epf.query.schema;
+package epf.persistence.config;
 
 import epf.schema.utility.EntityEvent;
 import epf.schema.utility.EntityTransaction;
 import epf.schema.utility.PostPersist;
 import epf.schema.utility.PostRemove;
 import epf.schema.utility.PostUpdate;
+import epf.util.json.ext.Adapter;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @RegisterForReflection(targets = {
-		EntityEvent.class,EntityTransaction.class,PostPersist.class,PostRemove.class,PostUpdate.class
+		EntityEvent.class,EntityTransaction.class,PostPersist.class,PostRemove.class,PostUpdate.class,Adapter.class
 })
 public interface Schema {
 
--- OpenUP/EPF-query/src/main/java/epf/query/config/Schema.java
+++ OpenUP/EPF-query/src/main/java/epf/query/config/Schema.java
@@ -1,14 +1,15 @@
-package epf.persistence.schema;
+package epf.query.config;
 
 import epf.schema.utility.EntityEvent;
 import epf.schema.utility.EntityTransaction;
 import epf.schema.utility.PostPersist;
 import epf.schema.utility.PostRemove;
 import epf.schema.utility.PostUpdate;
+import epf.util.json.ext.Adapter;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @RegisterForReflection(targets = {
-		EntityEvent.class,EntityTransaction.class,PostPersist.class,PostRemove.class,PostUpdate.class
+		EntityEvent.class,EntityTransaction.class,PostPersist.class,PostRemove.class,PostUpdate.class,Adapter.class
 })
 public interface Schema {
 
