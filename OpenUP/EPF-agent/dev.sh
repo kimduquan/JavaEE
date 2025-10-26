018de9e17abb86ea5cfcc7ec03014c10ba823a8f
kimduquan <kimduquan@gmail.com>
2025-10-27 00:39:18 +0700

    #

--- OpenUP/EPF-agent/dev.sh
+++ OpenUP/EPF-agent/dev.sh
@@ -1,7 +1,7 @@
 docker build -t epf-agent:1.0.0 .
 kubectl delete secret epf-agent
 kubectl delete hpa epf-agent
-kubectl create secret generic epf-agent --from-literal=OPENAI_API_KEY="$(openssl rand -hex 32)"
+kubectl create secret generic epf-agent --from-literal=MODEL_API_KEY="$(openssl rand -hex 32)"
 kubectl apply -f kubernetes.yml
 kubectl wait deployment --for condition=available --timeout=60s epf-agent
 kubectl wait pod --for condition=ready --timeout=60s -l app.kubernetes.io/name=epf-agent
--- OpenUP/EPF-agent/main.py
+++ OpenUP/EPF-agent/main.py
@@ -1,15 +1,27 @@
-from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+import os
+from agents.models import LiteLLMModel
+from agents import Agent, ModelSettings
 
 app = FastAPI()
 
+class InvokeRequest(BaseModel):
+    input: str
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+model = LiteLLMModel(
+    model=os.environ.get("MODEL"),
+    api_base=os.environ.get("MODEL_API_BASE"),
+    api_key=os.environ.get("MODEL_API_KEY"),
+)
 
+agent = Agent(
+    name="EPF Agent",
+    model=model,
+    model_settings=ModelSettings(include_usage=True)
+)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/invoke")
+async def query_agent(request: InvokeRequest):
+    response = await agent.invoke({"input": request.input})
+    return {"output": response.output_text}
