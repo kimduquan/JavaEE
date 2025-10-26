9172bc0d4fb6094721891376fc49f42cf7f00e98
kimduquan <kimduquan@gmail.com>
2025-10-26 23:38:19 +0700

    #

--- OpenUP/EPF-agent/dev.sh
+++ OpenUP/EPF-agent/dev.sh
@@ -1,5 +1,6 @@
 docker build -t epf-agent:1.0.0 .
 kubectl delete secret epf-agent
+kubectl delete hpa epf-agent
 kubectl create secret generic epf-agent --from-literal=OPENAI_API_KEY="$(openssl rand -hex 32)"
 kubectl apply -f kubernetes.yml
 kubectl wait deployment --for condition=available --timeout=60s epf-agent
