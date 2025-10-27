f715fde68c7f54ca3134ffac4080576647aa320f
kimduquan <kimduquan@gmail.com>
2025-10-27 20:51:11 +0700

    #

--- OpenUP/EPF-agent/dev.sh
+++ OpenUP/EPF-agent/dev.sh
@@ -1,5 +1,5 @@
 docker build -t epf-agent:1.0.0 .
-#docker run -d -e "MODEL=hosted_vllm/neuralmagic/Mistral-7B-Instruct-v0.3-GPTQ-4bit" -e MODEL_API_BASE=host.docker.internal:8000/v1 -p 8080:8080 epf-agent:1.0.0
+#docker run -d --rm --name epf-agent -e "MODEL_NAME=hosted_vllm/neuralmagic/Mistral-7B-Instruct-v0.3-GPTQ-4bit" -e "MODEL_BASE_URL=http://host.docker.internal:8000/v1" -e MODEL_API_KEY=EMPTY -p 8080:8080 epf-agent:1.0.0
 kubectl delete secret vllm
 kubectl delete hpa epf-agent
 kubectl create secret generic vllm --from-literal=MODEL_API_KEY="$(openssl rand -hex 32)"
