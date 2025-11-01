712e93a223bfbf5df6535e64ff36fac506c0a428
kimduquan <kimduquan@gmail.com>
2025-11-01 08:45:34 +0700

    #

--- OpenUP/EPF-agent/main.py
+++ OpenUP/EPF-agent/main.py
@@ -1,10 +1,14 @@
 from typing import Any
-from fastapi import FastAPI, Request
+from fastapi import Depends, FastAPI, Request
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from pydantic import BaseModel
 import os
-from agents import Agent, ModelSettings, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
+from agents import Agent, ModelSettings, Runner, AsyncOpenAI, OpenAIChatCompletionsModel, SQLiteSession
 from agents.mcp import MCPServerStreamableHttp
 from contextlib import asynccontextmanager
+from jose import jwt
+from aiocache import cached, SimpleMemoryCache
+from agents.extensions.memory.encrypt_session import EncryptedSession
 
 class RunRequest(BaseModel):
     input: str
@@ -19,38 +23,64 @@ model = OpenAIChatCompletionsModel(
     openai_client=client,
 )
 
-mcp_server = MCPServerStreamableHttp(
-    params={
-        "url":os.environ.get("MCP_SERVER_URL"),
-    }
-)
+authorization = SimpleMemoryCache()
 
-agent = Agent(
-    name="EPF Agent",
-    mcp_servers=[mcp_server],
-    model=model,
-    model_settings=ModelSettings(
-        tool_choice="auto",
-    ),
-)
+security = HTTPBearer()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await mcp_server.connect()
     yield
-    await mcp_server.cleanup()
+
+def get_claims(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    return jwt.decode(token=credentials.credentials)
+
+@cached
+def get_session(session_id: str):
+    underlying_session = SQLiteSession(session_id=session_id)
+    session = EncryptedSession(
+        session_id=session_id,
+        underlying_session=underlying_session,
+        encryption_key=os.environ.get("SESSION_ENCRYPTION_KEY"),
+        ttl=int(os.environ.get("SESSION_TTL")),
+    )
+    return session
+
+@cached
+def get_mcp_server(session_id: str):
+    mcp_server = MCPServerStreamableHttp(
+        params={
+            "url":os.environ.get("MCP_SERVER_URL"),
+            "headers": {
+                "Authorization": authorization.get(session_id)
+            }
+        }
+    )
+    return mcp_server
+
+@cached
+def get_agent(session_id: str):
+    mcp_server = get_mcp_server(session_id)
+    agent = Agent(
+        name="EPF Agent",
+        mcp_servers=[mcp_server],
+        model=model,
+        model_settings=ModelSettings(
+            tool_choice="auto",
+        ),
+    )
+    return agent
 
 app = FastAPI(lifespan=lifespan)
 
-@app.post("/run")
-async def run_agent(request: RunRequest):
-    response = await Runner.run(starting_agent=agent,input=request.input)
-    return {"final_output": response.final_output}
-
 @app.post("/prompts/{name}")
-async def run_prompt(name, request: Request):
+async def run_prompt(name, request: Request, claims: dict[str, Any] = Depends(get_claims)):
     arguments = dict[str, Any]
     for param_name, param_value in request.query_params.items():
         arguments[param_name] = param_value
+    session_id = str(claims["jti"])
+    authorization.set(session_id, request.headers.get("Authorization"))
+    mcp_server = get_mcp_server(session_id)
     prompt = await mcp_server.get_prompt(name=name, arguments=arguments)
-    await Runner.run(starting_agent=agent, input=prompt.messages[0].content.text)
+    agent = get_agent(session_id)
+    session = get_session(session_id)
+    await Runner.run(starting_agent=agent, input=prompt.messages[0].content.text,session=session)
