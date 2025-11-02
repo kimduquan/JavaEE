7edfe92639d150de241a3d83ff2294b3b35254b9
kimduquan <kimduquan@gmail.com>
2025-11-02 12:21:36 +0700

    #

--- OpenUP/EPF-agent/main.py
+++ OpenUP/EPF-agent/main.py
@@ -36,11 +36,11 @@ jwt_issuer = os.environ.get("JWT_VERIFY_ISSUER")
 async def lifespan(app: FastAPI):
     yield
 
-async def get_claims(credentials: HTTPAuthorizationCredentials = Depends(security)) -> dict[str, Any]:
+def get_claims(credentials: HTTPAuthorizationCredentials = Depends(security)) -> dict[str, Any]:
     signing_key = jwk_client.get_signing_key_from_jwt(token=credentials.credentials)
     return jwt.decode(token=credentials.credentials,key=signing_key.key,issuer=jwt_issuer)
 
-@cached
+@cached()
 async def get_session(session_id: str) -> EncryptedSession:
     underlying_session = RedisSession(session_id=session_id, redis_client=redis_client)
     session = EncryptedSession(
@@ -51,22 +51,22 @@ async def get_session(session_id: str) -> EncryptedSession:
     )
     return session
 
-@cached
+@cached()
 async def get_mcp_server(session_id: str) -> MCPServerStreamableHttp:
     mcp_server = MCPServerStreamableHttp(
         params={
             "url":os.environ.get("MCP_SERVER_URL"),
             "headers": {
-                "Authorization": authorization.get(session_id)
+                "Authorization": await authorization.get(session_id)
             }
         }
     )
     await mcp_server.connect()
     return mcp_server
 
-@cached
+@cached()
 async def get_agent(session_id: str) -> Agent:
-    mcp_server = get_mcp_server(session_id)
+    mcp_server = await get_mcp_server(session_id)
     agent = Agent(
         name="EPF Agent",
         mcp_servers=[mcp_server],
@@ -85,21 +85,22 @@ async def run_agent(name: str, request: Request, claims: dict[str, Any] = Depend
     for param_name, param_value in request.query_params.items():
         arguments[param_name] = param_value
     session_id = str(claims["sub"])
-    authorization.set(session_id, request.headers.get("Authorization"))
+    await authorization.set(session_id, request.headers.get("Authorization"))
     mcp_server = await get_mcp_server(session_id)
     prompt = await mcp_server.get_prompt(name=name, arguments=arguments)
     agent = await get_agent(session_id)
     session = await get_session(session_id)
-    await Runner.run(starting_agent=agent, input=prompt.messages[0].content.text,session=session)
+    run_result = await Runner.run(starting_agent=agent, input=prompt.messages[0].content.text,session=session)
+    return run_result.final_output
 
 @app.get("/sessions/items")
 async def get_session_items(claims: dict[str, Any] = Depends(get_claims)):
     session_id = str(claims["sub"])
     session = await get_session(session_id)
-    return await session.get_items()
+    return session.get_items()
     
 @app.delete("/sessions/items")
 async def clear_session_items(claims: dict[str, Any] = Depends(get_claims)):
     session_id = str(claims["sub"])
     session = await get_session(session_id)
-    await session.clear_session()
+    session.clear_session()
