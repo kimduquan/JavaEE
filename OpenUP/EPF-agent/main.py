0bb543901ea2532559357c47d62b73bd9af078f4
kimduquan <kimduquan@gmail.com>
2025-11-02 14:37:10 +0700

    #

--- OpenUP/EPF-agent/main.py
+++ OpenUP/EPF-agent/main.py
@@ -10,7 +10,11 @@ from aiocache import cached, SimpleMemoryCache
 from agents.extensions.memory.encrypt_session import EncryptedSession
 from agents.extensions.memory.redis_session import RedisSession
 from jwt import PyJWKClient
+from pydantic import BaseModel
 from redis import Redis
+
+class AgentRequest(BaseModel):
+    input: str
 
 client = AsyncOpenAI(
     base_url=os.environ.get("MODEL_BASE_URL"),
@@ -64,12 +68,16 @@ async def get_mcp_server(session_id: str) -> MCPServerStreamableHttp:
     await mcp_server.connect()
     return mcp_server
 
-@cached()
-async def get_agent(session_id: str) -> Agent:
-    mcp_server = await get_mcp_server(session_id)
+async def get_agent(session_id: str, name: str, arguments: dict[str, Any], handoffs: list[Agent[Any]]) -> Agent[Any]:
+    mcp_server: MCPServerStreamableHttp = await get_mcp_server(session_id)
+    prompt = await mcp_server.get_prompt(name=name, arguments=arguments)
+    instructions = prompt.messages[0].content.text
     agent = Agent(
-        name="EPF Agent",
+        name=name,
+        handoff_description=prompt.description,
         mcp_servers=[mcp_server],
+        instructions=instructions,
+        handoffs=handoffs,
         model=model,
         model_settings=ModelSettings(
             tool_choice="auto",
@@ -77,21 +85,42 @@ async def get_agent(session_id: str) -> Agent:
     )
     return agent
 
+async def get_handoffs(session_id: str, name: str, arguments: dict[str, Any]) -> list[Agent[Any]]:
+    handoffs: list[Agent[Any]] = []
+    mcp_server: MCPServerStreamableHttp = await get_mcp_server(session_id)
+    list_prompts = await mcp_server.list_prompts()
+    for prompt in list_prompts.prompts:
+        if(name != prompt.name):
+            agent_prompt = await mcp_server.get_prompt(name=name, arguments=arguments)
+            handoff_description = agent_prompt.description
+            instructions = agent_prompt.messages[0].content.text
+            handoff_agent = Agent(
+                name=name,
+                handoff_description=handoff_description,
+                mcp_servers=[mcp_server],
+                instructions=instructions,
+                model=model,
+                model_settings=ModelSettings(
+                    tool_choice="auto",
+                ),
+            )
+            handoffs.append(handoff_agent)
+    return handoffs
+
 app = FastAPI(lifespan=lifespan)
 
 @app.post("/agents/{name}")
-async def run_agent(name: str, request: Request, claims: dict[str, Any] = Depends(get_claims)):
+async def run_agent(name: str, request: Request, agent_request: AgentRequest, claims: dict[str, Any] = Depends(get_claims)):
     arguments: dict[str, str] = {}
     for param_name, param_value in request.query_params.items():
         arguments[param_name] = param_value
     session_id = str(claims["sub"])
     await authorization.set(session_id, request.headers.get("Authorization"))
-    mcp_server = await get_mcp_server(session_id)
-    prompt = await mcp_server.get_prompt(name=name, arguments=arguments)
-    agent = await get_agent(session_id)
+    handoffs = await get_handoffs(session_id=session_id,name=name,arguments=arguments)
+    starting_agent = await get_agent(session_id=session_id,name=name,arguments=arguments,handoffs=handoffs)
     session = await get_session(session_id)
-    run_result = await Runner.run(starting_agent=agent, input=prompt.messages[0].content.text,session=session)
-    return run_result.final_output
+    run_result = await Runner.run(starting_agent=starting_agent,input=agent_request.input,session=session)
+    return run_result
 
 @app.get("/sessions/items")
 async def get_session_items(claims: dict[str, Any] = Depends(get_claims)):
