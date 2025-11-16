774d7f137b54ed0439cbf281cd02353da079fa1d
kimduquan <kimduquan@gmail.com>
2025-11-16 22:23:50 +0700

    #

--- OpenUP/EPF-agent-webapp/dev.sh
+++ OpenUP/EPF-agent-webapp/dev.sh
@@ -3,7 +3,7 @@ npm install --verbose
 npm run build --verbose
 cd ../
 docker build -t epf-agent-webapp .
-#docker run --name epf-agent -d --rm -p 3000:3000 -e "CLIENT_ID=account" -e "CLIENT_SECRET=ojwWoprJT7GFoKFknEcdhTK7FFCUS0EX" -e "ISSUER=https://chipmunk-capable-prawn.ngrok-free.app/auth/realms/EPF-dev" -e "NEXTAUTH_SECRET=IaJCszU3X6BSO/5gj+ul6TnsFzohs/WyHbGbj/TEcV4=" epf-agent-webapp
+#docker run --name epf-agent -d --rm -p 3000:3000 -e "CLIENT_ID=account" -e "CLIENT_SECRET=ojwWoprJT7GFoKFknEcdhTK7FFCUS0EX" -e "ISSUER=https://chipmunk-capable-prawn.ngrok-free.app/auth/realms/EPF-dev" -e "NEXTAUTH_SECRET=IaJCszU3X6BSO/5gj+ul6TnsFzohs/WyHbGbj/TEcV4=" -e "NEXTAUTH_URL=http://localhost:3000" epf-agent-webapp
 export CLIENT_ID=account
 export CLIENT_SECRET=ojwWoprJT7GFoKFknEcdhTK7FFCUS0EX
 export ISSUER=https://chipmunk-capable-prawn.ngrok-free.app/auth/realms/EPF-dev
