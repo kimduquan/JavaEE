ee5c1d92290477e3e44f3fb1a25b5a26fbdc7f3d
kimduquan <kimduquan@gmail.com>
2025-12-16 07:29:08 +0700

    #

--- OpenUP/Supavisor/supavisor.sh
+++ OpenUP/Supavisor/supavisor.sh
@@ -1,8 +1,8 @@
-kubectl delete secret supavisor
+#kubectl delete secret supavisor
 kubectl delete hpa supavisor
 kubectl create secret generic supavisor --from-literal=SECRET_KEY_BASE="$(openssl rand -hex 32)" --from-literal=API_JWT_SECRET="$(openssl rand -hex 32)" --from-literal=METRICS_JWT_SECRET="$(openssl rand -hex 32)" --from-literal=VAULT_ENC_KEY="$(openssl rand -hex 16)"
 kubectl apply -f kubernetes.yml
-kubectl wait deployment --for condition=available --timeout=60s supavisor
-kubectl wait pod --for condition=ready --timeout=60s -l app.kubernetes.io/name=supavisor
+kubectl wait deployment --for condition=available --timeout=90s supavisor
+kubectl wait pod --for condition=ready --timeout=90s -l app.kubernetes.io/name=supavisor
 #psql postgres://epf.epf:Password1234@supavisor.default.svc.cluster.local:5452/epf?sslmode=disable
 kubectl autoscale deployment supavisor --max 1
