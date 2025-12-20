265b87617039f5376f6ab5ef09880f9bbee72336
kimduquan <kimduquan@gmail.com>
2025-12-20 19:52:50 +0700

    #

--- OpenUP/HAProxy/haproxy.sh
+++ OpenUP/HAProxy/haproxy.sh
@@ -1,3 +1,5 @@
-helm repo add haproxy-ingress https://haproxy-ingress.github.io/charts
-helm install haproxy-ingress haproxy-ingress/haproxy-ingress --namespace default --set controller.tcp.5452="default/supavisor:5452" --set controller.tcp.6543="default/supavisor:6543" --set controller.ingressClassResource.enabled=true --set controller.logs.enabled=true
-#helm install haproxy-kubernetes-ingress oci://ghcr.io/haproxytech/helm-charts/kubernetes-ingress --set controller.service.type=LoadBalancer -f values.yaml
+#helm repo add haproxy-ingress https://haproxy-ingress.github.io/charts
+#helm install haproxy-ingress haproxy-ingress/haproxy-ingress --namespace default --set controller.tcp.5452="default/supavisor:5452" --set controller.tcp.6543="default/supavisor:6543" --set controller.ingressClassResource.enabled=true --set controller.logs.enabled=true
+#https://artifacthub.io/packages/helm/haproxytech/kubernetes-
+kubectl apply -f tcp.yaml
+helm install haproxy-kubernetes-ingress oci://ghcr.io/haproxytech/helm-charts/kubernetes-ingress -f values.yaml
