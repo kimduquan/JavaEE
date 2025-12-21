0851ccc82693328b35e0813c83344e4a1fc5a7a5
kimduquan <kimduquan@gmail.com>
2025-12-21 17:10:31 +0700

    #

--- OpenUP/EPF-shell/src/main/java/epf/shell/schema/management/Management.java
+++ OpenUP/EPF-shell/src/main/java/epf/shell/schema/management/Management.java
@@ -2,12 +2,9 @@ package epf.shell.schema.management;
 
 import epf.naming.Naming;
 import epf.shell.Function;
-import epf.shell.security.CallerPrincipal;
-import epf.shell.security.Credential;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
-import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Command;
 
 @Command(name = Naming.MANAGEMENT)
@@ -19,10 +16,7 @@ public class Management {
 	transient EntityManager manager;
 	
 	@Command(name = "create")
-	public void generate(
-			@ArgGroup(exclusive = true, multiplicity = "1")
-			@CallerPrincipal
-			final Credential credential) throws Exception {
+	public void generate() throws Exception {
 		manager.getEntityManagerFactory().getSchemaManager().create(true);
 	}
 }
