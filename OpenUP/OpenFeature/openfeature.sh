ce82417932e8cfff79422f8ee548b586d86ef0b7
kimduquan <kimduquan@gmail.com>
2025-12-24 21:56:50 +0700

    #

--- OpenUP/OpenFeature/openfeature.sh
+++ OpenUP/OpenFeature/openfeature.sh
@@ -1,3 +1,3 @@
 helm repo add openfeature https://open-feature.github.io/open-feature-operator/
 helm repo update
-helm install openfeature openfeature/open-feature-operator --set defaultNamespace=openfeature
+helm install openfeature openfeature/open-feature-operator --set defaultNamespace=default --set namespace.create=false
