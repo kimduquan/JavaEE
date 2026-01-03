bb37fb186d2662a7c7fcfe06bc8e55b490640293
kimduquan <kimduquan@gmail.com>
2026-01-03 14:16:32 +0700

    #

--- OpenUP/EPF-agent-webapp/webapp/src/app/layout.tsx
+++ OpenUP/EPF-agent-webapp/webapp/src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
-
 import { CopilotKit } from "@copilotkit/react-core";
-import "./globals.css";
-import "@copilotkit/react-ui/styles.css";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
