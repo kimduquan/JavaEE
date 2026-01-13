df3fab8702f3d83f91910e5eaefc11d0c27df275
kimduquan <kimduquan@gmail.com>
2026-01-13 21:30:10 +0700

    #

--- OpenUP/EPF-agent-webapp/webapp/src/app/page.tsx
+++ OpenUP/EPF-agent-webapp/webapp/src/app/page.tsx
@@ -1,14 +1,24 @@
 "use client";
 
 import { CopilotChat } from "@copilotkit/react-ui";
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider, useSession } from "next-auth/react"
 
 export default function CopilotKitPage() {
   return (
     <main>
       <SessionProvider>
-        <CopilotChat />
+        <MainContent />
       </SessionProvider>
     </main>
+  );
+}
+
+function MainContent() {
+  useSession({ required: true });
+
+  return (
+    <div>
+      <CopilotChat />
+    </div>
   );
 }
