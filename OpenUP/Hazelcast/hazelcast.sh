059fe0c46b2b67343c3586037337e1fa4ec9f0c4
kimduquan <kimduquan@gmail.com>
2026-01-17 05:51:02 +0700

    #

--- OpenUP/Hazelcast/hazelcast.sh
+++
@@ -1,3 +0,0 @@
-helm repo add hazelcast https://hazelcast-charts.s3.amazonaws.com/
-helm repo update
-helm install hazelcast hazelcast/hazelcast -f hazelcast.yaml
--- OpenUP/Ollama/ollama.sh
+++
@@ -1,1 +0,0 @@
-docker run --name ollama --runtime nvidia --gpus all -v ollama:/root/.ollama -p 11434:11434 ollama/ollama
