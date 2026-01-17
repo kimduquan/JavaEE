52f13a85007206d1c7d5dd1e7e6401eb5c91be20
kimduquan <kimduquan@gmail.com>
2026-01-17 07:27:49 +0700

    #

--- OpenUP/KubeVirt/kubevirt.sh
+++
@@ -1,5 +0,0 @@
-#VERSION="$(curl --silent https://storage.googleapis.com/kubevirt-prow/release/kubevirt/kubevirt/stable.txt)"
-#kubectl create --filename="https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt-operator.yaml"
-#kubectl create --filename="https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt-cr.yaml"
-kubectl create -f kubevirt-operator.yaml
-kubectl create -f kubevirt-cr.yaml
--- OpenUP/Unsloth/unsloth.sh
+++ OpenUP/Unsloth/unsloth.sh
@@ -1,2 +1,2 @@
 mkdir work
-docker run -d -e JUPYTER_PASSWORD="123456" -p 8888:8888 -p 2222:22  -v ./work:/workspace/work --gpus all unsloth/unsloth
+docker run --name unsloth -d -e JUPYTER_PASSWORD="123456" -p 8888:8888 -p 2222:22  -v ./work:/workspace/work --gpus all unsloth/unsloth
