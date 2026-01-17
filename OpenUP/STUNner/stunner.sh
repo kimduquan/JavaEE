5ca629a8d8ad8d2475aec60a6be602641fb27d1f
kimduquan <kimduquan@gmail.com>
2026-01-17 17:26:29 +0700

    #

--- OpenUP/STUNner/stunner.sh
+++ OpenUP/STUNner/stunner.sh
@@ -1,6 +1,6 @@
 helm repo add stunner https://l7mp.io/stunner
 helm repo update
-helm install stunner stunner/stunner --namespace=default --wait
+helm install stunner stunner/stunner --namespace=default --wait -f values.yaml
 kubectl apply --wait=true -f stunner_auth_secret.yaml
 kubectl apply --wait=true -f gatewayconfig.yaml
 kubectl apply --wait=true -f gatewayclass.yaml
