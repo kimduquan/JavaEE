d215fecd9a7b4b0103a02dc310db2a400e6cbd6b
kimduquan <kimduquan@gmail.com>
2026-01-31 16:39:09 +0700

    #

--- OpenUP/EPF-agent-webapp/dev.sh
+++ OpenUP/EPF-agent-webapp/dev.sh
@@ -1,10 +1,4 @@
 ./build.sh
 ./stop.sh
 ./start.sh
-#docker run --name epf-agent -d --rm -p 3000:3000 -e "CLIENT_ID=account" -e "CLIENT_SECRET=ojwWoprJT7GFoKFknEcdhTK7FFCUS0EX" -e "ISSUER=https://chipmunk-capable-prawn.ngrok-free.app/auth/realms/EPF-dev" -e "NEXTAUTH_SECRET=IaJCszU3X6BSO/5gj+ul6TnsFzohs/WyHbGbj/TEcV4=" -e "NEXTAUTH_URL=http://localhost:3000" -e "LANGGRAPH_DEPLOYMENT_URL=http://host.docker.internal:8123" epf-agent-webapp
-#export CLIENT_ID=account
-#export CLIENT_SECRET=ojwWoprJT7GFoKFknEcdhTK7FFCUS0EX
-#export ISSUER=https://chipmunk-capable-prawn.ngrok-free.app/auth/realms/EPF-dev
-#export NEXTAUTH_SECRET=IaJCszU3X6BSO/5gj+ul6TnsFzohs/WyHbGbj/TEcV4=
-#export NEXTAUTH_URL=http://localhost:3000
-#npm run start --verbose
+#docker run --name epf-agent-webapp -e "CLIENT_ID=account" -e "CLIENT_SECRET=cIkzKJZ4A7jXKpXfaxC4GaLxJAmmEqhP" -e "ISSUER=https://chipmunk-capable-prawn.ngrok-free.app/auth/realms/EPF-dev" -e "NEXTAUTH_URL=http://localhost:3000" -e "EPF_AGENT_URL=http://host.docker.internal:8123" -e "NEXTAUTH_SECRET=01263aa9dca3b11efa2bd1c73dfea942cfb11783e07b6d0e7b565c2f61f77709" -e "AUTH_REQUEST_TIMEOUT=10000" -e "JWT_MAX_AGE=300" -e "DEBUG=true" -p 3000:3000 -d epf/epf-agent-webapp:1.0.0
