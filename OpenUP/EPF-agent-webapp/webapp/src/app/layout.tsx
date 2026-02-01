99f7d38f7d32a30f489f4c698ef91a1606f81252
kimduquan <kimduquan@gmail.com>
2026-02-01 10:18:00 +0700

    #

--- OpenUP/EPF-agent-webapp/webapp/src/app/layout.tsx
+++ OpenUP/EPF-agent-webapp/webapp/src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import "@copilotkit/react-ui/styles.css";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { SessionProvider } from "next-auth/react";
 
 export const metadata: Metadata = {
   title: "EPF Agent",
@@ -18,14 +19,16 @@ export default async function RootLayout({
 }>) {
   const session = await getServerSession(authOptions);
   console.log("[BEGIN]RootLayout");
-  console.log("session.expires:%s", session != null ? session.expires : "");
+  console.log("session.expires:%s", session != null ? session.expires : undefined);
   console.log("[END]RootLayout");
   return (
     <html lang="en">
       <body className={"antialiased"}>
-        <CopilotKit runtimeUrl="/api/copilotkit" agent="epf-agent">
-          {children}
-        </CopilotKit>
+        <SessionProvider>
+          <CopilotKit runtimeUrl="/api/copilotkit" agent="epf-agent">
+            {children}
+          </CopilotKit>
+        </SessionProvider>
       </body>
     </html>
   );
