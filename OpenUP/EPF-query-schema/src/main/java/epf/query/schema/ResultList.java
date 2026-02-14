b9c6f437360a04c042d85a033f684a676fdf6856
kimduquan <kimduquan@gmail.com>
2026-02-14 13:16:29 +0700

    #

--- OpenUP/EPF-query-schema/src/main/java/epf/query/schema/ResultList.java
+++ OpenUP/EPF-query-schema/src/main/java/epf/query/schema/ResultList.java
@@ -1,9 +1,13 @@
 package epf.query.schema;
 
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonClassDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 
+@JsonClassDescription("the query results")
 public class ResultList {
 
+	@JsonPropertyDescription("a list of the results, or an empty list if there are no results")
 	private List<?> resultList;
 
 	public List<?> getResultList() {
--- OpenUP/EPF-query-schema/src/main/java/epf/query/schema/SingleResult.java
+++ OpenUP/EPF-query-schema/src/main/java/epf/query/schema/SingleResult.java
@@ -1,7 +1,12 @@
 package epf.query.schema;
 
+import com.fasterxml.jackson.annotation.JsonClassDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+
+@JsonClassDescription("the single result.")
 public class SingleResult {
 	
+	@JsonPropertyDescription("the result, or null if there is no result")
 	private Object singleResult;
 
 	public Object getSingleResult() {
