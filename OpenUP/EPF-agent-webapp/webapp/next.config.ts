24a9e08f17cba593f8fd88c4c5d68f3c87577e09
kimduquan <kimduquan@gmail.com>
2026-03-08 18:20:45 +0700

    #

--- OpenUP/EPF-agent-webapp/webapp/next.config.ts
+++ OpenUP/EPF-agent-webapp/webapp/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   serverExternalPackages: ["@copilotkit/runtime"],
+  basePath: "/agent"
 };
 
 export default nextConfig;
