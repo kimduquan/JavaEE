97e5d92184ac8a420dd68c52ee4dc50e9b568382
kimduquan <kimduquan@gmail.com>
2026-03-08 21:14:06 +0700

    #

--- OpenUP/EPF-agent-webapp/build.sh
+++ OpenUP/EPF-agent-webapp/build.sh
@@ -1,3 +1,5 @@
+export NEXT_PUBLIC_NEXT_AUTH_BASE_URL=https://chipmunk-capable-prawn.ngrok-free.app
+export NEXT_PUBLIC_NEXT_AUTH_BASE_PATH=/agent/api/auth
 cd webapp
 npm install --verbose
 npm run build --verbose
