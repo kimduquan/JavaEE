d7de3affa36392c03cb7e1b6f7013f4b07d76804
kimduquan <kimduquan@gmail.com>
2026-03-11 06:39:37 +0700

    #

--- OpenUP/EPF-agent-webapp/dev.bat
+++ OpenUP/EPF-agent-webapp/dev.bat
@@ -4,16 +4,5 @@ set NEXT_PUBLIC_NEXT_AUTH_REFETCH_INTERVAL=60
 cd webapp
 npm install --verbose
 
-set CLIENT_ID=
-set CLIENT_SECRET=
-set ISSUER=https://chipmunk-capable-prawn.ngrok-free.app/auth/realms/EPF-dev
-set NEXTAUTH_URL=http://localhost:3000/agent/api/auth
-set AUTH_REQUEST_TIMEOUT=10000
-set JWT_MAX_AGE=300
-set JWT_UPDATE_AGE=60
-set NEXTAUTH_SECRET=
-set COPILOTKIT_PUBLIC_LICENSE_KEY=
-set EPF_AGENT_URL=http://localhost:8123
-set DEBUG=true
-set OIDC_PROVIDER_URL=http://localhost:8080/auth/realms/EPF-dev
+call env.bat
 npm run dev
