1406038c56bde187d5a3c46978149730ec3ce92c
kimduquan <kimduquan@gmail.com>
2026-03-11 06:40:49 +0700

    #

---
+++ OpenUP/EPF-agent-webapp/env.bat
@@ -0,0 +1,12 @@
+set CLIENT_ID=
+set CLIENT_SECRET=
+set ISSUER=https://chipmunk-capable-prawn.ngrok-free.app/auth/realms/EPF-dev
+set NEXTAUTH_URL=http://localhost:3000/agent/api/auth
+set AUTH_REQUEST_TIMEOUT=10000
+set JWT_MAX_AGE=300
+set JWT_UPDATE_AGE=60
+set NEXTAUTH_SECRET=
+set COPILOTKIT_PUBLIC_LICENSE_KEY=
+set EPF_AGENT_URL=http://localhost:8123
+set DEBUG=true
+set OIDC_PROVIDER_URL=http://localhost:8080/auth/realms/EPF-dev
