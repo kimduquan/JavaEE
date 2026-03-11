f36bc3e86f71ec87c03c0e0c5e8dc3dac619023e
kimduquan <kimduquan@gmail.com>
2026-03-11 12:13:32 +0700

    #

--- OpenUP/EPF-agent-webapp/debug.bat
+++ OpenUP/EPF-agent-webapp/debug.bat
@@ -1,8 +1,9 @@
+call env.bat
+
 set NEXT_PUBLIC_NEXT_AUTH_BASE_URL=http://localhost:3000
 set NEXT_PUBLIC_NEXT_AUTH_BASE_PATH=/agent/api/auth
 set NEXT_PUBLIC_NEXT_AUTH_REFETCH_INTERVAL=60
 cd webapp
 npm install --verbose
 
-call env.bat
 npm run dev
