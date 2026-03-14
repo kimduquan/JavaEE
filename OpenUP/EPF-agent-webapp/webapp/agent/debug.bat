8bd6f40359b006a8567a252183ac7e46677bf9ed
kimduquan <kimduquan@gmail.com>
2026-03-14 17:10:54 +0700

    #

--- OpenUP/EPF-agent-webapp/webapp/agent/debug.bat
+++ OpenUP/EPF-agent-webapp/webapp/agent/debug.bat
@@ -1,7 +1,9 @@
 
-start /B kubectl port-forward svc/epf-mcp-gateway 9197 &
 start /B kubectl port-forward svc/keycloak 8080 &
 start /B kubectl port-forward svc/redis-master 6379 &
 start /B kubectl port-forward svc/jaeger 4317 16686 &
+start /B kubectl port-forward svc/epf-query 9188 &
+start /B kubectl port-forward svc/epf-persistence 9181 &
+:: start /B kubectl port-forward svc/epf-mcp-gateway 9197 &
 
 :: .venv\Scripts\python.exe main.py
