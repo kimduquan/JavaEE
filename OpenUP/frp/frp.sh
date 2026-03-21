9765df16d88f16ba2282bc15ae6129710227daf8
kimduquan <kimduquan@gmail.com>
2026-03-21 11:45:29 +0700

    #

--- OpenUP/frp/frp.sh
+++ OpenUP/frp/frp.sh
@@ -1,3 +1,3 @@
 . ../env.sh
-docker build --file $FRP_SOURCE_DIR/dockerfiles/Dockerfile-for-frps --tag frps $FRP_SOURCE_DIR
+docker build --file $FRP_SOURCE_DIR/dockerfiles/Dockerfile-for-frps --tag epf/frp:1.0.0 $FRP_SOURCE_DIR
 mvn clean package -U
