c044a8de138167104f9833cb8d10b87c8dc3f64e
kimduquan <kimduquan@gmail.com>
2026-03-21 11:54:17 +0700

    #

--- OpenUP/frp/frp.sh
+++ OpenUP/frp/frp.sh
@@ -1,3 +1,4 @@
 . ../env.sh
 docker build --file $FRP_SOURCE_DIR/dockerfiles/Dockerfile-for-frps --tag epf/frp:1.0.0 $FRP_SOURCE_DIR
 mvn clean package -U
+helm install frp target/helm/kubernetes/frp --wait
