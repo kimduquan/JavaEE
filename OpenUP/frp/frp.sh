109b9971bb1965265d8442302ece337fa42d920f
kimduquan <kimduquan@gmail.com>
2026-03-22 11:27:15 +0700

    #

--- OpenUP/frp/frp.sh
+++ OpenUP/frp/frp.sh
@@ -1,7 +1,10 @@
 . ../env.sh
 kubectl delete configmap frp
 kubectl create configmap frp --from-file=frps=frps.toml
+kubectl delete -f tls.yaml
 kubectl apply -f tls.yaml
+kubectl delete -f ingress.yml
 docker build --file $FRP_SOURCE_DIR/dockerfiles/Dockerfile-for-frps --tag epf/frp:1.0.0 $FRP_SOURCE_DIR
 mvn clean package -U
 helm install frp target/helm/kubernetes/frp --wait
+kubectl apply -f ingress.yml
