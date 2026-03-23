61260f23ded7c17152227de3cedc5561bae3378a
kimduquan <kimduquan@gmail.com>
2026-03-23 08:23:27 +0700

    #

--- OpenUP/frp/frp.sh
+++ OpenUP/frp/frp.sh
@@ -1,7 +1,7 @@
 . ../env.sh
 kubectl delete configmap frp
 kubectl create configmap frp --from-file=frps=frps.toml
-ssh-keygen -t ed25519 -N 090323508 -f id_ed25519
+ssh-keygen -q -t ed25519 -N "" -f id_ed25519
 kubectl create secret generic frp-ssh --type=kubernetes.io/ssh-auth --from-file=ssh-privatekey=id_ed25519
 kubectl create secret generic frp-ssh-auth --from-literal=authorized_keys=''
 kubectl delete -f tls.yaml
