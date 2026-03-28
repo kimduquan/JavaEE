e606b64a3c54594c7efa172cb4be9e3609696943
kimduquan <kimduquan@gmail.com>
2026-03-28 14:39:46 +0700

    #

--- OpenUP/ChromeDevToolsMCP/chrome-devtools-mcp/main.py
+++ OpenUP/ChromeDevToolsMCP/chrome-devtools-mcp/main.py
@@ -1,10 +1,13 @@
 import asyncio
+import os
 from fastmcp import FastMCP
 from fastmcp.server.server import create_proxy
 from fastmcp.mcp_config import MCPConfig, StdioMCPServer
 
 CONFIG = MCPConfig(mcpServers={"chrome-devtools": StdioMCPServer(command="npx", args=["-y", "chrome-devtools-mcp@latest"])})
 APP = create_proxy(target=CONFIG)
+HOSTNAME = os.getenv(key="HOSTNAME")
+PORT = int(os.getenv(key="PORT"))
 
 if __name__ == "__main__":
-    asyncio.run(APP.run_http_async(show_banner=False, host="0.0.0.0"))
+    asyncio.run(APP.run_http_async(show_banner=False, host=HOSTNAME, port=PORT))
