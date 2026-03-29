561fe3189f9dece678d3eef0eee43cb17a1acd9f
kimduquan <kimduquan@gmail.com>
2026-03-29 23:17:29 +0700

    #

--- OpenUP/ChromeDevToolsMCP/build.sh
+++ OpenUP/ChromeDevToolsMCP/build.sh
@@ -1,4 +1,4 @@
 . ../env.sh
 docker build -t epf/chrome-devtools-mcp:1.0.0 .
 mvn clean package -U
-#helm install chrome-devtools-mcp target/helm/kubernetes/chrome-devtools-mcp --wait
+helm install chrome-devtools-mcp target/helm/kubernetes/chrome-devtools-mcp --wait
