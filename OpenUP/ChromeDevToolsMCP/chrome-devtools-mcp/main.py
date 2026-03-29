43188268e2148dd2bdf5cb423137b4aa2cae051c
kimduquan <kimduquan@gmail.com>
2026-03-30 00:47:29 +0700

    #

--- OpenUP/ChromeDevToolsMCP/chrome-devtools-mcp/main.py
+++ OpenUP/ChromeDevToolsMCP/chrome-devtools-mcp/main.py
@@ -6,12 +6,11 @@ from opentelemetry.instrumentation import auto_instrumentation
 
 auto_instrumentation.initialize()
 
+LOG_LEVEL = os.environ.get("LOG_LEVEL", "debug")
 CONFIG = MCPConfig(mcpServers={"chrome_devtools": StdioMCPServer(command="npx", args=["-y", "chrome-devtools-mcp@latest", "--no-performance-crux", "--no-usage-statistics"])})
 APP = create_proxy(target=CONFIG)
 HOSTNAME = os.environ.get("HOSTNAME", "0.0.0.0")
-PORT = int(os.environ.get("PORT", "8000"))
+PORT = int(os.environ.get("PORT", "8002"))
 
 if __name__ == "__main__":
-    tools = asyncio.run(APP.list_tools())
-    print(f"tools:{tools}")
-    asyncio.run(APP.run_http_async(show_banner=False, transport="streamable-http", host=HOSTNAME, port=PORT, json_response=True, stateless_http=True, stateless=True))
+    asyncio.run(APP.run_http_async(show_banner=False, transport="streamable-http", host=HOSTNAME, port=PORT, log_level=LOG_LEVEL, stateless_http=True, stateless=True))
