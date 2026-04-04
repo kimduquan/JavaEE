82ffa5e8f4a80c7788e4f509b5f96d0661bc4d15
kimduquan <kimduquan@gmail.com>
2026-04-04 18:06:12 +0700

    #

--- OpenUP/frp/client.bat
+++ OpenUP/frp/client.bat
@@ -1,2 +1,2 @@
 set FRP_TOKEN=
-ssh -R :80:127.0.0.1:8080 v0@frp.localhost.direct -p 2200 tcp --remote_port 8080 --token '%FRP_TOKEN%'
+ssh -R :2222:127.0.0.1:22 v0@frp.localhost.direct -p 2200 tcpmux --mux httpconnect --sd organization --user user --token '$FRP_TOKEN'
