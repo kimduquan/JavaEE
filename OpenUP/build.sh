f297ca6e43d8850e88b9456f4bd5deee2507f1f1
kimduquan <kimduquan@gmail.com>
2026-04-17 05:43:35 +0700

    #

--- OpenUP/build.sh
+++ OpenUP/build.sh
@@ -1,12 +1,85 @@
-. ./env.sh
-git pull
+./uninstall.sh
+
+cd EPF-agent-webapp
 git clean -f -d
-cp settings.xml ~/.m2
-./clean.sh
-./copy_dependency.sh
-#gu install native-image
-. ./config.sh
-./startup.sh
-#sudo apt-get install build-essential libz-dev zlib1g-dev
-mvn clean install -U -Depf-shell-native -Depf-gateway-native -Depf-persistence-native
-./shutdown.sh
+./build.sh
+
+cd webapp/agent
+git clean -f -d
+./build.sh
+cd ../../../
+
+cd EPF-cache
+git clean -f -d
+./build.sh
+cd ../
+
+cd EPF-concurrent
+git clean -f -d
+./build.sh
+cd ../
+
+cd EPF-config
+git clean -f -d
+./build.sh
+cd ../
+
+cd EPF-event
+git clean -f -d
+./build.sh
+cd ../
+
+cd EPF-gateway
+git clean -f -d
+./build.sh
+cd ../
+
+cd EPF-management
+git clean -f -d
+./build.sh
+cd ../
+
+cd EPF-mcp-gateway
+git clean -f -d
+./build.sh
+cd ../
+
+cd EPF-messaging
+git clean -f -d
+./build.sh
+cd ../
+
+cd EPF-net
+git clean -f -d
+./build.sh
+cd ../
+
+cd EPF-persistence
+git clean -f -d
+./build.sh
+cd ../
+
+cd EPF-query
+git clean -f -d
+./build.sh
+cd ../
+
+cd EPF-registry
+git clean -f -d
+./build.sh
+cd ../
+
+cd EPF-transaction
+git clean -f -d
+./build.sh
+cd ../
+
+cd EPF-workflow-management
+git clean -f -d
+./build.sh
+cd ../
+
+cd EPF-workflow-v1
+git clean -f -d
+./build.sh
+cd ../
