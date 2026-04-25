86e6c271672172d3fd70b9387480822bd9b0b164
kimduquan <kimduquan@gmail.com>
2026-04-25 20:33:25 +0700

    #

--- OpenUP/install.sh
+++ OpenUP/install.sh
@@ -1,4 +1,4 @@
 kubectl create secret generic nextauth --from-literal=NEXTAUTH_SECRET="$(openssl rand -hex 32)"
 kubectl create secret generic copilotkit --from-literal=PUBLIC_LICENSE_KEY=""
 helm dependency build EPF-parent-ext
-helm upgrade --install epf-parent EPF-parent-ext --wait
+helm upgrade --install epf-parent EPF-parent-ext --wait --debug
