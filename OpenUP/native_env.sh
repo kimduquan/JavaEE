977c8866c1bfc8ef456dcb5496458f2fe4e5ffc6
kimduquan <kimduquan@gmail.com>
2026-04-25 20:33:46 +0700

    #

--- OpenUP/native_env.sh
+++ OpenUP/native_env.sh
@@ -1,3 +1,3 @@
-export GRAALVM_HOME=~/graalvm-community-openjdk-25.0.1+8.1
+export GRAALVM_HOME=~/graalvm-community-openjdk-25.0.2+10.1
 #export JAVA_HOME=$GRAALVM_HOME
 #export PATH=$PATH:$GRAALVM_HOME/bin
