b8794400c5a10cda4b73043c42ca064f365b5b45
kimduquan <kimduquan@gmail.com>
2026-04-26 09:38:20 +0700

    #

--- OpenUP/env.sh
+++ OpenUP/env.sh
@@ -3,5 +3,5 @@ export SOURCE_DIR=~/git/JavaEE/OpenUP
 export FRP_SOURCE_DIR=~/git/frp
 export ODOO_SOURCE_DIR=~/git/bitnami/containers/bitnami/odoo/19/debian-12
 export JAVA_HOME=~/jdk-21.0.9+10
-export MAVEN_HOME=~/apache-maven-3.9.12/bin
+export MAVEN_HOME=~/apache-maven-3.9.15/bin
 export PATH=$MAVEN_HOME:$PATH
