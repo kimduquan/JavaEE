e7d426437ccdf9e7dddc0976ed7161b65f51356b
kimduquan <kimduquan@gmail.com>
2026-06-13 19:54:32 +0700

    #

--- OpenUP/install.sh
+++ OpenUP/install.sh
@@ -1,6 +1,6 @@
 kubectl create secret generic nextauth --from-literal=NEXTAUTH_SECRET="$(openssl rand -hex 32)"
 kubectl create secret generic copilotkit --from-literal=PUBLIC_LICENSE_KEY=""
 kubectl create secret generic stripe --from-literal=STRIPE_API_KEY=""
-kubectl create configmap stripe --from-literal=DEFAULT_PRODUCT_ID=""
+kubectl create configmap stripe --from-literal=PRODUCT_ID=""
 helm dependency build EPF-parent-ext
 helm upgrade --install epf-parent EPF-parent-ext --wait --debug
--- OpenUP/webapp_deploy.sh
+++ OpenUP/webapp_deploy.sh
@@ -1,20 +1,4 @@
 . ./env.sh
-kubectl port-forward svc/wildfly-internal 9990:9990 &
-cd EPF-webapp
+kubectl port-forward svc/wildfly 9990:9990 &
+cd EPF-webapp-v2
 mvn wildfly:deploy
-cd ../
-cd EPF-messaging-webapp
-mvn wildfly:deploy
-cd ../
-cd EPF-persistence-webapp
-mvn wildfly:deploy
-cd ../
-cd EPF-security-auth-webapp
-mvn wildfly:deploy
-cd ../
-cd EPF-security-webapp
-mvn wildfly:deploy
-cd ../
-cd EPF-workflow-webapp
-mvn wildfly:deploy
-cd ../
