265df8c4a3d61c28cacaa0a339364b5f47c50efb
kimduquan <kimduquan@gmail.com>
2026-06-21 21:18:16 +0700

    #

--- OpenUP/RustFS/rustfs.sh
+++ OpenUP/RustFS/rustfs.sh
@@ -1,3 +1,3 @@
 helm repo add rustfs https://charts.rustfs.com
-kubectl create secret generic rustfs --from-literal=access_key='85da6aac2aba19a6dbf66034d199af599616644578306c8f7116e86f4fb0a7a3' --from-literal=secret_key='064f13d6b689a11459949e5d5259158230eed290780de054b50c60e1d3c130fd'
+kubectl create secret generic rustfs --from-literal=RUSTFS_ACCESS_KEY='85da6aac2aba19a6dbf66034d199af599616644578306c8f7116e86f4fb0a7a3' --from-literal=RUSTFS_SECRET_KEY='064f13d6b689a11459949e5d5259158230eed290780de054b50c60e1d3c130fd'
 helm upgrade --install rustfs rustfs/rustfs -f values-rustfs.yaml --wait
