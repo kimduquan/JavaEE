51989db71424dbb5e30bcc20e9a7660eabadb3e8
kimduquan <kimduquan@gmail.com>
2026-06-28 00:29:03 +0700

    #

--- OpenUP/Keycloak/keycloak.sh
+++ OpenUP/Keycloak/keycloak.sh
@@ -1,3 +1,3 @@
 kubectl delete secret keycloak-admin
 kubectl create secret generic keycloak-admin --from-literal=CLIENT_SECRET='y1wf2SyO6p8eZq65KbvbNB1gE0cEYyMx' --from-literal=CLIENT_ID='admin-cli'
-helm install keycloak oci://registry-1.docker.io/bitnamicharts/keycloak -f values-keycloak.yaml --wait
+helm upgrade --install keycloak oci://registry-1.docker.io/bitnamicharts/keycloak -f values-keycloak.yaml --wait
