a1486823a7e9045de4810e150eda6751603f3984
kimduquan <kimduquan@gmail.com>
2026-06-28 02:57:01 +0700

    #

--- OpenUP/NGINX/nginx.sh
+++ OpenUP/NGINX/nginx.sh
@@ -1,3 +1,3 @@
 ssh-keygen -q -t ed25519 -N "" -C "kimduquan03@gmail.com" -f id_ed25519
 kubectl create secret generic git-ssh --type=kubernetes.io/ssh-auth --from-file=ssh-privatekey=id_ed25519 --from-file=ssh-publickey=id_ed25519.pub
-helm install nginx oci://registry-1.docker.io/bitnamicharts/nginx -f values.yaml --wait
+helm upgrade --install nginx oci://registry-1.docker.io/bitnamicharts/nginx -f values.yaml --wait
