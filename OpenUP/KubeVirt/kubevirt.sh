a6c752f930cbf4dc4970fb8c7841a19260fe6842
kimduquan <kimduquan@gmail.com>
2026-06-29 21:38:00 +0700

    #

--- OpenUP/KubeVirt/kubevirt.sh
+++ OpenUP/KubeVirt/kubevirt.sh
@@ -1,5 +1,5 @@
 export RELEASE=$(curl https://storage.googleapis.com/kubevirt-prow/release/kubevirt/kubevirt/stable.txt)
 kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/${RELEASE}/kubevirt-operator.yaml
 kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/${RELEASE}/kubevirt-cr.yaml
-kubectl -n kubevirt wait kv kubevirt --for condition=Available
-#kubectl edit -n kubevirt kubevirt kubevirt
+kubectl -n kubevirt wait kv kubevirt --for condition=Ready
+kubectl patch kubevirt kubevirt -n kubevirt --type=merge -p '{ "spec": { "configuration": { "developerConfiguration": { "useEmulation": true } } } }'
