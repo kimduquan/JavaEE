5b49e00887c55c822ce99b9964b7b38072d132d8
kimduquan <kimduquan@gmail.com>
2026-06-29 21:02:34 +0700

    #

--- OpenUP/KServe/kserve.sh
+++ OpenUP/KServe/kserve.sh
@@ -1,3 +1,2 @@
-kubectl create namespace kserve
 helm upgrade --install --wait kserve-crd oci://ghcr.io/kserve/charts/kserve-crd --version v0.18.0
 helm upgrade --install --wait kserve oci://ghcr.io/kserve/charts/kserve-resources --version v0.18.0 --set kserve.controller.deploymentMode=Standard --set kserve.controller.gateway.ingressGateway.className=haproxy
