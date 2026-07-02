2e7220c7ee654d5119434cbcf76e8a37f26befce
kimduquan <kimduquan@gmail.com>
2026-07-02 21:49:44 +0700

    #

--- OpenUP/HAProxy/haproxy.sh
+++ OpenUP/HAProxy/haproxy.sh
@@ -1,2 +1,2 @@
 kubectl apply -f tcp.yaml
-helm install haproxy oci://ghcr.io/haproxytech/helm-charts/kubernetes-ingress -f values.yaml --wait
+helm install haproxy oci://ghcr.io/haproxytech/helm-charts/kubernetes-ingress -f values-kubernetes-ingress.yaml --wait
