c00748b566f1f39ad90201fe0fa6ef44e10d089a
kimduquan <kimduquan@gmail.com>
2026-07-04 06:12:29 +0700

    #

--- OpenUP/MicroK8s/microk8s.sh
+++ OpenUP/MicroK8s/microk8s.sh
@@ -1,6 +1,5 @@
 sudo snap install microk8s --classic
 sudo microk8s status --wait-ready
-sudo microk8s enable dns
 sudo microk8s enable metallb
 sudo microk8s enable rbac
 sudo microk8s enable registry
