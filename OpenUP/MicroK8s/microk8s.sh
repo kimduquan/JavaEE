ba9b6dd3508f4ec6b0e98740bded125f34c039a5
kimduquan <kimduquan@gmail.com>
2026-07-04 06:46:03 +0700

    #

--- OpenUP/MicroK8s/microk8s.sh
+++ OpenUP/MicroK8s/microk8s.sh
@@ -3,6 +3,7 @@ sudo snap install kubectl --classic
 sudo microk8s status --wait-ready
 sudo microk8s enable cis-hardening
 sudo microk8s enable metallb 10.64.140.43-10.64.140.49
+sudo microk8s enable host-access
 sudo microk8s enable registry
 sudo microk8s enable nvidia
 sudo microk8s config > ~/.kube/config
