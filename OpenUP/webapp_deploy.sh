27988576a753e76c421b06c7095c21f472d7f30b
kimduquan <kimduquan@gmail.com>
2026-07-05 13:14:43 +0700

    #

--- OpenUP/webapp_deploy.sh
+++ OpenUP/webapp_deploy.sh
@@ -2,3 +2,7 @@
 kubectl port-forward svc/wildfly 9990:9990 &
 cd EPF-webapp-v2
 mvn wildfly:deploy
+kubectl exec $(kubectl get pods -l 'app.kubernetes.io/name=wildfly' -o jsonpath='{.items[0].metadata.name}') -c wildfly -- /opt/bitnami/wildfly/bin/jboss-cli.sh --connect --command='/subsystem=undertow/application-security-domain=other:write-attribute(name=integrated-jaspi, value=false)'
+kubectl exec $(kubectl get pods -l 'app.kubernetes.io/name=wildfly' -o jsonpath='{.items[0].metadata.name}') -c wildfly -- /opt/bitnami/wildfly/bin/jboss-cli.sh --connect --command='/subsystem=undertow/server=default-server/host=default-host/location="\/":remove()'
+kubectl exec $(kubectl get pods -l 'app.kubernetes.io/name=wildfly' -o jsonpath='{.items[0].metadata.name}') -c wildfly -- /opt/bitnami/wildfly/bin/jboss-cli.sh --connect --command='/subsystem=undertow/configuration=handler/file=welcome-content:remove()'
+kubectl exec $(kubectl get pods -l 'app.kubernetes.io/name=wildfly' -o jsonpath='{.items[0].metadata.name}') -c wildfly -- /opt/bitnami/wildfly/bin/jboss-cli.sh --connect --command=':reload'
