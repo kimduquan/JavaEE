9f68724105fef57cccaf5839f59df39f3989aa08
kimduquan <kimduquan@gmail.com>
2026-07-05 19:33:18 +0700

    #

--- OpenUP/GPU/nvidia-device-plugin.sh
+++ OpenUP/GPU/nvidia-device-plugin.sh
@@ -1,4 +1,5 @@
 helm repo add nvdp https://nvidia.github.io/k8s-device-plugin
 helm repo update
 kubectl apply -f runtime.yaml
-helm upgrade -i nvdp nvdp/nvidia-device-plugin --namespace nvdp --create-namespace -f values.yaml --wait
+#helm upgrade -i nvdp nvdp/nvidia-device-plugin --namespace nvdp --create-namespace -f values.yaml --wait
+helm upgrade -i nvdp nvdp/nvidia-device-plugin -f values.yaml --wait
