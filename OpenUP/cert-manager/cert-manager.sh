bb4a1d18b49f6c15ec7e05304ef30724eb47323c
kimduquan <kimduquan@gmail.com>
2026-07-08 21:50:43 +0700

    #

--- OpenUP/cert-manager/cert-manager.sh
+++ OpenUP/cert-manager/cert-manager.sh
@@ -1,2 +1,2 @@
 helm repo add jetstack https://charts.jetstack.io --force-update
-helm install cert-manager jetstack/cert-manager -f values-cert-manager.yaml --wait
+helm upgrade --install cert-manager jetstack/cert-manager -f values-cert-manager.yaml --wait
