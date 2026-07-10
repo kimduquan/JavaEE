a9083ae4139bcbc75ad84977830e5d8a205f023f
kimduquan <kimduquan@gmail.com>
2026-07-11 06:49:24 +0700

    #

--- OpenUP/KubeAI/kubeai.sh
+++ OpenUP/KubeAI/kubeai.sh
@@ -2,4 +2,4 @@ helm repo add kubeai https://www.kubeai.org
 helm repo update
 kubectl create secret generic huggingface --from-literal=token=''
 kubectl apply -f pvc.yaml
-helm upgrade --install kubeai kubeai/kubeai --wait
+helm upgrade --install kubeai kubeai/kubeai --wait -f values.yaml
