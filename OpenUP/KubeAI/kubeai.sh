71cf23876d617750db1c94b3ff7959ec1947d91f
kimduquan <kimduquan@gmail.com>
2026-07-11 15:15:55 +0700

    #

--- OpenUP/KubeAI/kubeai.sh
+++ OpenUP/KubeAI/kubeai.sh
@@ -2,7 +2,7 @@ helm repo add kubeai https://www.kubeai.org
 helm repo update
 kubectl create secret generic huggingface --from-literal=token=''
 kubectl apply -f pvc.yaml
-#kubectl apply -f job.yaml
+kubectl apply -f job.yaml
 #VOLUME_NAME=$(kubectl get pvc model-pvc -o jsonpath='{.spec.volumeName}')
 #HOST_PATH=$(kubectl get pv $VOLUME_NAME -o jsonpath='{.spec.hostPath.path}')
 #cp ~/gemma-4-E4B_q4_0-it.gguf $HOST_PATH
