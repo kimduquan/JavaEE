d20b8b5c65bf1c2f57d0f69f40fdc920cafd3a57
kimduquan <kimduquan@gmail.com>
2026-07-11 08:06:28 +0700

    #

--- OpenUP/KubeAI/kubeai.sh
+++ OpenUP/KubeAI/kubeai.sh
@@ -3,6 +3,7 @@ helm repo update
 kubectl create secret generic huggingface --from-literal=token=''
 kubectl apply -f pvc.yaml
 kubectl apply -f job.yaml
+kubectl logs -f jobs/ollama-load-model-to-pvc
 kubectl wait --for=condition=complete job/ollama-load-model-to-pvc
 helm upgrade --install kubeai kubeai/kubeai --wait -f values.yaml
 helm upgrade --install --reuse-values --wait kubeai-models kubeai/models -f models/values.yaml
