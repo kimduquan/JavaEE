43ec539327126c015002fd5d9dc51bd4066f9b4c
kimduquan <kimduquan@gmail.com>
2026-07-18 15:40:17 +0700

    #

--- OpenUP/env.sh
+++ OpenUP/env.sh
@@ -5,3 +5,4 @@ export ODOO_SOURCE_DIR=~/git/bitnami/containers/bitnami/odoo/19/debian-12
 export JAVA_HOME=~/jdk-21.0.9+10
 export MAVEN_HOME=~/apache-maven-3.9.15/bin
 export PATH=$MAVEN_HOME:$PATH
+export DOCKER_BUILDKIT=1
