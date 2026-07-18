ae1b735de6757b43ea9b61447df49a8884658dce
kimduquan <kimduquan@gmail.com>
2026-07-18 17:57:55 +0700

    #

--- OpenUP/PostgreSQL/postgresql.sh
+++ OpenUP/PostgreSQL/postgresql.sh
@@ -1,2 +1,4 @@
 kubectl create configmap initdb --from-file=initdb.sql
+kubectl delete pvc/data-postgresql-primary-0
+kubectl delete pvc/data-postgresql-read-0
 helm install postgresql oci://registry-1.docker.io/bitnamicharts/postgresql -f values-postgresql.yaml --wait
