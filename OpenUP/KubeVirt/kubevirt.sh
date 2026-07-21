dd298ca259ef21b8e2060bafd096ece740b08afb
kimduquan <kimduquan@gmail.com>
2026-07-21 07:08:44 +0700

    #

--- OpenUP/KubeVirt/kubevirt.sh
+++ OpenUP/KubeVirt/kubevirt.sh
@@ -1,3 +1,4 @@
+sudo ln -s /var/snap/microk8s/common/var/lib/kubelet /var/lib/kubelet
 #export RELEASE=$(curl https://storage.googleapis.com/kubevirt-prow/release/kubevirt/kubevirt/stable.txt)
 #kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/${RELEASE}/kubevirt-operator.yaml
 #kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/${RELEASE}/kubevirt-cr.yaml
