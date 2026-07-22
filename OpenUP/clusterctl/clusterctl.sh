d47dafa188442af44b0566e17c9feed548bcbb67
kimduquan <kimduquan@gmail.com>
2026-07-22 23:54:17 +0700

    #

--- OpenUP/clusterctl/clusterctl.sh
+++ OpenUP/clusterctl/clusterctl.sh
@@ -8,4 +8,4 @@ export CAPK_GUEST_K8S_VERSION="${NODE_VM_IMAGE_TEMPLATE/*:/}"
 export CRI_PATH="unix:///var/run/containerd/containerd.sock"
 #clusterctl generate cluster epf-cluster --kubernetes-version ${CAPK_GUEST_K8S_VERSION} --flavor lb --control-plane-machine-count 1 --infrastructure kubevirt --target-namespace default > epf-cluster.yaml
 kubectl apply -f epf-cluster.yaml
-clusterctl upgrade apply --contract v1beta2 --wait-providers
+#clusterctl upgrade apply --contract v1beta2 --wait-providers
