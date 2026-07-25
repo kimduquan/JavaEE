c152e20ef58ca1cdbb9a74ada19b50d1f43c183d
kimduquan <kimduquan@gmail.com>
2026-07-25 17:55:48 +0700

    #

--- OpenUP/clusterctl/capd/capd.sh
+++ OpenUP/clusterctl/capd/capd.sh
@@ -1,5 +1,5 @@
 export CLUSTER_TOPOLOGY=true
 clusterctl init --infrastructure docker --target-namespace default --wait-providers
 rm epf-cluster.yaml
-clusterctl generate cluster epf-cluster --kubernetes-version v1.35.1 --infrastructure docker --target-namespace default > epf-cluster.yaml
+clusterctl generate cluster epf-cluster --kubernetes-version v1.34.1 --flavor development --target-namespace default > epf-cluster.yaml
 kubectl apply -f epf-cluster.yaml
