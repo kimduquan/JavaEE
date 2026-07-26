2268e6f06766743cb8021815141087c707169425
kimduquan <kimduquan@gmail.com>
2026-07-26 20:11:03 +0700

    #

--- OpenUP/KubeVirt/kubevirt.sh
+++ OpenUP/KubeVirt/kubevirt.sh
@@ -1,5 +1,6 @@
-#sudo mount --rbind /var/snap/microk8s/common/var/lib/kubelet /var/lib/kubelet
-#sudo mount --make-rshared /var/lib/kubelet
+sudo unlink /var/lib/kubelet
+sudo mount --rbind /var/snap/microk8s/common/var/lib/kubelet /var/lib/kubelet
+sudo mount --make-rshared /var/lib/kubelet
 #export RELEASE=$(curl https://storage.googleapis.com/kubevirt-prow/release/kubevirt/kubevirt/stable.txt)
 #kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/${RELEASE}/kubevirt-operator.yaml
 #kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/${RELEASE}/kubevirt-cr.yaml
