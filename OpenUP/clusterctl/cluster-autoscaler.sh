83940b8126353e3c6ccf2682011596d3fd2316c5
kimduquan <kimduquan@gmail.com>
2026-08-02 22:46:58 +0700

    #

--- OpenUP/clusterctl/cluster-autoscaler.sh
+++ OpenUP/clusterctl/cluster-autoscaler.sh
@@ -7,3 +7,8 @@ kubectl --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} apply -f https://raw.githubuserc
 kubectl delete secret epf-cluster
 kubectl create secret generic epf-cluster --from-file=value=${EPF_CLUSTER_KUBE_CONFIG}
 helm upgrade --install --wait cluster-autoscaler autoscaler/cluster-autoscaler -f values-cluster-autoscaler.yaml
+rm epf-cluster-ssh-key
+kubectl get secret epf-cluster-ssh-keys -o jsonpath='{.data.key}' | base64 --decode > epf-cluster-ssh-key
+chmod 600 epf-cluster-ssh-key
+VM_IP=$(kubectl get vmi --no-headers | awk '{print $4}')
+ssh -i epf-cluster-ssh-key capk@${VM_IP}
