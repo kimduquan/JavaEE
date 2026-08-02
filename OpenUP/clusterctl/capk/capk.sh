aef8958f698e26f7c1463970ee85fc8c7f7010ee
kimduquan <kimduquan@gmail.com>
2026-08-02 07:25:40 +0700

    #

--- OpenUP/clusterctl/capk/capk.sh
+++ OpenUP/clusterctl/capk/capk.sh
@@ -1,4 +1,5 @@
 export CLUSTER_TOPOLOGY=true
+clusterctl upgrade apply --contract v1beta2
 clusterctl init --infrastructure kubevirt --target-namespace default --wait-providers
 export NODE_VM_IMAGE_TEMPLATE="quay.io/capk/ubuntu-2404-container-disk:v1.34.1"
 export CAPK_GUEST_K8S_VERSION="${NODE_VM_IMAGE_TEMPLATE/*:/}"
@@ -6,7 +7,6 @@ export CRI_PATH="unix:///var/run/containerd/containerd.sock"
 clusterctl generate cluster epf-cluster --kubernetes-version ${CAPK_GUEST_K8S_VERSION} --flavor lb --control-plane-machine-count 1 --infrastructure kubevirt --target-namespace default > epf-cluster.yaml
 kubectl apply -f epf-cluster.yaml
 kubectl wait cluster epf-cluster --for condition=Available
-clusterctl upgrade apply --contract v1beta2
 #. ../../env.sh
 #rm ${EPF_CLUSTER_KUBE_CONFIG}
 #clusterctl get kubeconfig epf-cluster > ${EPF_CLUSTER_KUBE_CONFIG}
