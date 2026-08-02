e4ac6f6d8e341569bfb93326ddab807f480e05a2
kimduquan <kimduquan@gmail.com>
2026-08-02 12:58:53 +0700

    #

--- OpenUP/clusterctl/capd/capd.sh
+++
@@ -1,8 +0,0 @@
-export CLUSTER_TOPOLOGY=true
-clusterctl init --infrastructure docker --target-namespace default --wait-providers
-rm epf-cluster.yaml
-clusterctl generate cluster epf-cluster --kubernetes-version v1.34.1 --flavor development --target-namespace default --control-plane-machine-count=1 --worker-machine-count=1 > epf-cluster.yaml
-kubectl apply -f epf-cluster.yaml
-rm epf-cluster.kubeconfig
-clusterctl get kubeconfig epf-cluster > epf-cluster.kubeconfig
-kubectl --kubeconfig=epf-cluster.kubeconfig apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/calico.yaml
--- OpenUP/clusterctl/cluster-autoscaler.sh
+++ OpenUP/clusterctl/cluster-autoscaler.sh
@@ -5,5 +5,5 @@ rm ${EPF_CLUSTER_KUBE_CONFIG}
 clusterctl get kubeconfig epf-cluster > ${EPF_CLUSTER_KUBE_CONFIG}
 kubectl --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.32.1/manifests/calico.yaml
 kubectl delete secret epf-cluster
-kubectl create secret generic epf-cluster --from-file=epf-cluster=${EPF_CLUSTER_KUBE_CONFIG}
+kubectl create secret generic epf-cluster --from-file=${EPF_CLUSTER_KUBE_CONFIG}
 helm upgrade --install --wait cluster-autoscaler autoscaler/cluster-autoscaler -f values-cluster-autoscaler.yaml
