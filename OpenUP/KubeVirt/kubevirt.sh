3eb07867437a74295fc74e29c9f7550acb36038c
kimduquan <kimduquan@gmail.com>
2026-08-02 08:56:19 +0700

    #

--- OpenUP/KubeVirt/kubevirt.sh
+++ OpenUP/KubeVirt/kubevirt.sh
@@ -1,10 +1,7 @@
-#sudo unlink /var/lib/kubelet
-#sudo mount --rbind /var/snap/microk8s/common/var/lib/kubelet /var/lib/kubelet
-#sudo mount --make-rshared /var/lib/kubelet
 #export RELEASE=$(curl https://storage.googleapis.com/kubevirt-prow/release/kubevirt/kubevirt/stable.txt)
 #kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/${RELEASE}/kubevirt-operator.yaml
 #kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/${RELEASE}/kubevirt-cr.yaml
 kubectl apply -f kubevirt-operator.yaml
 kubectl apply -f kubevirt-cr.yaml
 kubectl -n kubevirt wait kv kubevirt --for condition=Available
-#kubectl patch -n kubevirt kubevirt kubevirt --type merge --patch '{"spec": {"infra": {"nodePlacement": {"nodeSelector": {"node-role.kubernetes.io/control-plane": ""}}}}}'
+kubectl patch -n kubevirt kubevirt kubevirt --type merge --patch '{"spec": {"infra": {"nodePlacement": {"nodeSelector": {"node-role.kubernetes.io/control-plane": ""}}}}}'
