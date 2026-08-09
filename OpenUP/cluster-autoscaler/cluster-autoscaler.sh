4cd3bd65ba6058134916306e1eac8c92b0aa9171
kimduquan <kimduquan@gmail.com>
2026-08-09 21:30:27 +0700

    #

--- OpenUP/cluster-autoscaler/cluster-autoscaler.sh
+++ OpenUP/cluster-autoscaler/cluster-autoscaler.sh
@@ -1,7 +1,5 @@
 helm repo add autoscaler https://kubernetes.github.io/autoscaler
 helm repo update
-. ../env.sh
-kubectl --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.32.1/manifests/calico.yaml
 kubectl delete secret epf-cluster
 kubectl create secret generic epf-cluster --from-file=value=${EPF_CLUSTER_KUBE_CONFIG}
 helm upgrade --install --wait cluster-autoscaler autoscaler/cluster-autoscaler -f values-cluster-autoscaler.yaml
