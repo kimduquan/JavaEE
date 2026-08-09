3abdf5133334ace8de8e75e2db1fce2822b0248e
kimduquan <kimduquan@gmail.com>
2026-08-10 00:32:55 +0700

    #

--- OpenUP/EPF-cluster/Calico/calico.sh
+++ OpenUP/EPF-cluster/Calico/calico.sh
@@ -1,2 +1,2 @@
-. ../env.sh
+. ../../env.sh
 kubectl --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.32.1/manifests/calico.yaml
--- OpenUP/EPF-cluster/Rook-Ceph/rook-ceph.sh
+++ OpenUP/EPF-cluster/Rook-Ceph/rook-ceph.sh
@@ -1,7 +1,6 @@
-. ../env.sh
-export KUBECONFIG=$
+. ../../env.sh
 helm repo add rook https://charts.rook.io/release
-helm install --create-namespace --namespace rook-ceph rook-ceph rook/rook-ceph -f values.yaml
-helm install --create-namespace --namespace rook-ceph rook-ceph-cluster --set operatorNamespace=rook-ceph rook/rook-ceph-cluster -f values-external.yaml
+helm --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} upgrade --install --create-namespace --namespace rook-ceph --wait rook-ceph rook/rook-ceph -f values.yaml
+helm --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} upgrade --install --create-namespace --namespace rook-ceph --wait rook-ceph-cluster --set operatorNamespace=rook-ceph rook/rook-ceph-cluster -f values-external.yaml
 helm repo add ceph-csi-operator https://ceph.github.io/ceph-csi-operator
-helm install ceph-csi-drivers --namespace rook-ceph ceph-csi-operator/ceph-csi-drivers -f https://raw.githubusercontent.com/rook/rook/master/deploy/charts/ceph-csi-drivers/values.yaml
+helm --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} upgrade --install ceph-csi-drivers --namespace rook-ceph --wait ceph-csi-operator/ceph-csi-drivers -f https://raw.githubusercontent.com/rook/rook/master/deploy/charts/ceph-csi-drivers/values.yaml
