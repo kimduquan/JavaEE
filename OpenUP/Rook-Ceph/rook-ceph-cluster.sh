171c69a1753b618522e4e9f3f0328a1741d12b39
kimduquan <kimduquan@gmail.com>
2026-08-09 09:20:23 +0700

    #

--- OpenUP/Rook-Ceph/rook-ceph-cluster.sh
+++ OpenUP/Rook-Ceph/rook-ceph-cluster.sh
@@ -1,2 +1,2 @@
 helm repo add rook https://charts.rook.io/release
-helm upgrade --install --create-namespace --namespace rook-ceph rook-ceph-cluster --set operatorNamespace=rook-ceph rook/rook-ceph-cluster -f values-rook-ceph-cluster.yaml
+helm upgrade --install --create-namespace --namespace rook-ceph rook-ceph-cluster rook/rook-ceph-cluster --wait -f values-rook-ceph-cluster.yaml
