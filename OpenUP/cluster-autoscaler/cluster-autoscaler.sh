117a20c5011141f2b122cd6c4613f61013eaab20
kimduquan <kimduquan@gmail.com>
2026-08-09 21:25:49 +0700

    #

--- OpenUP/cluster-autoscaler/cluster-autoscaler.sh
+++ OpenUP/cluster-autoscaler/cluster-autoscaler.sh
@@ -1,14 +1,9 @@
 helm repo add autoscaler https://kubernetes.github.io/autoscaler
 helm repo update
 . ../env.sh
-rm ${EPF_CLUSTER_KUBE_CONFIG}
-clusterctl get kubeconfig epf-cluster > ${EPF_CLUSTER_KUBE_CONFIG}
 kubectl --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.32.1/manifests/calico.yaml
 kubectl delete secret epf-cluster
 kubectl create secret generic epf-cluster --from-file=value=${EPF_CLUSTER_KUBE_CONFIG}
 helm upgrade --install --wait cluster-autoscaler autoscaler/cluster-autoscaler -f values-cluster-autoscaler.yaml
-rm epf-cluster-ssh-key
-kubectl get secret epf-cluster-ssh-keys -o jsonpath='{.data.key}' | base64 --decode > epf-cluster-ssh-key
-chmod 600 epf-cluster-ssh-key
 VM_IP=$(kubectl get vmi --no-headers | awk '{print $4}')
-ssh -i epf-cluster-ssh-key capk@${VM_IP}
+ssh -i ${EPF_CLUSTER_SSH_KEY} capk@${VM_IP}
