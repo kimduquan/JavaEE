ecb250a22d18c77a51c2eefa538cc3341f8d4d69
kimduquan <kimduquan@gmail.com>
2026-08-13 20:58:43 +0700

    #

--- OpenUP/EPF-cluster/epf-cluster.sh
+++ OpenUP/EPF-cluster/epf-cluster.sh
@@ -6,12 +6,18 @@ export CRI_PATH="unix:///var/run/containerd/containerd.sock"
 #rm epf-cluster.yaml
 #clusterctl generate cluster epf-cluster --kubernetes-version ${CAPK_GUEST_K8S_VERSION} --flavor lb-kccm --control-plane-machine-count 1 --infrastructure kubevirt --target-namespace default > epf-cluster.yaml
 kubectl apply -f epf-cluster.yaml
-kubectl wait cluster epf-cluster --for condition=Available
+kubectl wait cluster epf-cluster --for condition=InfrastructureReady
 clusterctl upgrade apply --contract v1beta2 --wait-providers
-kubectl wait cluster epf-cluster --for condition=Available
+kubectl wait cluster epf-cluster --for condition=InfrastructureReady
 . ../env.sh
 rm ${EPF_CLUSTER_KUBE_CONFIG}
 clusterctl get kubeconfig epf-cluster > ${EPF_CLUSTER_KUBE_CONFIG}
 rm ${EPF_CLUSTER_SSH_KEY}
 kubectl get secret epf-cluster-ssh-keys -o jsonpath='{.data.key}' | base64 --decode > ${EPF_CLUSTER_SSH_KEY}
 chmod 600 ${EPF_CLUSTER_SSH_KEY}
+
+cd Calico
+./calico.sh
+cd ../
+
+kubectl wait cluster epf-cluster --for condition=Available
