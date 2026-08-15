f69b530a0aa81df6de34dacbbbf027f0667e84f4
kimduquan <kimduquan@gmail.com>
2026-08-15 17:32:00 +0700

    #

--- OpenUP/EPF-cluster/HAProxy/haproxy.sh
+++ OpenUP/EPF-cluster/HAProxy/haproxy.sh
@@ -1,2 +1,3 @@
-kubectl create secret tls localhost-direct --cert=/home/kimduquan/localhost.direct.SS.crt --key=/home/kimduquan/localhost.direct.SS.key
-helm upgrade --install haproxy oci://ghcr.io/haproxytech/helm-charts/kubernetes-ingress -f values-kubernetes-ingress.yaml --wait
+. ../../env.sh
+kubectl --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} create secret tls epf-cluster-tls --cert=${EPF_CLUSTER_CERT} --key=${EPF_CLUSTER_CERT_KEY}
+helm --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} upgrade --install haproxy oci://ghcr.io/haproxytech/helm-charts/kubernetes-ingress -f values-kubernetes-ingress.yaml --wait
