cd57e704bc0fee6248c22315baf485b1d193e2c0
kimduquan <kimduquan@gmail.com>
2026-08-16 08:14:01 +0700

    #

--- OpenUP/metrics-server/metrics-server.sh
+++ OpenUP/metrics-server/metrics-server.sh
@@ -1,2 +1,2 @@
 helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/ --force-update
-helm upgrade --install metrics-server metrics-server/metrics-server -f values-metrics-server.yaml --wait
+helm upgrade --install metrics-server metrics-server/metrics-server -f values-metrics-server.yaml --wait --timeout 20m
