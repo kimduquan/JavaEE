481eae384e74350a30fcb7caa820781e23d06b7d
kimduquan <kimduquan@gmail.com>
2026-08-16 10:19:09 +0700

    #

--- OpenUP/cluster-autoscaler/cluster-autoscaler.sh
+++ OpenUP/cluster-autoscaler/cluster-autoscaler.sh
@@ -3,6 +3,6 @@ helm repo update
 . ../env.sh
 kubectl delete secret epf-cluster
 kubectl create secret generic epf-cluster --from-file=value=${EPF_CLUSTER_KUBE_CONFIG}
-helm upgrade --install --wait cluster-autoscaler autoscaler/cluster-autoscaler -f values-cluster-autoscaler.yaml
+helm upgrade --install --wait --timeout 10m cluster-autoscaler autoscaler/cluster-autoscaler -f values-cluster-autoscaler.yaml
 VM_IP=$(kubectl get vmi --no-headers | awk '{print $4}')
 #ssh -i ${EPF_CLUSTER_SSH_KEY} capk@${VM_IP}
