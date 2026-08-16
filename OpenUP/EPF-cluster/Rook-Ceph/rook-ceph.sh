43c5667f03cdec4206a3cd45ff307cf322334b42
kimduquan <kimduquan@gmail.com>
2026-08-16 16:46:01 +0700

    #

--- OpenUP/EPF-cluster/Rook-Ceph/rook-ceph.sh
+++ OpenUP/EPF-cluster/Rook-Ceph/rook-ceph.sh
@@ -1,6 +1,6 @@
 . ../../env.sh
 helm repo add rook https://charts.rook.io/release
-helm --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} upgrade --install --create-namespace --namespace rook-ceph --wait --timeout 10m rook-ceph rook/rook-ceph -f values.yaml
+helm --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} upgrade --install --create-namespace --namespace rook-ceph --wait --timeout 20m rook-ceph rook/rook-ceph -f values.yaml
 helm --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} upgrade --install --create-namespace --namespace rook-ceph --wait --timeout 10m rook-ceph-cluster --set operatorNamespace=rook-ceph rook/rook-ceph-cluster -f values-external.yaml
 helm repo add ceph-csi-operator https://ceph.github.io/ceph-csi-operator
 helm --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} upgrade --install ceph-csi-drivers --namespace rook-ceph --wait --timeout 10m ceph-csi-operator/ceph-csi-drivers -f csi-values.yaml
