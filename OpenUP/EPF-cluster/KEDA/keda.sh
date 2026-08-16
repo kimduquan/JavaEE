3d04b9041863640bc5baa5ada6b8d62328ce7739
kimduquan <kimduquan@gmail.com>
2026-08-16 08:14:45 +0700

    #

--- OpenUP/EPF-cluster/KEDA/keda.sh
+++ OpenUP/EPF-cluster/KEDA/keda.sh
@@ -1,4 +1,4 @@
 . ../../env.sh
 helm repo add kedacore https://kedacore.github.io/charts
 helm repo update
-helm --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} upgrade --install --create-namespace --namespace keda --wait keda kedacore/keda -f values-keda.yaml
+helm --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} upgrade --install --create-namespace --namespace keda --wait --timeout 20m keda kedacore/keda -f values-keda.yaml
