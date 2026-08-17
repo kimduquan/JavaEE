a22b17d85ffc706674a73496a0e35d8efa8b82c5
kimduquan <kimduquan@gmail.com>
2026-08-18 04:15:01 +0700

    #

--- OpenUP/EPF-cluster/disk.sh
+++ OpenUP/EPF-cluster/disk.sh
@@ -3,6 +3,7 @@ sudo systemctl stop kubelet containerd
 sudo mkdir -p /mnt/var-lib-containerd
 sudo mount /dev/vdb /mnt/var-lib-containerd
 sudo rsync -aHAX /var/lib/containerd/ /mnt/var-lib-containerd/
+sudo umount /mnt/var-lib-containerd
 sudo rm -rf /var/lib/containerd/*
 sudo mount /dev/vdb /var/lib/containerd
 sudo mount --make-rshared /var/lib/containerd
