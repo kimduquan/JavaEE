586990b118f64a1692c1dd14e4d7e1e3e07b2a00
kimduquan <kimduquan@gmail.com>
2026-08-18 11:36:27 +0700

    #

--- OpenUP/Rook-Ceph/rook-ceph-cluster.sh
+++ OpenUP/Rook-Ceph/rook-ceph-cluster.sh
@@ -1,2 +1,2 @@
 helm repo add rook https://charts.rook.io/release
-helm upgrade --install --create-namespace --namespace rook-ceph rook-ceph-cluster rook/rook-ceph-cluster --wait -f values-rook-ceph-cluster.yaml
+helm upgrade --install --create-namespace --namespace rook-ceph rook-ceph-cluster rook/rook-ceph-cluster --wait --timeout 20m -f values-rook-ceph-cluster.yaml
--- OpenUP/Rook-Ceph/rook-ceph.sh
+++ OpenUP/Rook-Ceph/rook-ceph.sh
@@ -1,2 +1,2 @@
 helm repo add rook https://charts.rook.io/release
-helm upgrade --install --create-namespace --namespace rook-ceph rook-ceph rook/rook-ceph --wait -f values-rook-ceph.yaml
+helm upgrade --install --create-namespace --namespace rook-ceph rook-ceph rook/rook-ceph --wait --timeout 20m -f values-rook-ceph.yaml
