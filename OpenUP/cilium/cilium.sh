e385ee4c4644c3021d1d5386bce4e3e4f69adcdc
kimduquan <kimduquan@gmail.com>
2026-08-19 21:03:54 +0700

    #

--- OpenUP/cilium/cilium.sh
+++ OpenUP/cilium/cilium.sh
@@ -1,2 +1,2 @@
-$ helm repo add cilium https://helm.cilium.io/
-$ helm upgrade --install --wait cilium cilium/cilium --namespace=kube-system -f values-cilium.yaml
+helm repo add cilium https://helm.cilium.io/
+helm upgrade --install --wait cilium cilium/cilium --namespace=kube-system -f values-cilium.yaml
