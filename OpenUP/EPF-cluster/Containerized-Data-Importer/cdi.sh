294a1482e1e3b61e714344d5abad2575c8e13156
kimduquan <kimduquan@gmail.com>
2026-08-21 06:46:00 +0700

    #

---
+++ OpenUP/EPF-cluster/Containerized-Data-Importer/cdi.sh
@@ -0,0 +1,4 @@
+#export TAG=$(curl -s -w %{redirect_url} https://github.com/kubevirt/containerized-data-importer/releases/latest)
+#export VERSION=$(echo ${TAG##*/})
+#kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-operator.yaml
+#kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-cr.yaml
