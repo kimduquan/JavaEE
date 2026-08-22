6a602987f42f3025ee3490a26c2ea7f912da2600
kimduquan <kimduquan@gmail.com>
2026-08-23 04:19:04 +0700

    #

--- OpenUP/Rook-Ceph/rook-ceph-cluster.sh
+++ OpenUP/Rook-Ceph/rook-ceph-cluster.sh
@@ -1,2 +1,3 @@
 helm repo add rook https://charts.rook.io/release
 helm upgrade --install --create-namespace --namespace rook-ceph rook-ceph-cluster rook/rook-ceph-cluster --wait --timeout 20m -f values-rook-ceph-cluster.yaml
+kubectl wait -n rook-ceph cephcluster rook-ceph --for condition=Ready --timeout=1200s
