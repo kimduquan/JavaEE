4cb78ccb442b6d56633b82aa3b35fa74dac9a957
kimduquan <kimduquan@gmail.com>
2026-08-23 21:09:20 +0700

    #

--- OpenUP/Rook-Ceph/ceph-csi-drivers.sh
+++ OpenUP/Rook-Ceph/ceph-csi-drivers.sh
@@ -1,2 +1,3 @@
 helm repo add ceph-csi-operator https://ceph.github.io/ceph-csi-operator
 helm upgrade --install ceph-csi-drivers --namespace rook-ceph --wait --timeout 30m ceph-csi-operator/ceph-csi-drivers -f values.yaml
+kubectl wait -n rook-ceph -l app=rook-ceph.rbd.csi.ceph.com-ctrlplugin pod --for condition=Ready --timeout=1200s
