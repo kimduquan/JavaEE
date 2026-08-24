e59214dc32c1e7a196959d1f3f080f4a7b4fceef
kimduquan <kimduquan@gmail.com>
2026-08-25 01:31:58 +0700

    #

--- OpenUP/EPF-cluster/Calico/calico.sh
+++ OpenUP/EPF-cluster/Calico/calico.sh
@@ -1,2 +1,3 @@
 . ../../env.sh
-kubectl --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.32.1/manifests/calico.yaml
+#kubectl --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.32.1/manifests/calico.yaml
+kubectl --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} apply -f calico.yaml
