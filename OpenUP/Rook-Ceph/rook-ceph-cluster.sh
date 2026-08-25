0b2035295e3af0e02a1ed76aecab185aadf8a6ff
kimduquan <kimduquan@gmail.com>
2026-08-25 08:09:23 +0700

    #

--- OpenUP/Rook-Ceph/rook-ceph-cluster.sh
+++ OpenUP/Rook-Ceph/rook-ceph-cluster.sh
@@ -1,4 +1,4 @@
 helm repo add rook https://charts.rook.io/release
-helm upgrade --install --create-namespace --namespace rook-ceph rook-ceph-cluster rook/rook-ceph-cluster --wait --timeout 20m -f values-rook-ceph-cluster.yaml
+helm upgrade --install --create-namespace --namespace rook-ceph rook-ceph-cluster rook/rook-ceph-cluster --wait --timeout 20m --set cephObjectStores=null -f values-rook-ceph-cluster.yaml
 kubectl wait -n rook-ceph cephcluster rook-ceph --for condition=Ready --timeout=1200s
 kubectl wait -n rook-ceph --for=condition=Ready pod -l app=rook-ceph-osd --timeout=10m
