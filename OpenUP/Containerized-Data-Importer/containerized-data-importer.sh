a1a551aab319ee530197ac7a870b96b49a11fae6
kimduquan <kimduquan@gmail.com>
2026-08-26 22:48:02 +0700

    #

--- OpenUP/Containerized-Data-Importer/containerized-data-importer.sh
+++ OpenUP/Containerized-Data-Importer/containerized-data-importer.sh
@@ -4,4 +4,5 @@
 #kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/v1.66.0/cdi-cr.yaml
 kubectl create -f cdi-operator.yaml
 kubectl create -f cdi-cr.yaml
+kubectl wait -n cdi -l name=cdi-operator pod --for condition=Ready --timeout=1200s
 kubectl wait -n cdi -l app=containerized-data-importer pod --for condition=Ready --timeout=1200s
