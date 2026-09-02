fdbfab29bfd1afbefe370363ddc53f5abf94d7c8
kimduquan <kimduquan@gmail.com>
2026-09-02 13:21:55 +0700

    #

--- OpenUP/clusterctl/clusterctl.sh
+++ OpenUP/clusterctl/clusterctl.sh
@@ -1,3 +1,3 @@
-curl -L https://github.com/kubernetes-sigs/cluster-api/releases/download/v1.14.0/clusterctl-linux-amd64 -o clusterctl
+curl -L https://github.com/kubernetes-sigs/cluster-api/releases/download/v1.14.1/clusterctl-linux-amd64 -o clusterctl
 sudo install -o root -g root -m 0755 clusterctl /usr/local/bin/clusterctl
 clusterctl version
