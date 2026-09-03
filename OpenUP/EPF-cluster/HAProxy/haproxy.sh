fc83ef50469c9e7c35aa113ec828a58563d68f53
kimduquan <kimduquan@gmail.com>
2026-09-04 00:26:57 +0700

    #

--- OpenUP/EPF-cluster/HAProxy/haproxy.sh
+++
@@ -1,3 +0,0 @@
-. ../../env.sh
-kubectl --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} create secret tls epf-cluster-tls --cert=${EPF_CLUSTER_CERT} --key=${EPF_CLUSTER_CERT_KEY}
-helm --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} upgrade --install haproxy oci://ghcr.io/haproxytech/helm-charts/kubernetes-ingress -f values-kubernetes-ingress.yaml --wait --timeout 20m
