b28c08fe187b0891908120f4bde1519666c0d01d
kimduquan <kimduquan@gmail.com>
2026-09-03 11:14:31 +0700

    #

--- OpenUP/EPF-cluster/cilium/cilium.sh
+++ OpenUP/EPF-cluster/cilium/cilium.sh
@@ -1,2 +1,3 @@
+. ../../env.sh
 helm repo add cilium https://helm.cilium.io/
-helm upgrade --install --wait cilium cilium/cilium --version 1.20.1 --namespace=kube-system -f values-cilium.yaml
+helm --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} upgrade --install --wait cilium cilium/cilium --version 1.20.1 --namespace=kube-system -f values-cilium.yaml
