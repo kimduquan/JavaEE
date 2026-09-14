c37603932a91186474c4dece9a41a13d6954d8db
kimduquan <kimduquan@gmail.com>
2026-09-15 02:06:52 +0700

    #

--- OpenUP/KubeVirt/kubevirt.sh
+++ OpenUP/KubeVirt/kubevirt.sh
@@ -4,5 +4,5 @@
 kubectl apply -f kubevirt-operator.yaml
 kubectl apply -f kubevirt-cr.yaml
 kubectl -n kubevirt wait kv kubevirt --for condition=Available --timeout=600s
-kubectl patch -n kubevirt kubevirt kubevirt --type merge --patch '{"spec": {"infra": {"nodePlacement": {"nodeSelector": {"node-role.kubernetes.io/control-plane": ""}}}, "workloads": {"nodePlacement": {"nodeSelector": {"kubernetes.io/hostname": "desktop-q9gd575"}}}}}'
+kubectl patch -n kubevirt kubevirt kubevirt --type merge --patch '{"spec": {"infra": {"nodePlacement": {"nodeSelector": {"node-role.kubernetes.io/control-plane": ""}}}}}'
 kubectl -n kubevirt wait kv kubevirt --for condition=Available --timeout=600s
