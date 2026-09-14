598ccd02cf4fecd71ccf157c0ce9d25c0088711c
kimduquan <kimduquan@gmail.com>
2026-09-15 00:35:50 +0700

    #

--- OpenUP/EPF-worker/network.ps1
+++ OpenUP/EPF-worker/network.ps1
@@ -7,3 +7,5 @@ Set-NetIPInterface `
     -InterfaceAlias "vEthernet (Default Switch)" `
     -AddressFamily IPv4 `
     -Forwarding Enabled
+
+Set-VMProcessor -VMName "epf-node" -ExposeVirtualizationExtensions $true
