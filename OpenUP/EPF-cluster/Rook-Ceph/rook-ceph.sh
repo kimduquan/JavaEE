367305458190ac6e0475e84e834664b6eeea9aeb
kimduquan <kimduquan@gmail.com>
2026-09-20 10:32:33 +0700

    #

--- OpenUP/EPF-cluster/Rook-Ceph/rook-ceph.sh
+++ OpenUP/EPF-cluster/Rook-Ceph/rook-ceph.sh
@@ -1,6 +1,13 @@
 . ../../env.sh
+TOOLBOX_IMAGE=ceph/ceph:v20.2.2
+CEPH_IMAGE_TAG=v20.2.2
+../../registry.sh quay.io "$TOOLBOX_IMAGE"
+../../registry.sh quay.io "ceph/ceph:$CEPH_IMAGE_TAG"
 helm repo add rook https://charts.rook.io/release
-helm --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} upgrade --install --create-namespace --namespace rook-ceph --wait --timeout 40m rook-ceph rook/rook-ceph -f values.yaml
+helm --kubeconfig="${EPF_CLUSTER_KUBE_CONFIG}" upgrade --install --create-namespace --namespace rook-ceph --wait --timeout 40m rook-ceph rook/rook-ceph -f values.yaml \
+	--set "toolbox.image=$EPF_CLUSTER_IMAGE_REGISTRY/$TOOLBOX_IMAGE" \
+	--set "cephImage.repository=$EPF_CLUSTER_IMAGE_REGISTRY/ceph/ceph" \
+	--set "cephImage.tag=$CEPH_IMAGE_TAG" \
 helm --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} upgrade --install --create-namespace --namespace rook-ceph --wait --timeout 40m rook-ceph-cluster --set operatorNamespace=rook-ceph rook/rook-ceph-cluster -f values-external.yaml
 kubectl --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} wait -n rook-ceph cephcluster rook-ceph --for condition=Connecting=True --timeout=1200s
 helm repo add ceph-csi-operator https://ceph.github.io/ceph-csi-operator
