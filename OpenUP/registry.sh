24d4d680c776129f1f60f279b99ad96e682e7ef1
kimduquan <kimduquan@gmail.com>
2026-09-20 13:21:53 +0700

    #

--- OpenUP/registry.sh
+++ OpenUP/registry.sh
@@ -1,6 +1,6 @@
 #. env.sh
 REPOSITORY="$1"
 IMAGE="$2"
-sudo microk8s ctr image pull "$REPOSITORY/$IMAGE"
+sudo microk8s ctr image pull --platform linux/amd64 "$REPOSITORY/$IMAGE"
 sudo microk8s ctr image tag "$REPOSITORY/$IMAGE" "$EPF_CLUSTER_IMAGE_REGISTRY/$IMAGE"
 sudo microk8s ctr image push --plain-http --platform linux/amd64 "$EPF_CLUSTER_IMAGE_REGISTRY/$IMAGE"
