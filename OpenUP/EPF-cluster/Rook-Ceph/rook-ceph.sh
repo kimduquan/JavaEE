7f5112d5615fb9da118dffb298e69e16bc6e6c21
kimduquan <kimduquan@gmail.com>
2026-09-20 20:28:04 +0700

    #

--- OpenUP/EPF-cluster/Rook-Ceph/rook-ceph.sh
+++ OpenUP/EPF-cluster/Rook-Ceph/rook-ceph.sh
@@ -4,6 +4,7 @@ CEPH_IMAGE_TAG=v20.2.2
 ROOK_CEPH_IMAGE_TAG=v1.20.7
 CEPH_CSI_OPERATOR_IMAGE_TAG=v1.0.4
 CEPH_CSI_IMAGE_TAG=v3.17.1
+CSI_REGISTRAR_IMAGE_TAG=v2.17.0
 CSI_PROVISIONER_IMAGE_TAG=v6.2.0
 CSI_RESIZER_IMAGE_TAG=v2.1.0
 CSI_ATTACHER_IMAGE_TAG=v4.12.0
@@ -13,6 +14,7 @@ CSI_SNAPSHOTTER_IMAGE_TAG=v8.5.0
 ../../registry.sh quay.io "cephcsi/ceph-csi-operator:$CEPH_CSI_OPERATOR_IMAGE_TAG"
 ../../registry.sh docker.io "rook/ceph:$ROOK_CEPH_IMAGE_TAG"
 ../../registry.sh quay.io "cephcsi/cephcsi:$CEPH_CSI_IMAGE_TAG"
+../../registry.sh registry.k8s.io "sig-storage/csi-node-driver-registrar:$CSI_REGISTRAR_IMAGE_TAG"
 ../../registry.sh registry.k8s.io "sig-storage/csi-provisioner:$CSI_PROVISIONER_IMAGE_TAG"
 ../../registry.sh registry.k8s.io "sig-storage/csi-resizer:$CSI_RESIZER_IMAGE_TAG"
 ../../registry.sh registry.k8s.io "sig-storage/csi-attacher:$CSI_ATTACHER_IMAGE_TAG"
@@ -28,6 +30,8 @@ helm --kubeconfig="${EPF_CLUSTER_KUBE_CONFIG}" upgrade --install --create-namesp
 	--set "image.tag=$ROOK_CEPH_IMAGE_TAG" \
 	--set "csi.cephcsi.repository=$EPF_CLUSTER_IMAGE_REGISTRY/cephcsi/cephcsi" \
 	--set "csi.cephcsi.tag=$CEPH_CSI_IMAGE_TAG" \
+	--set "csi.registrar.repository=$EPF_CLUSTER_IMAGE_REGISTRY/sig-storage/csi-node-driver-registrar" \
+	--set "csi.registrar.tag=$CSI_REGISTRAR_IMAGE_TAG" \
 	--set "csi.provisioner.repository=$EPF_CLUSTER_IMAGE_REGISTRY/sig-storage/csi-provisioner" \
 	--set "csi.provisioner.tag=$CSI_PROVISIONER_IMAGE_TAG" \
 	--set "csi.resizer.repository=$EPF_CLUSTER_IMAGE_REGISTRY/sig-storage/csi-resizer" \
