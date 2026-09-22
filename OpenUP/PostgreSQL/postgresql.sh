9fb4b67d060b638f09a15c3c00d8fc369060d8ec
kimduquan <kimduquan@gmail.com>
2026-09-22 23:11:14 +0700

    #

--- OpenUP/PostgreSQL/postgresql.sh
+++ OpenUP/PostgreSQL/postgresql.sh
@@ -1,4 +1,9 @@
+. ../env.sh
+../registry.sh registry-1.docker.io "bitnami/postgresql:latest"
 kubectl create configmap initdb --from-file=initdb.sql
 kubectl delete pvc/data-postgresql-primary-0
 kubectl delete pvc/data-postgresql-read-0
-helm install postgresql oci://registry-1.docker.io/bitnamicharts/postgresql -f values-postgresql.yaml --wait
+helm upgrade --install postgresql oci://registry-1.docker.io/bitnamicharts/postgresql -f values-postgresql.yaml --wait \
+	--set "global.imageRegistry=$EPF_CLUSTER_IMAGE_REGISTRY" \
+	--set "global.defaultStorageClass=ceph-rbd" \
+	--set "image.registry=$EPF_CLUSTER_IMAGE_REGISTRY"
