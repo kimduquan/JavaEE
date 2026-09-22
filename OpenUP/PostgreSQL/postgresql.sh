47fe1f8bdf5c1b2e23a739e8b8fb6427b1f8f2ba
kimduquan <kimduquan@gmail.com>
2026-09-22 23:20:51 +0700

    #

--- OpenUP/PostgreSQL/postgresql.sh
+++ OpenUP/PostgreSQL/postgresql.sh
@@ -6,4 +6,5 @@ kubectl delete pvc/data-postgresql-read-0
 helm upgrade --install postgresql oci://registry-1.docker.io/bitnamicharts/postgresql -f values-postgresql.yaml --wait \
 	--set "global.imageRegistry=$EPF_CLUSTER_IMAGE_REGISTRY" \
 	--set "global.defaultStorageClass=ceph-rbd" \
-	--set "image.registry=$EPF_CLUSTER_IMAGE_REGISTRY"
+	--set "image.registry=$EPF_CLUSTER_IMAGE_REGISTRY" \
+	--set "global.security.allowInsecureImages=true"
