c5c244c49e0feee93bdfe7ab3f6e7bb53a3bbc10
kimduquan <kimduquan@gmail.com>
2026-09-22 07:16:27 +0700

    #

--- OpenUP/EPF-cluster/Rook-Ceph/rook-ceph.sh
+++ OpenUP/EPF-cluster/Rook-Ceph/rook-ceph.sh
@@ -45,3 +45,17 @@ kubectl --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} wait -n rook-ceph cephcluster ro
 helm repo add ceph-csi-operator https://ceph.github.io/ceph-csi-operator
 helm --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} upgrade --install ceph-csi-drivers --namespace rook-ceph --wait --timeout 30m ceph-csi-operator/ceph-csi-drivers -f csi-values.yaml
 kubectl --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} wait -n rook-ceph -l app=rook-ceph.rbd.csi.ceph.com-ctrlplugin pod --for condition=Ready --timeout=1200s
+kubectl --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} wait -n rook-ceph -l app=rook-ceph.cephfs.csi.ceph.com-ctrlplugin pod --for condition=Ready --timeout=1200s
+
+: > export-env.sh
+
+./export-external-cluster.sh > export-env.sh
+
+. export-env.sh
+
+export KUBECONFIG=$EPF_CLUSTER_KUBE_CONFIG
+
+./import-external-cluster.sh
+
+export KUBECONFIG=
+kubectl --kubeconfig=${EPF_CLUSTER_KUBE_CONFIG} wait -n rook-ceph cephcluster rook-ceph --for condition=Connected=True --timeout=1200s
