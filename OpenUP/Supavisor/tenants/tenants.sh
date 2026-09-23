5ff105c8a89a9a2932fdf6c9be34cfbe88bd4214
kimduquan <kimduquan@gmail.com>
2026-09-23 23:45:07 +0700

    #

--- OpenUP/Supavisor/tenants/tenants.sh
+++ OpenUP/Supavisor/tenants/tenants.sh
@@ -1,6 +1,6 @@
-Authorization="Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE3NzQwMTMzMjgsImV4cCI6MTgwNTU0OTMyOCwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSJ9.Ec31X-x1OOUTkfqduvYFG5UE6k_-9OXMYOS_U2zpi_w"
-curl -X PUT http://localhost:4000/api/tenants/postgres -H "Content-Type: application/json" -H $Authorization -v --data @postgres.json
-curl -X PUT http://localhost:4000/api/tenants/epf -H "Content-Type: application/json" -H $Authorization -v --data @epf.json
-curl -X PUT http://localhost:4000/api/tenants/keycloak -H "Content-Type: application/json" -H $Authorization -v --data @keycloak.json
-curl -X PUT http://localhost:4000/api/tenants/odoo -H "Content-Type: application/json" -H $Authorization -v --data @odoo.json
-curl -X PUT http://localhost:4000/api/tenants/gitea -H "Content-Type: application/json" -H $Authorization -v --data @gitea.json
+Authorization=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE3NzQwMTMzMjgsImV4cCI6MTgwNTU0OTMyOCwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSJ9.Ec31X-x1OOUTkfqduvYFG5UE6k_-9OXMYOS_U2zpi_w
+curl -X PUT http://localhost:4000/api/tenants/postgres -H "Content-Type: application/json" -H "Authorization: Bearer $Authorization" -v --data @postgres.json
+curl -X PUT http://localhost:4000/api/tenants/epf -H "Content-Type: application/json" -H "Authorization: Bearer $Authorization" -v --data @epf.json
+curl -X PUT http://localhost:4000/api/tenants/keycloak -H "Content-Type: application/json" -H "Authorization: Bearer $Authorization" -v --data @keycloak.json
+curl -X PUT http://localhost:4000/api/tenants/odoo -H "Content-Type: application/json" -H "Authorization: Bearer $Authorization" -v --data @odoo.json
+curl -X PUT http://localhost:4000/api/tenants/gitea -H "Content-Type: application/json" -H "Authorization: Bearer $Authorization" -v --data @gitea.json
