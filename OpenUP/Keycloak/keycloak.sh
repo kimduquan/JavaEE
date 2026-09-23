d79d1298e03bacb01a7692de8240d069a8e9d352
kimduquan <kimduquan@gmail.com>
2026-09-23 22:56:40 +0700

    #

--- OpenUP/Keycloak/keycloak.sh
+++ OpenUP/Keycloak/keycloak.sh
@@ -1,3 +1,8 @@
+. ../env.sh
+../registry.sh docker.io "bitnamilegacy/keycloak:26.3.3-debian-12-r0"
 kubectl delete secret keycloak-admin
 kubectl create secret generic keycloak-admin --from-literal=CLIENT_SECRET='y1wf2SyO6p8eZq65KbvbNB1gE0cEYyMx' --from-literal=CLIENT_ID='admin-cli'
-helm upgrade --install keycloak oci://registry-1.docker.io/bitnamicharts/keycloak -f values-keycloak.yaml --wait
+helm upgrade --install keycloak oci://registry-1.docker.io/bitnamicharts/keycloak -f values-keycloak.yaml --wait \
+	--set "global.imageRegistry=$EPF_CLUSTER_IMAGE_REGISTRY" \
+	--set "image.registry=$EPF_CLUSTER_IMAGE_REGISTRY" \
+	--set "ingress.ingressClassName="
