f99bb8c63e3090b096b16e964597801abf699089
kimduquan <kimduquan@gmail.com>
2026-09-26 17:09:43 +0700

    #

--- OpenUP/cluster-autoscaler/cluster-autoscaler.sh
+++ OpenUP/cluster-autoscaler/cluster-autoscaler.sh
@@ -1,8 +1,10 @@
 helm repo add autoscaler https://kubernetes.github.io/autoscaler
 helm repo update
 . ../env.sh
+IMAGE_TAG=v1.35.0
+../registry.sh registry.k8s.io "autoscaling/cluster-autoscaler:$IMAGE_TAG"
 kubectl delete secret epf-cluster
 kubectl create secret generic epf-cluster --from-file=value=${EPF_CLUSTER_KUBE_CONFIG}
-helm upgrade --install --wait --timeout 10m cluster-autoscaler autoscaler/cluster-autoscaler -f values-cluster-autoscaler.yaml
-VM_IP=$(kubectl get vmi --no-headers | awk '{print $4}')
-#ssh -i ${EPF_CLUSTER_SSH_KEY} capk@${VM_IP}
+helm upgrade --install --wait --timeout 10m cluster-autoscaler autoscaler/cluster-autoscaler -f values-cluster-autoscaler.yaml \
+	--set "image.repository=$EPF_CLUSTER_IMAGE_REGISTRY" \
+	--set "image.tag=$IMAGE_TAG"
