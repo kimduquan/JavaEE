555e2def92a7f1354f96b7193098daec18180013
kimduquan <kimduquan@gmail.com>
2026-09-26 14:17:24 +0700

    #

--- OpenUP/EPF-cluster/cilium/cilium.sh
+++ OpenUP/EPF-cluster/cilium/cilium.sh
@@ -1,10 +1,10 @@
 . ../../env.sh
-IMAGE_TAG=v1.20.1
-CERTGEN_IMAGE_TAG=v0.4.9
-ENVOY_IMAGE_TAG=v1.37.5-1786810558-766ccfb37260a43e9d228837aa84ce3faf9f64e7
-OPERATOR_IMAGE_TAG=v1.20.1
+IMAGE_TAG=v1.20.2
+CERTGEN_IMAGE_TAG=v0.4.11
+ENVOY_IMAGE_TAG=v1.37.6-1789133542-cbec91f666af0bf742da986d43832932dbb26b82
+OPERATOR_IMAGE_TAG=v1.20.2
 NODEINIT_IMAGE_TAG=1782916218-36ae25f
-CLUSTERMESH_APISERVER_IMAGE_TAG=v1.20.1
+CLUSTERMESH_APISERVER_IMAGE_TAG=v1.20.2
 ../../registry.sh quay.io "cilium/cilium:$IMAGE_TAG"
 ../../registry.sh quay.io "cilium/certgen:$CERTGEN_IMAGE_TAG"
 ../../registry.sh quay.io "cilium/cilium-envoy:$ENVOY_IMAGE_TAG"
@@ -13,7 +13,7 @@ CLUSTERMESH_APISERVER_IMAGE_TAG=v1.20.1
 ../../registry.sh quay.io "cilium/startup-script:$NODEINIT_IMAGE_TAG"
 ../../registry.sh quay.io "cilium/clustermesh-apiserver:$CLUSTERMESH_APISERVER_IMAGE_TAG"
 helm repo add cilium https://helm.cilium.io/
-helm --kubeconfig="${EPF_CLUSTER_KUBE_CONFIG}" upgrade --install --wait --timeout 40m cilium cilium/cilium --version 1.20.1 --namespace=kube-system -f values-cilium.yaml \
+helm --kubeconfig="${EPF_CLUSTER_KUBE_CONFIG}" upgrade --install --wait --timeout 40m cilium cilium/cilium --version 1.20.2 --namespace=kube-system -f values-cilium.yaml \
 	--set "image.repository=$EPF_CLUSTER_IMAGE_REGISTRY/cilium/cilium" \
 	--set "image.tag=$IMAGE_TAG" \
 	--set "certgen.image.repository=$EPF_CLUSTER_IMAGE_REGISTRY/cilium/certgen" \
@@ -25,4 +25,6 @@ helm --kubeconfig="${EPF_CLUSTER_KUBE_CONFIG}" upgrade --install --wait --timeou
 	--set "nodeinit.image.repository=$EPF_CLUSTER_IMAGE_REGISTRY/cilium/startup-script" \
 	--set "nodeinit.image.tag=$NODEINIT_IMAGE_TAG" \
 	--set "clustermesh.apiserver.image.repository=$EPF_CLUSTER_IMAGE_REGISTRY/cilium/clustermesh-apiserver" \
-	--set "clustermesh.apiserver.image.tag=$CLUSTERMESH_APISERVER_IMAGE_TAG" \
+	--set "clustermesh.apiserver.image.tag=$CLUSTERMESH_APISERVER_IMAGE_TAG"
+#	--set "k8sServiceHost=172.23.225.250" \
+#	--set "k8sServicePort=30443"
